b62b0a7150efa49daaf82efaa86f9962ebe794fa
NaayoungKwon <kny8092@gmail.com>
2022-11-13 19:25:15 +0900

    setting: mongodb 연결

--- server/apps/api/src/api.module.ts
+++ server/apps/api/src/api.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ApiController } from './api.controller';
 import { ApiService } from './api.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `config/${process.env.NODE_ENV}.env`,
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URL),
+  ],
   controllers: [ApiController],
   providers: [ApiService],
 })
