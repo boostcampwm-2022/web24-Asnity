6ec8850d7349eed6802e3759d3c03fb6bcfb02bf
NaayoungKwon <kny8092@gmail.com>
2022-11-14 22:44:33 +0900

    setting: mongodb create user

--- server/apps/api/src/user/user.service.ts
+++ server/apps/api/src/user/user.service.ts
@@ -1,22 +1,14 @@
 import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { User, UserDocument } from '../../../../dao/schemas/user.schema';
-import { Model } from 'mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UsersRepository } from '@repository/user.repository';
 
 @Injectable()
 export class UserService {
-  constructor(@InjectModel(User.name) private doc: Model<UserDocument>) {}
+  constructor(private readonly userRepository: UsersRepository) {}
 
-  getAllFollowers() {}
+  // getAllFollowers() {}
 
   createUser(createUserDto: CreateUserDto) {
-    const { id, pw } = createUserDto;
-    const user = {
-      id,
-      pw,
-      description: 'mongodb test',
-    };
-
+    this.userRepository.create(createUserDto);
   }
 }
--- server/dao/repository/user.repository.ts
+++ server/dao/repository/user.repository.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { User, UserDocument } from '../../../../dao/schemas/user.schema';
+import { User, UserDocument } from '@schemas/user.schema';
 import { Model } from 'mongoose';
-import { CreateUserDto } from './dto/create-user.dto';
+import { CreateUserDto } from '@user/dto/create-user.dto';
 
 @Injectable()
 export class UsersRepository {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
-  createUser(createUserDto: CreateUserDto) {
+  async create(createUserDto: CreateUserDto) {
     const createdUser = new this.userModel(createUserDto);
-    return createdUser.save();
+    await createdUser.save();
+    console.log(this.userModel.find());
+    return null;
   }
 }
