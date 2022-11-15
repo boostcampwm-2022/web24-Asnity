8d9c8e3fb65d3d0a51fbb2d80358cf23d27ddd7c
soomanbaek <qortnaks311@gmail.com>
2022-11-15 17:51:01 +0900

    feat: 회원가입 기능 구현
    - 비밀번호 hash로 변환
    - DB 저장
    - 중복시 예외처리

--- server/apps/api/src/auth/auth.service.ts
+++ server/apps/api/src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { SignUpDto } from './dto';
 import * as argon from 'argon2';
 import { UserRepository } from '@repository/user.repository';
+import { responseForm } from '@utils/responseForm';
 
 @Injectable()
 export class AuthService {
@@ -9,9 +10,14 @@ export class AuthService {
   async signup(dto: SignUpDto) {
     const hash = await argon.hash(dto.password);
     try {
-      const user = await this.userRepository.signup(dto);
+      await this.userRepository.create({ ...dto, password: hash });
     } catch (error) {
+      if (error.name === 'MongoServerError' && error.code === 11000)
+        // 아이디 중복시 에러
+        throw new ForbiddenException('아이디가 중복되었습니다.');
       throw error;
     }
+
+    return responseForm('200', '회원가입 성공!');
   }
 }
