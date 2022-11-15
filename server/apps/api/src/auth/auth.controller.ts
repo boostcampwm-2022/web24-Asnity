7947ee05d51fbcaa52dd7901485cdafa28f8301c
soomanbaek <qortnaks311@gmail.com>
2022-11-15 23:04:12 +0900

    refactor: 로그인 로직 수정
    - service에서 응답을 생성했던 것을 controller 에서 응답 생성하도록 수정

--- server/apps/api/src/auth/auth.controller.ts
+++ server/apps/api/src/auth/auth.controller.ts
@@ -1,18 +1,29 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignUpDto } from './dto';
+import { responseForm } from '@utils/responseForm';
 
 @Controller('api/user/auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signUp(@Body() signUpDto: SignUpDto) {
-    return this.authService.signUp(signUpDto);
+  async signUp(@Body() signUpDto: SignUpDto) {
+    try {
+      const result = await this.authService.signUp(signUpDto);
+      return responseForm(200, result);
+    } catch (error) {
+      throw error;
+    }
   }
 
   @Post('signin')
-  signIn(@Body() signInDto: SignUpDto) {
-    return this.authService.signIn(signInDto);
+  async signIn(@Body() signInDto: SignUpDto) {
+    try {
+      const result = await this.authService.signIn(signInDto);
+      return responseForm(200, result);
+    } catch (error) {
+      throw error;
+    }
   }
 }
