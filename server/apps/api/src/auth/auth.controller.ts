80ec4062138054b1be01dbf8a091af20feb441c5
soomanbaek <qortnaks311@gmail.com>
2022-11-15 16:41:47 +0900

    rename: auth 폴더 경로 변경
    - ./user/auth -> ./auth
    - auth 구현중인 내용 있음

---
+++ server/apps/api/src/auth/auth.controller.ts
@@ -0,0 +1,12 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { SignInDto, SignUpDto } from './dto';
+
+@Controller('api/user/auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+  @Post('signup')
+  signup(@Body() dto: SignUpDto) {
+    return this.authService.signup(dto);
+  }
+}
