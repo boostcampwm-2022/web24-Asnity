814d3294e6280ad4d1b02e35e61a24abcd0672ed
soomanbaek <qortnaks311@gmail.com>
2022-11-15 17:51:01 +0900

    feat: 회원가입 기능 구현
    - 비밀번호 hash로 변환
    - DB 저장
    - 중복시 예외처리

--- server/apps/api/src/auth/auth.controller.ts
+++ server/apps/api/src/auth/auth.controller.ts
@@ -1,10 +1,11 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { SignInDto, SignUpDto } from './dto';
+import { SignUpDto } from './dto';
 
 @Controller('api/user/auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
   @Post('signup')
   signup(@Body() dto: SignUpDto) {
     return this.authService.signup(dto);
