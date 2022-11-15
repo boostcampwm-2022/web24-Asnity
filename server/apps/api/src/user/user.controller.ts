3e8ac367c830a290603781f33cb33fe5495f6779
NaayoungKwon <kny8092@gmail.com>
2022-11-16 00:53:05 +0900

    feat: 팔로우 취소 요청 처리 #50

--- server/apps/api/src/user/user.controller.ts
+++ server/apps/api/src/user/user.controller.ts
@@ -1,9 +1,7 @@
-import { Body, Controller, Get, Inject, LoggerService, Param, Post } from '@nestjs/common';
+import {Body, Controller, Delete, Get, Inject, LoggerService, Param, Post} from '@nestjs/common';
 import { UserService } from './user.service';
-import { AddFollowingDto } from '@user/dto/add-following.dto';
+import { followerDto } from '@user/dto/follower.dto';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
-import { User } from '@schemas/user.schema';
-import { CreateUserDto } from '@user/dto/create-user.dto';
 import { responseForm } from '@utils/responseForm';
 
 @Controller('api/user')
@@ -25,7 +23,7 @@ export class UserController {
     try {
       const myId = '63734e98384f478a32c3a1cc';
       // TODO: Request Header에서 access token으로 현재 사용자 알아내기
-      const addFollowingDto: AddFollowingDto = { myId, followId: id };
+      const addFollowingDto: followerDto = { myId, followId: id };
       await this.userService.addFollowing(addFollowingDto);
       return responseForm(200, {});
     } catch (error) {
@@ -34,6 +32,19 @@ export class UserController {
     }
   }
 
+  @Delete('following/:id')
+  async unFollowing(@Param('id') id: string) {
+    try {
+      const myId = '63734e98384f478a32c3a1cc';
+      // TODO: Request Header에서 access token으로 현재 사용자 알아내기
+      const unFollowingDto: followerDto = { myId, followId: id };
+      await this.userService.unFollowing(unFollowingDto);
+      return responseForm(200, {});
+    } catch (error) {
+      this.logger.error(JSON.stringify(error.response));
+      return error.response;
+    }
+  }
 
   // @Post()
   // createUser(@Body() createUserDto: CreateUserDto) {
