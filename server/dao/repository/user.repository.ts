785752992deeb3ad85c2f126b6fdbe761f557b8b
soomanbaek <qortnaks311@gmail.com>
2022-11-15 17:49:32 +0900

    feat: user.repository.ts create함수 매개변수 형식 변경
    - CreateUserDto -> SignUpDto

--- server/dao/repository/user.repository.ts
+++ server/dao/repository/user.repository.ts
@@ -2,13 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User, UserDocument } from '@schemas/user.schema';
 import { Model } from 'mongoose';
-import { CreateUserDto } from '@user/dto/create-user.dto';
+import { SignUpDto } from '@api/src/auth/dto';
 
 @Injectable()
 export class UserRepository {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
-  async create(createUserDto: CreateUserDto) {
+  async create(createUserDto: SignUpDto) {
     await this.userModel.create(createUserDto);
     return null;
   }
