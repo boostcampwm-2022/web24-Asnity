217002b11217a897140be083d9cf9e33fe47b256
leegwae <leegwae@gmail.com>
2022-11-17 04:33:49 +0900

    design: 공통 레이아웃 - `Gnb` 스타일

---
+++ client/src/features/Gnb/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Gnb = () => {
+  return (
+    <div className="flex flex-col w-[96px] h-full bg-background border-r border-line">
+      Gnb
+    </div>
+  );
+};
+
+export default Gnb;
