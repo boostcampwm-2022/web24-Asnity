51d7f9cf89087c52d41aebd57212899940464980
NaayoungKwon <kny8092@gmail.com>
2022-11-16 15:19:15 +0900

    refactor: 불필요한 미들웨어 삭제  #22

--- server/apps/api/src/main.ts
+++ server/apps/api/src/main.ts
@@ -5,7 +5,6 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
   await app.listen(3000);
 }
