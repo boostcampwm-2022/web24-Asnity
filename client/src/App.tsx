8e93330b5a6a96932674661ba1086eeee686395b
leegwae <leegwae@gmail.com>
2022-11-17 01:08:14 +0900

    feat: 팔로잉, 팔로워, 사용자 검색을 query params에 따라 조건부 렌더링
    - 기존: `/followings`, `/followers`, `/user-search`에 각각 라우터가 등록되어있었음.
    따라서 재렌더링하지 않아도 되는 부분까지 재렌더링해야했음
    - 변경 후: `/dms?tab=`처럼 `tab` query params에 따라 조건부 렌더링

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,11 @@
 import Community from '@pages/Community';
 import DM from '@pages/DM';
 import DMRoom from '@pages/DMRoom';
-import Followers from '@pages/Followers';
-import Followings from '@pages/Followings';
+import Friends from '@pages/Friends';
 import Home from '@pages/Home';
 import NotFound from '@pages/NotFound';
 import SignIn from '@pages/SignIn';
 import SignUp from '@pages/SignUp';
-import UserSearch from '@pages/UserSearch';
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
@@ -15,9 +13,7 @@ const App = () => (
   <Routes>
     <Route path="/" element={<Home />}>
       <Route path="dms" element={<DM />}>
-        <Route path="followings" element={<Followings />} />
-        <Route path="followers" element={<Followers />} />
-        <Route path="user-search" element={<UserSearch />} />
+        <Route index element={<Friends />} />
         <Route path=":roomId" element={<DMRoom />} />
       </Route>
       <Route
