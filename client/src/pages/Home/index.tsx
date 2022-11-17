5228e8e363a29cb6db412a75b0991939582ba192
leegwae <leegwae@gmail.com>
2022-11-17 10:43:41 +0900

    feat: `Home` 페이지 레이아웃 #27
    - display `<Gnb />` and `<Sidebar />`

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,11 +1,13 @@
+import Gnb from '@features/Gnb';
+import Sidebar from '@features/Sidebar';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div className="flex">
-      <div>gnb</div>
-      <div>sidebar</div>
+    <div className="flex flex-1 h-screen">
+      <Gnb />
+      <Sidebar />
       <Outlet />
     </div>
   );
