a555a5d25cf41360e055f96be1958269cfce4e16
Cola <bcad1591@gmail.com>
2022-11-17 14:28:25 +0900

    feat: ToastContainer 추가

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,9 +3,10 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import { injectStyle } from 'react-toastify/dist/inject-style';
 
 import App from './App';
-
 import './index.css';
 
 if (process.env.NODE_ENV === 'development') {
@@ -19,11 +20,18 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
+/**
+ * react-toastify import css 안되는 이슈
+ * // TODO: 링크 업데이트하기
+ */
+injectStyle();
+
 root.render(
   <QueryClientProvider client={queryClient}>
-    <ReactQueryDevtools />
+    {/* <ReactQueryDevtools />*/}
     <BrowserRouter>
       <App />
+      <ToastContainer />
     </BrowserRouter>
   </QueryClientProvider>,
 );
