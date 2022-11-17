21f5589cc1362a3fdfeef0f0f2f25c7c2019063b
soomanbaek <qortnaks311@gmail.com>
2022-11-17 11:23:36 +0900

    feat: ValidationPipe 전역 파이프로 설정

--- server/apps/api/src/main.ts
+++ server/apps/api/src/main.ts
@@ -6,6 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(ApiModule);
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
