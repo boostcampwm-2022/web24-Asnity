db27fae0402d8759b587e53b89ca7d9d35af15aa
leegwae <leegwae@gmail.com>
2022-11-18 01:22:53 +0900

    feat: 팔로잉 목록 보여주는 `Followings` 탭 구현 #65

--- client/src/pages/Followings/index.tsx
+++ client/src/pages/Followings/index.tsx
@@ -1,7 +1,24 @@
+import UserProfile from '@components/UserProfile';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 import React from 'react';
+import { GetUserResponse } from 'shared/lib/getUserResponse';
+
+const getFollowings = () =>
+  axios.get('/api/followings').then((res) => res.data);
 
 const Followings = () => {
-  return <div>Followings</div>;
+  const { isLoading, data } = useQuery(['followings'], getFollowings);
+
+  if (isLoading) return <div>loading</div>;
+
+  return (
+    <ul className="flex flex-col gap-[12px]">
+      {data.result.followings.map(({ _id, ...rest }: GetUserResponse) => (
+        <UserProfile key={_id} user={rest} />
+      ))}
+    </ul>
+  );
 };
 
 export default Followings;
