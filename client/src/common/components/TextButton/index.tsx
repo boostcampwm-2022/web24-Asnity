ea96306779bb639a5211aafd764f3a984112e158
Cola <bcad1591@gmail.com>
2022-11-17 14:38:00 +0900

    [Client] 회원가입 요청 (#71)
    * fix: SuccessMessage color 수정
    
    * feat: Button 컴포넌트 추가
    
    * feat: TextButton 컴포넌트 추가
    
    * feat: 회원가입 페이지 업데이트
    
    - 회원가입 버튼
    - 로그인 페이지 이동 버튼 추가
    - logo섹션을 form에서 분리
    
    * chore: EnvironmentPlugin 업데이트
    
    API_URL 추가
    
    * chore: API_URL 상수 정의
    
    * chore: Auth Mock API 업데이트
    
    * feat: 회원가입 기능 추가
    
    * feat: ToastContainer 추가
    
    * chore: react-toastify 패키지 추가

---
+++ client/src/common/components/TextButton/index.tsx
@@ -0,0 +1,49 @@
+import type { ReactNode, ComponentPropsWithoutRef, CSSProperties } from 'react';
+
+import cn from 'classnames';
+import React from 'react';
+
+const textButtonSize = {
+  xl: 'text-s20',
+  lg: 'text-s18',
+  md: 'text-s16',
+  sm: 'text-s14',
+  xs: 'text-s12',
+};
+
+const textButtonColor = {
+  default: '',
+  primary: 'text-primary hover:text-primary-dark active:text-primary',
+  secondary: 'text-secondary hover:text-secondary-dark active:text-secondary',
+  dark: 'text-body hover:text-titleActive active:text-body',
+};
+
+export type TextButtonColor = keyof typeof textButtonColor;
+export type TextButtonSize = keyof typeof textButtonSize;
+
+export interface Props extends ComponentPropsWithoutRef<'button'> {
+  children: ReactNode;
+  size: TextButtonSize;
+  color?: TextButtonColor;
+  style?: CSSProperties;
+  className?: string;
+}
+
+const TextButton: React.FC<Props> = ({
+  children,
+  size,
+  color = 'default',
+  className,
+  style,
+  ...restProps
+}) => {
+  const classes = cn([textButtonSize[size], textButtonColor[color], className]);
+
+  return (
+    <button style={style} {...restProps} className={classes}>
+      {children}
+    </button>
+  );
+};
+
+export default TextButton;
