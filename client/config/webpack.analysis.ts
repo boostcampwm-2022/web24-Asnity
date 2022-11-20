647a5161993e2cd68c0fcf4b026d5f5d26bda332
Cola <bcad1591@gmail.com>
2022-11-20 22:41:22 +0900

    chore: 번들 사이즈 분석용 스크립트 추가

---
+++ client/config/webpack.analysis.ts
@@ -0,0 +1,47 @@
+import type { Configuration } from 'webpack';
+
+import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import TerserPlugin from 'terser-webpack-plugin';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+import { merge } from 'webpack-merge';
+
+import common from './webpack.common';
+
+const config: Configuration = {
+  devtool: 'inline-source-map',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      reportFilename: 'report.html',
+      openAnalyzer: false,
+    }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin(),
+      new TerserPlugin({
+        terserOptions: {
+          format: {
+            comments: false,
+          },
+        },
+        extractComments: false,
+      }),
+    ],
+  },
+};
+
+export default merge(common, config);
