6604278bf715728ed7817e20022f4130f328d181
Cola <bcad1591@gmail.com>
2022-11-20 20:58:57 +0900

    feat: tokenStore 추가
    액세스 토큰 보관 스토어

---
+++ client/src/stores/tokenStore.ts
@@ -0,0 +1,19 @@
+import { useStore } from 'zustand';
+import { devtools } from 'zustand/middleware';
+import createVanillaStore from 'zustand/vanilla';
+
+type TokenStore = {
+  accessToken: null | string;
+  setAccessToken: (accessToken: string) => void;
+};
+
+export const tokenStore = createVanillaStore<TokenStore>()(
+  devtools((set) => ({
+    accessToken: null,
+    setAccessToken: (newAccessToken) =>
+      set(() => ({ accessToken: newAccessToken })),
+  })),
+);
+
+export const useTokenStore = <T>(selector: (state: TokenStore) => T) =>
+  useStore(tokenStore, selector);
