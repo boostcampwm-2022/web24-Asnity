b43f808f052f0a5a0d4e77182c56ce2e4cd47320
leegwae <leegwae@gmail.com>
2022-11-21 06:18:18 +0900

    feat: GET `/api/users/followings` 목업 API에 검색 query 추가 #65

--- client/src/mocks/handlers/Friend.js
+++ client/src/mocks/handlers/Friend.js
@@ -2,14 +2,20 @@ import { rest } from 'msw';
 
 import { users } from '../data';
 
-const GetFollowings = rest.get('/api/user/followings', (req, res, ctx) => {
+const GetFollowings = rest.get('/api/users/followings', (req, res, ctx) => {
+  const query = req.url.searchParams.get('query') ?? '';
+
   return res(
     ctx.delay(),
     ctx.status(200),
     ctx.json({
       statusCode: 200,
       result: {
-        followings: users,
+        followings: query
+          ? users.filter(({ nickname }) =>
+              nickname.toUpperCase().includes(query.toUpperCase()),
+            )
+          : users,
       },
     }),
   );
