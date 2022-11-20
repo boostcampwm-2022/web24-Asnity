b7d1a4e71d99f3933259a2f99e5c3e2469cfb877
leegwae <leegwae@gmail.com>
2022-11-20 22:38:06 +0900

    design: figma 시안에 맞게 scale 조정 #65

--- client/src/features/Gnb/index.tsx
+++ client/src/features/Gnb/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Gnb = () => {
   return (
-    <div className="flex flex-col w-[96px] h-full bg-background border-r border-line">
+    <div className="flex flex-col w-20 h-full bg-background border-r border-line">
       Gnb
     </div>
   );
