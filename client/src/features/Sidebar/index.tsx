6248b84d93d1fbf3182e6880acfd2ba2a527fb01
leegwae <leegwae@gmail.com>
2022-11-20 22:46:06 +0900

    feat: Sidebar에 로그인한 사용자 프로필 컴포넌트 구현

--- client/src/features/Sidebar/index.tsx
+++ client/src/features/Sidebar/index.tsx
@@ -1,18 +1,28 @@
+import UserProfile from '@components/UserProfile';
 import CommunityNav from '@features/CommunityNav';
 import DmNav from '@features/DmNav';
+import { Cog6ToothIcon } from '@heroicons/react/20/solid';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 
+const getMyInfo = () => axios.get('/api/user/auth/me').then((res) => res.data);
+
 const Sidebar = () => {
   const { pathname } = useLocation();
+  const { isLoading, data } = useQuery(['me'], getMyInfo);
 
   return (
     <div className="flex flex-col w-80 h-full bg-background border-r border-line">
       <nav className="flex flex-col flex-1">
         {pathname.startsWith('/dms') ? <DmNav /> : <CommunityNav />}
       </nav>
-      <div className="flex w-full h-20 bg-inputBackground border-t border-line">
-        user profile
+      <div className="flex justify-between items-center w-full px-4 bg-inputBackground border-t border-line">
+        {isLoading ? 'loading' : <UserProfile user={data.result.user} />}
+        <button className="w-7 h-7">
+          <Cog6ToothIcon className="fill-label" />
+        </button>
       </div>
     </div>
   );
