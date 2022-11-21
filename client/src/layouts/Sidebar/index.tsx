94b61d32f0088d25d9b614da4c6b0eb69443300e
Cola <bcad1591@gmail.com>
2022-11-22 04:17:53 +0900

    refactor: refactor할 내용들 주석으로 추가

--- client/src/layouts/Sidebar/index.tsx
+++ client/src/layouts/Sidebar/index.tsx
@@ -11,17 +11,22 @@ const getMyInfo = () => axios.get('/api/user/auth/me').then((res) => res.data);
 
 const Sidebar = () => {
   const { pathname } = useLocation();
+
+  // TODO: 쿼리 컨벤션으로 바꾸기!
   const { isLoading, data } = useQuery(['me'], getMyInfo);
 
+  // TODO: DMNav와 CommunityNav가 좀 더 정확한 의미를 갖도록 바꾸기!
   return (
     <div className="flex flex-col min-w-[320px] w-[320px] h-full bg-background border-r border-line">
       <nav className="flex flex-col flex-1">
         {pathname.startsWith('/dms') ? <DmNav /> : <CommunityNav />}
       </nav>
       <div className="flex justify-between items-center w-full px-4 bg-inputBackground border-t border-line">
+        {/* TODO: ...Query.isLoading ? ... 로 바꾸기!  */}
         {isLoading ? 'loading' : <UserProfile user={data.result.user} />}
         <button>
-          <Cog6ToothIcon className="w-7 h-7 fill-label" />
+          <span className="sr-only">환경설정</span>
+          <Cog6ToothIcon className="w-[28px] h-auto fill-label" />
         </button>
       </div>
     </div>
