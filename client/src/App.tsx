a34b32fac35b18c810d91eb144e2fe10ff4a16ff
Cola <bcad1591@gmail.com>
2022-11-21 16:15:52 +0900

    refactor: 라우터 구조 수정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,30 +1,49 @@
+import AuthorizedLayer from '@components/AuthorizedLayer';
+import UnAuthorizedLayer from '@components/UnAuthorizedLayer';
+import AccessDenied from '@pages/AccessDenied';
 import Community from '@pages/Community';
 import DM from '@pages/DM';
 import DMRoom from '@pages/DMRoom';
 import Friends from '@pages/Friends';
 import Home from '@pages/Home';
 import NotFound from '@pages/NotFound';
+import Root from '@pages/Root';
 import SignIn from '@pages/SignIn';
 import SignUp from '@pages/SignUp';
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import {
+  RouterProvider,
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
 
-const App = () => (
-  <Routes>
-    <Route path="/" element={<Home />}>
-      <Route path="dms" element={<DM />}>
-        <Route index element={<Friends />} />
-        <Route path=":roomId" element={<DMRoom />} />
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<Root />} />
+      <Route element={<AuthorizedLayer />}>
+        <Route element={<Home />}>
+          <Route path="dms" element={<DM />}>
+            <Route index element={<Friends />} />
+            <Route path=":roomId" element={<DMRoom />} />
+          </Route>
+          <Route
+            path="communities/:communityId/channels/:roomId"
+            element={<Community />}
+          />
+        </Route>
       </Route>
-      <Route
-        path="communities/:communityId/channels/:roomId"
-        element={<Community />}
-      />
-    </Route>
-    <Route path="/sign-in" element={<SignIn />} />
-    <Route path="/sign-up" element={<SignUp />} />
-    <Route path="*" element={<NotFound />} />
-  </Routes>
+      <Route element={<UnAuthorizedLayer />}>
+        <Route path="sign-in" element={<SignIn />} />
+        <Route path="sign-up" element={<SignUp />} />
+      </Route>
+      <Route path="/error" element={<AccessDenied />} />
+      <Route path="*" element={<NotFound />} />
+    </Route>,
+  ),
 );
+
+const App = () => <RouterProvider router={router} />;
 
 export default App;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,6 @@ import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import { injectStyle } from 'react-toastify/dist/inject-style';
 
@@ -28,10 +27,8 @@ injectStyle();
 
 root.render(
   <QueryClientProvider client={queryClient}>
-    {/* <ReactQueryDevtools />*/}
-    <BrowserRouter>
-      <App />
-      <ToastContainer />
-    </BrowserRouter>
+    <ReactQueryDevtools />
+    <App />
+    <ToastContainer />
   </QueryClientProvider>,
 );
