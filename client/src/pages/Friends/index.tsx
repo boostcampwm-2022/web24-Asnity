be31f92b71fc9ca41c7c80b414437b418fb277ca
Cola <bcad1591@gmail.com>
2022-11-22 04:17:53 +0900

    refactor: refactor할 내용들 주석으로 추가

--- client/src/pages/Friends/index.tsx
+++ client/src/pages/Friends/index.tsx
@@ -3,12 +3,14 @@ import Followers from '@pages/Followers';
 import UserSearch from '@pages/UserSearch';
 import React, { useState } from 'react';
 
+// TODO: 네이밍 생각해보기
 const TAB = {
   FOLLOWINGS: 'followings',
   FOLLOWERS: 'followers',
   USER_SEARCH: 'user-search',
-};
+} as const;
 
+// TODO: 필드 이름 수정하기
 const tabs = [
   {
     name: '팔로잉',
@@ -22,8 +24,9 @@ const tabs = [
     name: '사용자 검색',
     tab: 'user-search',
   },
-];
+] as const;
 
+// TODO: 컴포넌트 이름 수정하기 (FollowingTab -> Followings)
 const TabPanel: Record<string, JSX.Element> = {
   [TAB.FOLLOWINGS]: <FollowingTab />,
   [TAB.FOLLOWERS]: <Followers />,
@@ -33,7 +36,9 @@ const TabPanel: Record<string, JSX.Element> = {
 const DEFAULT_TAB = TAB.FOLLOWINGS;
 
 const Friends = () => {
-  const [tab, setTab] = useState(DEFAULT_TAB);
+  const [tab, setTab] = useState<'followings' | 'followers' | 'user-search'>(
+    DEFAULT_TAB,
+  );
 
   return (
     <div className="w-full h-full flex flex-col">
