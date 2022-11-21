2d11df59960de970f27d47a349e4e1bbd79304ba
NaayoungKwon <kny8092@gmail.com>
2022-11-21 21:08:28 +0900

    test: createCommunity test

---
+++ server/__mock__/community.mock.ts
@@ -0,0 +1,8 @@
+import { CreateCommunityDto } from '@api/src/community/dto/create-community.dto';
+
+export const communityDto1 = {
+  name: 'asnity commu',
+  managerId: '63734af9e62b37012c73e399',
+  description: 'test description',
+  profileUrl: 'test profileUrl',
+} as CreateCommunityDto;
