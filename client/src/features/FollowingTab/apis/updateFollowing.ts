0e7fd86d859dc3f95018cad163474b9e40f7c0ea
Cola <bcad1591@gmail.com>
2022-11-21 15:52:56 +0900

    refactor: Mock API BASE_URL 추가

--- client/src/features/FollowingTab/apis/updateFollowing.ts
+++ client/src/features/FollowingTab/apis/updateFollowing.ts
@@ -1,6 +1,9 @@
+import { API_URL } from '@constants/url';
 import axios from 'axios';
 
+const BASE_URL = `${API_URL}/api`;
+
 const updateFollowing = (userId: string) =>
-  axios.post(`/api/user/following/${userId}`).then((res) => res.data);
+  axios.post(`${BASE_URL}/user/following/${userId}`).then((res) => res.data);
 
 export default updateFollowing;
--- client/src/mocks/handlers/Friend.js
+++ client/src/mocks/handlers/Friend.js
@@ -1,28 +1,34 @@
+import { API_URL } from '@constants/url';
 import { rest } from 'msw';
 
-import { users } from '../data';
+import { users } from '../data/users';
 
-const GetFollowings = rest.get('/api/user/followings', (req, res, ctx) => {
-  const query = req.url.searchParams.get('query') ?? '';
+const BASE_URL = `${API_URL}/api`;
 
-  return res(
-    ctx.delay(),
-    ctx.status(200),
-    ctx.json({
-      statusCode: 200,
-      result: {
-        followings: query
-          ? users.filter(({ nickname }) =>
-              nickname.toUpperCase().includes(query.toUpperCase()),
-            )
-          : users,
-      },
-    }),
-  );
-});
+const GetFollowings = rest.get(
+  `${BASE_URL}/user/followings`,
+  (req, res, ctx) => {
+    const query = req.url.searchParams.get('query') ?? '';
+
+    return res(
+      ctx.delay(),
+      ctx.status(200),
+      ctx.json({
+        statusCode: 200,
+        result: {
+          followings: query
+            ? users.filter(({ nickname }) =>
+                nickname.toUpperCase().includes(query.toUpperCase()),
+              )
+            : users,
+        },
+      }),
+    );
+  },
+);
 
 const UpdateFollowing = rest.post(
-  '/api/user/following/:userId',
+  `${BASE_URL}/user/following/:userId`,
   (req, res, ctx) => {
     const { userId } = req.params;
     const idx = users.findIndex((user) => user._id === userId);
