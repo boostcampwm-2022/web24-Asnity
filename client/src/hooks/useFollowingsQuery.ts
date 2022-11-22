9b46b74480da25a6b23d9462da1282dc1c0bec1d
leegwae <leegwae@gmail.com>
2022-11-22 19:55:19 +0900

    refactor: 팔로잉 목록 가져오는 쿼리 `useFollowinQuery`로 분리 #99
    - `useQuery`의 `select` 옵션으로 검색 필터 적용

--- client/src/hooks/useFollowingsQuery.ts
+++ client/src/hooks/useFollowingsQuery.ts
@@ -1,22 +1,17 @@
 import type { UseQueryOptions } from '@tanstack/react-query';
 
+import { getFollowings, GetFollowingsResponse } from '@apis/user';
 import { useQuery } from '@tanstack/react-query';
+import queryKeyCreator from 'src/queryKeyCreator';
 
-import getFollowings from '../apis/getFollowings';
-
-type QueryOptions = Omit<
-  UseQueryOptions<{ s: string }, unknown, { s: string }, string[]>,
-  'queryKey' | 'queryFn' | 'initialData'
->;
-
-// TODO: useQuery<응답 데이터> 제네릭 활용해서 타이핑이 가능합니다.
-// TODO: 옵션 타이핑 방법은 더 찾아봐야 알겠지만 위와 같이 가능한 것 같기도.
-const useFollowingsQuery = (search: string, options?: QueryOptions) => {
-  const query = useQuery(
-    ['followings', search],
-    () => getFollowings(search),
-    options,
-  );
+const useFollowingsQuery = (options?: {
+  suspense: boolean;
+  select: (data: GetFollowingsResponse) => any;
+}) => {
+  const key = queryKeyCreator.followings();
+  const query = useQuery(key, getFollowings, {
+    ...options,
+  });
 
   return query;
 };
