635d36f8d98706f0aba235f8d0cc5b48a9680888
Cola <bcad1591@gmail.com>
2022-11-23 05:08:17 +0900

    feat: Root 페이지 추가

--- client/src/pages/Root/index.tsx
+++ client/src/pages/Root/index.tsx
@@ -1,7 +1,15 @@
+import { useTokenStore } from '@stores/tokenStore';
 import React from 'react';
+import { Navigate } from 'react-router-dom';
 
 const Root = () => {
-  return <div></div>;
+  const user = useTokenStore((state) => state.user);
+  const accessToken = useTokenStore((state) => state.accessToken);
+
+  // 로그인 되어있으면 dms, 로그인 되어있지 않으면 /sign-in
+  // 조건문에 둘 중 하나라도 없으면 /sign-in -> / -> /sign-in ... 무한 루프돕니다.
+  if (user || accessToken) return <Navigate to="/dms" replace />;
+  return <Navigate to="/sign-in" replace />;
 };
 
 export default Root;
