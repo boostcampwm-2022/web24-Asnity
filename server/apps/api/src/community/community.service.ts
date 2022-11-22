06d4d776daeb93559fb7bdb15512d35ed94c3199
NaayoungKwon <kny8092@gmail.com>
2022-11-22 23:55:51 +0900

    feat: 커뮤니티에 사용자 추가 service layer 구현 #95

--- server/apps/api/src/community/community.service.ts
+++ server/apps/api/src/community/community.service.ts
@@ -2,6 +2,8 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateCommunityDto } from '@api/src/community/dto/create-community.dto';
 import { CommunityRepository } from '@repository/community.repository';
 import { UserRepository } from '@repository/user.repository';
+import { AppendUsersToCommunityDto } from '@api/src/community/dto/append-particitants-to-community.dto';
+import { getUserBasicInfo } from '@user/dto/user-basic-info.dto';
 
 @Injectable()
 export class CommunityService {
@@ -19,5 +21,31 @@ export class CommunityService {
       ...createCommunityDto,
       users: [createCommunityDto.managerId],
     });
+  }
+
+  async appendParticipantsToCommunity(appendUsersToCommunityDto: AppendUsersToCommunityDto) {
+    const community = await this.communityRepository.findById(
+      appendUsersToCommunityDto.community_id,
+    );
+    if (!community) {
+      throw new BadRequestException('해당하는 커뮤니티의 _id가 올바르지 않습니다.');
+    }
+    await Promise.all(
+      appendUsersToCommunityDto.users.map((user) =>
+        this.userRepository.findById(user).then((result) => {
+          if (!result) {
+            throw new BadRequestException(
+              `커뮤니티에 추가를 요청한 사용자 _id(${user})가 올바르지 않습니다.`,
+            );
+          }
+        }),
+      ),
+    );
+    await this.communityRepository.addArrAtArr(
+      { _id: appendUsersToCommunityDto.community_id },
+      'users',
+      appendUsersToCommunityDto.users,
+    );
+    return { message: '커뮤니티 사용자 추가 완료' };
   }
 }
