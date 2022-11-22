6bf4c46497da58a9e67a3221ca9ac33557e98d69
leegwae <leegwae@gmail.com>
2022-11-22 19:55:19 +0900

    refactor: 팔로잉 목록 가져오는 쿼리 `useFollowinQuery`로 분리 #99
    - `useQuery`의 `select` 옵션으로 검색 필터 적용

--- client/src/layouts/FollowingTab/index.tsx
+++ client/src/layouts/FollowingTab/index.tsx
@@ -1,19 +1,26 @@
-import React, { useState, Suspense } from 'react';
+import useDebouncedValue from '@hooks/useDebouncedValue';
+import useFollowingsQuery from '@hooks/useFollowingsQuery';
+import React, { useState, Suspense, useEffect } from 'react';
 
 import FollowingList from './components/list';
 import SearchInput from './components/searchInput';
-import useDebouncedValue from './hooks/useDebouncedValue';
-import useFollowingsQuery from './hooks/useFollowingsQuery';
 
 const FollowingTab = () => {
   const DEBOUNCE_DELAY = 500;
   const [filter, setFilter] = useState('');
   const debouncedFilter = useDebouncedValue(filter, DEBOUNCE_DELAY);
-
-  // TODO: Suspense는 모든 쿼리 인스턴스에 사용할 것인가요?
-  // TODO: Suspense가 사용되는 쿼리와 그렇지 않은 쿼리는 어떤 차이를 두고 사용하시나요?
-  const { data } = useFollowingsQuery(debouncedFilter, {
+  const followingQuery = useFollowingsQuery({
     suspense: true,
+    select: (data) => {
+      const { result } = data;
+      const followings = debouncedFilter
+        ? result.followings.filter(({ nickname }) =>
+          nickname.toUpperCase().includes(filter.toUpperCase()),
+        )
+        : result.followings;
+
+      return { ...data, result: { ...result, followings } };
+    },
   });
 
   return (
@@ -26,8 +33,8 @@ const FollowingTab = () => {
         />
       </div>
       <Suspense fallback={<div>loading...</div>}>
-        {data.result.followings.length ? (
-          <FollowingList users={data.result.followings} />
+        {followingQuery.data?.result.followings ? (
+          <FollowingList users={followingQuery.data.result.followings} />
         ) : (
           '일치하는 사용자가 없습니다.'
         )}
