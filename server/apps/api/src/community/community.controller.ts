9a7c0c4ee3673ab7a0815a8619d131c480ccdac1
NaayoungKwon <kny8092@gmail.com>
2022-11-22 13:53:57 +0900

    refactor: access token 가져와서 _id 확인

--- server/apps/api/src/community/community.controller.ts
+++ server/apps/api/src/community/community.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Inject, LoggerService, Post } from '@nestjs/common';
+import { Body, Controller, Inject, LoggerService, Post, Req, UseGuards } from '@nestjs/common';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { CommunityService } from '@api/src/community/community.service';
 import { CreateCommunityDto } from '@api/src/community/dto/create-community.dto';
 import { responseForm } from '@utils/responseForm';
+import { JwtAccessGuard } from '@api/src/auth/guard';
 
 @Controller('api/community')
 export class CommunityController {
@@ -12,9 +13,10 @@ export class CommunityController {
   ) {}
 
   @Post()
-  async crateCommunity(@Body() createCommunityDto: CreateCommunityDto) {
+  @UseGuards(JwtAccessGuard)
+  async crateCommunity(@Body() createCommunityDto: CreateCommunityDto, @Req() req: any) {
     try {
-      const _id = '6379c1b25d4f08bbe0c940e1';
+      const _id = req.user._id;
       const result = await this.communityService.createCommunity({
         managerId: _id,
         ...createCommunityDto,
