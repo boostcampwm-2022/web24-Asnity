944668ef27110c924530505752b44b65afb4e0bb
Cola <bcad1591@gmail.com>
2022-11-23 22:41:25 +0900

    feat: user 상태 업데이트 (기존에 테스트용으로 tokenStore에 추가했던 user상태를 삭제)

--- client/src/stores/tokenStore.ts
+++ client/src/stores/tokenStore.ts
@@ -2,16 +2,13 @@ import { useStore } from 'zustand';
 import { devtools } from 'zustand/middleware';
 import createVanillaStore from 'zustand/vanilla';
 
-// NOTE: user는 임시로 넣어둔 상태입니다.
 type TokenStore = {
-  user: boolean;
   accessToken: null | string;
   setAccessToken: (accessToken: null | string) => void;
 };
 
 export const tokenStore = createVanillaStore<TokenStore>()(
   devtools((set) => ({
-    user: false,
     accessToken: null,
     setAccessToken: (newAccessToken) =>
       set(() => ({ accessToken: newAccessToken })),
