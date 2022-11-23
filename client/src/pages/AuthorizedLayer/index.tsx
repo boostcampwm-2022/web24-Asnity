6b9f7c600502cc5d46da7d4283f96a77356a0b57
Cola <bcad1591@gmail.com>
2022-11-23 22:39:25 +0900

    feat: user 정보 가져오기 업데이트, 주석 삭제

--- client/src/pages/AuthorizedLayer/index.tsx
+++ client/src/pages/AuthorizedLayer/index.tsx
@@ -1,3 +1,4 @@
+import { useMyInfo } from '@hooks/useMyInfoQuery';
 import useReissueTokenMutation from '@hooks/useReissueTokenMutation';
 import { useTokenStore } from '@stores/tokenStore';
 import React, { useEffect } from 'react';
@@ -10,20 +11,10 @@ import { Outlet, useNavigate } from 'react-router-dom';
  * - 토큰 갱신 요청시, 알 수 없는 에러가 발생하면 **`/unknown-error`** 로 리다이렉트 된다.
  */
 const AuthorizedLayer = () => {
-  const user = useTokenStore((state) => state.user);
+  const user = useMyInfo();
 
   const accessToken = useTokenStore((state) => state.accessToken);
   const navigate = useNavigate();
-
-  // 1-1. 유저가 있는지 없는지 체크
-  // 1-2. 유저가 있으면 통과
-  // if (user) return <Outlet />
-  // TODO: 준영님 리팩토링 머지 되면 작업할 예정.
-
-  // Silent Refresh
-  // 성공시 현재 페이지 그대로 렌더링
-  // 유효하지 않은 토큰 에러: /sign-in (/access-denied 페이지로 이동하는게 맞는 것 같기도?)
-  // 알 수 없는 에러: /unknown-error
 
   const reissueTokenMutation = useReissueTokenMutation(() => {
     navigate('/sign-in', { state: { alreadyTriedReissueToken: true } });
@@ -35,7 +26,7 @@ const AuthorizedLayer = () => {
     reissueTokenMutation.mutate();
   }, []);
 
-  if (!user && !accessToken) return <div>로딩중...</div>; // Spinner 넣기
+  if (!(user || accessToken)) return <div>로딩중...</div>; // Spinner 넣기
   return <Outlet />;
 };
 
