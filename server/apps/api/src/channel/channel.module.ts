9f12184d8114413db767ab6a7a745d6409fd5db2
soomanbaek <qortnaks311@gmail.com>
2022-11-23 16:50:18 +0900

    feat: channel repository 생성 및 channel moduel에 추가

--- server/apps/api/src/channel/channel.module.ts
+++ server/apps/api/src/channel/channel.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ChannelController } from './channel.controller';
 import { ChannelService } from './channel.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Community, CommunitySchema } from '@schemas/community.schema';
+import { ChannelRepository } from '@repository/channel.repository';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Community.name, schema: CommunitySchema }])],
   controllers: [ChannelController],
-  providers: [ChannelService]
+  providers: [ChannelService, ChannelRepository],
 })
 export class ChannelModule {}
