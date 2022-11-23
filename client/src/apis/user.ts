7ff663cbfc85b0293126fd74c59710f28218fc22
leegwae <leegwae@gmail.com>
2022-11-23 17:22:26 +0900

    feat: 팔로워 목록 가져오기 API 작성 #66

--- client/src/apis/user.ts
+++ client/src/apis/user.ts
@@ -1,6 +1,6 @@
+import type { SuccessResponse } from '@@types/apis/response';
 import type { User } from 'shared/lib/user';
 
-import { SuccessResponse } from '@@types/apis/response';
 import { API_URL } from '@constants/url';
 import axios from 'axios';
 
@@ -21,3 +21,12 @@ export const updateFollowing = (
   userId: string,
 ): Promise<UpdateFollowingResponse> =>
   axios.post(`${API_URL}/api/user/following/${userId}`).then((res) => res.data);
+
+export interface GetFollowersResult {
+  followers: User[];
+}
+
+export type GetFollowersResponse = SuccessResponse<GetFollowersResult>;
+
+export const getFollowers = (): Promise<GetFollowersResponse> =>
+  axios.get(`${API_URL}/api/user/followers`).then((res) => res.data);
