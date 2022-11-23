41ab558d6b19d835d4ace72fefde8a0525ade23b
leegwae <leegwae@gmail.com>
2022-11-23 17:22:49 +0900

    feat: `useFollowersQuery` 작성 #66

--- client/src/queryKeyCreator.ts
+++ client/src/queryKeyCreator.ts
@@ -3,6 +3,7 @@ const queryKeyCreator = {
   signUp: () => ['signUp'],
   signIn: () => ['signIn'],
   followings: (): [string] => ['followings'],
+  followers: (): [string] => ['followers'],
 } as const;
 
 export default queryKeyCreator;
