325e8a9cdaf9151c3882a49513795bb3fd382e26
soomanbaek <qortnaks311@gmail.com>
2022-11-24 18:00:05 +0900

    fix: 에러 처리

--- server/apps/api/src/channel/channel.service.ts
+++ server/apps/api/src/channel/channel.service.ts
@@ -14,26 +14,26 @@ export class ChannelService {
 
   async createChannel(createChannelDto: CreateChannelDto) {
     // 자신이 속한 커뮤니티 찾기
-    const community = await this.communityRepository.findById(createChannelDto.communityId);
-    if (community === null) {
-      throw new BadRequestException('커뮤니티가 존재하지 않음');
+    try {
+      const community = await this.communityRepository.findById(createChannelDto.communityId);
+
+      // 채널 생성
+      const channel = await this.channelRepository.create({
+        ...createChannelDto,
+        users: [createChannelDto.managerId],
+      });
+
+      // 커뮤니티 목록에 채널 업데이트
+      await this.communityRepository.addArrAtArr({ _id: community.id }, 'channels', [
+        channel._id.toString(),
+      ]);
+
+      // 유저 목록에 자신이 속한 채널 업데이트
+      const newChannel = {};
+      newChannel[`communities.${community._id.toString()}.channels.${channel._id}`] = new Date();
+      await this.userRepository.updateObject({ _id: createChannelDto.managerId }, newChannel);
+    } catch (error) {
+      throw new BadRequestException('채널 생성 중 오류 발생!');
     }
-    if (createChannelDto.name in community.channels) {
-      throw new BadRequestException('중복된 이름의 채널이 같은 커뮤니티에 있습니다.');
-    }
-
-    // 채널 생성
-    const channel = await this.channelRepository.create({
-      ...createChannelDto,
-      users: [createChannelDto.managerId],
-    });
-    const newChannel = {};
-    newChannel[`communities.${community._id.toString()}.channels.${channel._id}`] = new Date();
-
-    // 커뮤니티 목록에 채널 업데이트
-    await this.communityRepository.addArrAtArr({ id: community.id }, 'channels', [channel._id]);
-
-    // 유저 목록에 자신이 속한 채널 업데이트
-    await this.userRepository.updateObject({ _id: createChannelDto.managerId }, newChannel);
   }
 }
