ee0df87399aafae9c817794dcddc63e9cc3fe231
Cola <bcad1591@gmail.com>
2022-11-25 04:01:16 +0900

    feat: Avatar 업데이트
    Props 추가, children props 추가

--- client/src/components/Avatar/index.tsx
+++ client/src/components/Avatar/index.tsx
@@ -1,3 +1,5 @@
+import type { ReactNode } from 'react';
+
 import React from 'react';
 
 export interface AvatarProps {
@@ -5,6 +7,8 @@ export interface AvatarProps {
   variant: 'circle' | 'rectangle';
   name: string;
   url?: string;
+  className?: string;
+  children?: ReactNode;
 }
 
 const ROUNDED = {
@@ -17,20 +21,42 @@ const SCALE = {
   medium: 'w-[65px] h-[65px]',
 };
 
-const Avatar: React.FC<AvatarProps> = ({ name, url, size, variant }) => {
+const getFirstLetter = (str: string) => {
+  const firstLetter = str.at(0);
+
+  if (!firstLetter) {
+    console.warn(
+      `getFirstLetter의 인자로는 반드시 길이 1이상의 문자열이 들어와야 합니다.`,
+    );
+    return '';
+  }
+
+  return firstLetter;
+};
+
+const Avatar: React.FC<AvatarProps> = ({
+  name,
+  url,
+  size,
+  variant,
+  className = '',
+  children,
+}) => {
   return (
     <div
-      className={`flex justify-center items-center ${SCALE[size]} border border-line ${ROUNDED[variant]} text-s24 italic font-bold overflow-hidden select-none`}
+      className={`flex justify-center items-center ${SCALE[size]} border border-line ${ROUNDED[variant]} text-s24 italic font-bold overflow-hidden select-none ${className}`}
     >
-      {url ? (
-        <img
-          className="object-cover"
-          src={url}
-          alt={`${name}의 프로필 이미지`}
-        />
-      ) : (
-        name
-      )}
+      {children}
+      {!children &&
+        (url ? (
+          <img
+            className="object-cover"
+            src={url}
+            alt={`${name}의 프로필 이미지`}
+          />
+        ) : (
+          getFirstLetter(name)
+        ))}
     </div>
   );
 };
