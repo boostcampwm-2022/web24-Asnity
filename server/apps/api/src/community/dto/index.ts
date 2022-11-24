d5d8cd70339c5a94fe012668f5bbd5d06952b63b
NaayoungKwon <kny8092@gmail.com>
2022-11-25 01:31:07 +0900

    refactor: 파일 이름 오타 수정

--- server/apps/api/src/community/dto/index.ts
+++ server/apps/api/src/community/dto/index.ts
@@ -1,4 +1,4 @@
-export * from './append-particitants-to-community.dto';
+export * from './append-participants-to-community.dto';
 export * from './create-community.dto';
 export * from './modify-community.dto';
 export * from './delete-community.dto';
