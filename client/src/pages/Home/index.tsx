2bae95fa6c7088e81a30a1a8d11c3d95b58134b8
Cola <bcad1591@gmail.com>
2022-11-24 21:07:47 +0900

    refactor: 루트에 wrapper 적용

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -5,8 +5,8 @@ import { Outlet } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div className="flex flex-1 h-screen">
-      <Gnb />
+    <div className="wrapper flex flex-1">
+      <Gnb className="flex flex-col min-w-[80px] w-[80px] h-full bg-background border-r border-line" />
       <Sidebar />
       <Outlet />
     </div>
