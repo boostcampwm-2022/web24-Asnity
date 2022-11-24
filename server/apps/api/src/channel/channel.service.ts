d4f663f520c24d248c58a2a960afbadc8dc189b9
soomanbaek <qortnaks311@gmail.com>
2022-11-24 17:34:21 +0900

    feat: 커뮤니티 목록에 채널 업데이트 및 주석 추가

--- server/apps/api/src/channel/channel.service.ts
+++ server/apps/api/src/channel/channel.service.ts
@@ -11,7 +11,9 @@ export class ChannelService {
     private readonly communityRepository: CommunityRepository,
     private readonly userRepository: UserRepository,
   ) {}
+
   async createChannel(createChannelDto: CreateChannelDto) {
+    // 자신이 속한 커뮤니티 찾기
     const community = await this.communityRepository.findById(createChannelDto.communityId);
     if (community === null) {
       throw new BadRequestException('커뮤니티가 존재하지 않음');
@@ -19,12 +21,19 @@ export class ChannelService {
     if (createChannelDto.name in community.channels) {
       throw new BadRequestException('중복된 이름의 채널이 같은 커뮤니티에 있습니다.');
     }
+
+    // 채널 생성
     const channel = await this.channelRepository.create({
       ...createChannelDto,
       users: [createChannelDto.managerId],
     });
     const newChannel = {};
     newChannel[`communities.${community._id.toString()}.channels.${channel._id}`] = new Date();
+
+    // 커뮤니티 목록에 채널 업데이트
+    await this.communityRepository.addArrAtArr({ id: community.id }, 'channels', [channel._id]);
+
+    // 유저 목록에 자신이 속한 채널 업데이트
     await this.userRepository.updateObject({ _id: createChannelDto.managerId }, newChannel);
   }
 }
