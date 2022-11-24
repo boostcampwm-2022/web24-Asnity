8779ff80e769e7d51c9e5079d4bf01c0efe698e1
leegwae <leegwae@gmail.com>
2022-11-24 11:49:00 +0900

    feat: `useUsersQuery` 작성 #67

---
+++ client/src/hooks/useUsersQuery.ts
@@ -0,0 +1,34 @@
+import type { GetUsersResponse, GetUsersResult } from '@apis/user';
+
+import { GetUsers } from '@apis/user';
+import { useQuery } from '@tanstack/react-query';
+
+import queryKeyCreator from '@/queryKeyCreator';
+
+type UsersQueryData = {
+  statusCode: number;
+} & GetUsersResult;
+
+const useUserSearchQuery = (
+  filter: string,
+  options?: { suspense?: boolean; enabled?: boolean; refetchOnMount?: boolean },
+) => {
+  const key = queryKeyCreator.userSearch(filter);
+  const query = useQuery<GetUsersResponse, unknown, UsersQueryData>(
+    key,
+    () => GetUsers(filter),
+    {
+      ...options,
+      select: (data) => {
+        const { statusCode, result } = data;
+        const { users } = result;
+
+        return { statusCode, ...result, users };
+      },
+    },
+  );
+
+  return query;
+};
+
+export default useUserSearchQuery;
