571ac5f32f1e33adacd029b2fe3f729c004c4979
leegwae <leegwae@gmail.com>
2022-11-24 15:43:47 +0900

    design: `SearchInput` 높이 `40px`로 조정 #118

--- client/src/components/SearchInput/index.tsx
+++ client/src/components/SearchInput/index.tsx
@@ -6,13 +6,14 @@ import React from 'react';
 type SearchInputProps = InputHTMLAttributes<HTMLInputElement>;
 
 const SearchInput: React.FC<SearchInputProps> = ({
+  className,
   value,
   onChange,
   placeholder,
   ...props
 }) => {
   return (
-    <div className="relative">
+    <div className={`relative h-[40px] ${className}`}>
       <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
         <span className="sr-only">검색</span>
         <MagnifyingGlassIcon className="w-5 h-5 fill-label" />
@@ -21,7 +22,7 @@ const SearchInput: React.FC<SearchInputProps> = ({
         {...props}
         type="text"
         id="input-group-1"
-        className="bg-inputBackground border border-line text-s16 text-label rounded-2xl min-w-full pl-10 p-2.5 focus:outline-line focus:bg-offWhite focus:font-bold"
+        className="h-full bg-inputBackground border border-line text-s14 text-label rounded-2xl min-w-full pl-10 focus:outline-line focus:bg-offWhite focus:font-bold"
         placeholder={placeholder}
         value={value}
         onChange={onChange}
