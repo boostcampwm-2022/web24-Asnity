7152d977de6683bb6d4582e8994c498434fc035c
soomanbaek <qortnaks311@gmail.com>
2022-11-26 02:34:24 +0900

    refactor: addChannelToUserForm 수정
    - es6 최신 문법으로 수정

--- server/utils/addObjectForm.ts
+++ server/utils/addObjectForm.ts
@@ -1,6 +1,5 @@
 export function addChannelToUserForm(communityId, channelId) {
-  const newChannel = {};
-  newChannel[`communities.${communityId.toString()}.channels.${channelId.toString()}`] = new Date();
-
-  return newChannel;
+  return {
+    [`communities.${communityId.toString()}.channels.${channelId.toString()}`]: new Date(),
+  };
 }
