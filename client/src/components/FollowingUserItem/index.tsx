d26b86d3f0c538d149f0dca87f2093fcf53880b5
leegwae <leegwae@gmail.com>
2022-11-25 19:42:27 +0900

    refactor: 라우팅 목적으로 사용하는 `button`을 `Link`로 변경 #125

--- client/src/components/FollowingUserItem/index.tsx
+++ client/src/components/FollowingUserItem/index.tsx
@@ -6,34 +6,29 @@ import {
   ChatBubbleLeftIcon,
 } from '@heroicons/react/20/solid';
 import useFollowingMutation from '@hooks/useFollowingMutation';
-import React, { memo } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 interface Props {
   user: User;
 }
 
 const FollowingUserItem: React.FC<Props> = ({ user }) => {
-  const navigate = useNavigate();
   const followingMutation = useFollowingMutation(user._id);
   const { mutate: updateFollowing } = followingMutation;
-
-  const handleChatButtonClick = () => {
-    navigate(`/dms/${user._id}`);
-  };
 
   return (
     <UserItem
       user={user}
       right={
         <div className="flex gap-4">
-          <button
+          <Link
+            to={`/dms/${user._id}`}
             className="p-2 rounded-full border border-line"
-            onClick={handleChatButtonClick}
           >
             <span className="sr-only">다이렉트 메시지</span>
             <ChatBubbleLeftIcon className="w-6 h-6 fill-indigo" />
-          </button>
+          </Link>
           <button
             className="p-2 rounded-full border border-line"
             onClick={() => updateFollowing(user)}
