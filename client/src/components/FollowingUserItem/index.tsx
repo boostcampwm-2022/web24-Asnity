bd50d2aea834c4fdb8f67aaaad044be77b5d4eb6
Cola <bcad1591@gmail.com>
2022-11-25 20:13:18 +0900

    fix: import memo 추가

--- client/src/components/FollowingUserItem/index.tsx
+++ client/src/components/FollowingUserItem/index.tsx
@@ -6,7 +6,7 @@ import {
   ChatBubbleLeftIcon,
 } from '@heroicons/react/20/solid';
 import useFollowingMutation from '@hooks/useFollowingMutation';
-import React from 'react';
+import React, { memo } from 'react';
 import { Link } from 'react-router-dom';
 
 interface Props {
