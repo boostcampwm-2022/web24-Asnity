8502e4cac87804e13d7788961fedcd106fa565cc
Cola <bcad1591@gmail.com>
2022-11-25 19:48:48 +0900

    feat: CreateCommunityModal 추가
    아직 세부적인 구현은 X

---
+++ client/src/components/Modals/CreateCommunityModal/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactModal from 'react-modal';
+
+interface Props {}
+
+const CreateCommunityModal: React.FC<Props> = () => {
+  return <ReactModal isOpen>커뮤니티 모달</ReactModal>;
+};
+
+export default CreateCommunityModal;
