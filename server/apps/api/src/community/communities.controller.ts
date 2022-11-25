b1838eee52039ea9713f0555a784437ec18f25c7
NaayoungKwon <kny8092@gmail.com>
2022-11-26 01:53:47 +0900

    fix: 사용자의 커뮤니티들의 정보를 받아오는 API 복수형 변경으로 controller 분리

---
+++ server/apps/api/src/community/communities.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Get, Inject, LoggerService, Req, UseGuards } from '@nestjs/common';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { CommunityService } from '@api/src/community/community.service';
+import { responseForm } from '@utils/responseForm';
+import { JwtAccessGuard } from '@api/src/auth/guard';
+
+@Controller('api/communities')
+export class CommunitiesController {
+  constructor(
+    @Inject(WINSTON_MODULE_NEST_PROVIDER) private readonly logger: LoggerService,
+    private communityService: CommunityService,
+  ) {}
+
+  @Get()
+  @UseGuards(JwtAccessGuard)
+  async getCommunities(@Req() req: any) {
+    try {
+      const result = await this.communityService.getCommunities(req.user._doc);
+      return responseForm(200, result);
+    } catch (error) {
+      this.logger.error(JSON.stringify(error.response));
+      throw error;
+    }
+  }
+}
