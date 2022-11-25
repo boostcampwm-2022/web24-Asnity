384bd15aa229fa8a4306080d0ee834c81f3abe9a
leegwae <leegwae@gmail.com>
2022-11-25 17:52:00 +0900

    feat: `directMessageQueryKey` 작성 #125

--- client/src/queryKeyCreator.ts
+++ client/src/queryKeyCreator.ts
@@ -1,3 +1,9 @@
+const directMessageQueryKey = {
+  all: ['directMessages'] as const,
+  list: () => [...directMessageQueryKey.all] as const,
+  detail: (id: string) => [...directMessageQueryKey.all, id] as const,
+} as const;
+
 const queryKeyCreator = {
   me: () => ['me'] as const,
   signUp: () => ['signUp'] as const,
@@ -6,6 +12,7 @@ const queryKeyCreator = {
   followers: (): [string] => ['followers'],
   reissueToken: () => ['reissueToken'] as const,
   userSearch: (filter: string) => ['userSearch', { filter }],
+  directMessage: directMessageQueryKey,
 } as const;
 
 export default queryKeyCreator;
