a65d18ff41f8f500503ed06c21478de3d8ab5f8d
Cola <bcad1591@gmail.com>
2022-11-27 03:20:07 +0900

    feat: Community invalidateQueries 추가
    추가함에 따라 반환 타입이 달라져서 이 훅을 사용하는 컴포넌트 리팩토링

--- client/src/hooks/community.ts
+++ client/src/hooks/community.ts
@@ -8,14 +8,20 @@ import type { UseMutationOptions } from '@tanstack/react-query';
 import type { AxiosError } from 'axios';
 
 import { createCommunity, getCommunities } from '@apis/community';
-import { useMutation, useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { useCallback } from 'react';
 import queryKeyCreator from 'src/queryKeyCreator';
 
 export const useCommunitiesQuery = () => {
+  const queryClient = useQueryClient();
+
   const key = queryKeyCreator.community.all();
   const query = useQuery<GetCommunitiesResult, AxiosError>(key, getCommunities);
+  const invalidate = useCallback(() => {
+    queryClient.invalidateQueries(key);
+  }, [queryClient, key]);
 
-  return query;
+  return { communitiesQuery: query, invalidateCommunitiesQuery: invalidate };
 };
 
 export const useCreateCommunityMutation = (
--- client/src/layouts/CommunityNav/index.tsx
+++ client/src/layouts/CommunityNav/index.tsx
@@ -4,7 +4,7 @@ import { useParams } from 'react-router-dom';
 
 const CommunityNav = () => {
   const { communityId } = useParams();
-  const communitiesQuery = useCommunitiesQuery();
+  const { communitiesQuery } = useCommunitiesQuery();
   const communitySummary = communitiesQuery.data?.find(
     ({ _id }) => _id === communityId,
   );
