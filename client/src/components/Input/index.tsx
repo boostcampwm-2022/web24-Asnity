4d560b894df16619d2bc9e1302038de86ab1d943
Cola <bcad1591@gmail.com>
2022-11-27 02:36:28 +0900

    design: Input 스타일 업데이트

--- client/src/components/Input/index.tsx
+++ client/src/components/Input/index.tsx
@@ -13,7 +13,7 @@ const Input: React.FC<Props> = forwardRef(
         <input
           {...restProps}
           ref={ref}
-          className={`border w-full h-full text-s14 rounded-xl px-[12px] disabled:bg-line disabled:opacity-30 disabled:border-line`}
+          className={`border w-full h-full text-s14 rounded-xl px-[12px] border-line bg-inputBackground focus:bg-offWhite disabled:bg-line disabled:opacity-30 disabled:border-line`}
         />
       </div>
     );
