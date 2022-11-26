88b1d2f2f1b23b347312cee53eccb651a536ed9e
Cola <bcad1591@gmail.com>
2022-11-27 02:28:01 +0900

    feat: CommunityNav 업데이트
    아이디가 아닌 커뮤니티 이름을 보여주도록 변경

--- client/src/layouts/CommunityNav/index.tsx
+++ client/src/layouts/CommunityNav/index.tsx
@@ -1,13 +1,18 @@
+import { useCommunitiesQuery } from '@hooks/community';
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
 const CommunityNav = () => {
   const { communityId } = useParams();
+  const communitiesQuery = useCommunitiesQuery();
+  const communitySummary = communitiesQuery.data?.find(
+    ({ _id }) => _id === communityId,
+  );
 
   return (
     <nav className="flex flex-col flex-1">
       <header className="flex items-center px-[22px] w-full h-header border-b border-line font-ipSans text-title select-none">
-        {communityId}
+        {communitySummary?.name}
       </header>
     </nav>
   );
