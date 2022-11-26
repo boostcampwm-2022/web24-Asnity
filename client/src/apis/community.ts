4d530ba7f4e189e021d6f9d0f80bb0e012db509c
Cola <bcad1591@gmail.com>
2022-11-26 17:28:14 +0900

    feat: createCommunity api 요청 함수 추가

---
+++ client/src/apis/community.ts
@@ -0,0 +1,39 @@
+import type { SuccessResponse } from '@@types/apis/response';
+import type { User } from '@apis/user';
+
+import { tokenAxios } from '@utils/axios';
+
+export interface CreateCommunityRequest {
+  name: string;
+  description: string;
+  profileUrl?: string;
+}
+
+export interface CreateCommunityResult {
+  name: string;
+  managerId: string;
+  description: string;
+  profileUrl: string;
+  createdAt: string;
+  updatedAt: string;
+  channels: [];
+  users: Array<User['id']>;
+  _id: string;
+  __v: 0;
+}
+
+export type CreateCommunity = (
+  fields: CreateCommunityRequest,
+) => Promise<SuccessResponse<CreateCommunityResult>>;
+
+export const createCommunity: CreateCommunity = ({
+  name,
+  description,
+  profileUrl = '',
+}) => {
+  const endPoint = `/api/community`;
+
+  return tokenAxios
+    .post(endPoint, { name, description, profileUrl })
+    .then((response) => response.data);
+};
