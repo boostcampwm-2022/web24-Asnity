bc81c6b33bf5e67d504cd1c0ba703443da9fc48f
leegwae <leegwae@gmail.com>
2022-11-28 01:26:01 +0900

    fix: `ChannelMetadata`에서 커뮤니티가 아닌 채널의 프로필 사진을 출력하도록 수정 #156

--- client/src/pages/Channel/index.tsx
+++ client/src/pages/Channel/index.tsx
@@ -1,27 +1,23 @@
-import Avatar from '@components/Avatar';
 import ChannelMetadata from '@components/ChannelMetadata';
 import { useChannelQuery } from '@hooks/channel';
-import { useCommunityQuery } from '@hooks/community';
 import { useUserQuery } from '@hooks/user';
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
 const Channel = () => {
-  const { communityId, roomId } = useParams();
-  const { communityQuery } = useCommunityQuery(communityId as string);
+  const { roomId } = useParams();
   const { channelQuery } = useChannelQuery(roomId as string);
   const { userQuery } = useUserQuery(channelQuery.data?.managerId as string, {
     enabled: !!channelQuery.data?.managerId,
   });
 
-  if (channelQuery.isLoading || communityQuery.isLoading)
-    return <div>loading...</div>;
+  if (channelQuery.isLoading) return <div>loading...</div>;
 
   return (
     <div>
-      {channelQuery.data && userQuery.data && communityQuery.data && (
+      {channelQuery.data && userQuery.data && (
         <ChannelMetadata
-          profileUrl={communityQuery.data.profileUrl}
+          profileUrl={channelQuery.data.profileUrl}
           channelName={channelQuery.data.name}
           isPrivate={channelQuery.data.isPrivate}
           createdAt={channelQuery.data.createdAt}
