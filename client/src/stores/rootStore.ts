a9a813c0d4050c86b285b127ed3d5d6b94ba759a
Cola <bcad1591@gmail.com>
2022-11-27 21:48:11 +0900

    feat: rootStore에 컨텍스트 메뉴 모달 slice 추가

--- client/src/stores/rootStore.ts
+++ client/src/stores/rootStore.ts
@@ -1,14 +1,16 @@
+import type { ContextMenuModalSlice } from '@stores/contextMenuModalSlice';
 import type { CreateCommunityModalSlice } from '@stores/createCommunityModalSlice';
 
+import { contextMenuModalSlice } from '@stores/contextMenuModalSlice';
+import { createCommunityModalSlice } from '@stores/createCommunityModalSlice';
 import store from 'zustand';
 import { devtools } from 'zustand/middleware';
 
-import { createCommunityModalSlice } from './createCommunityModalSlice';
-
-export type Store = CreateCommunityModalSlice;
+export type Store = CreateCommunityModalSlice & ContextMenuModalSlice;
 
 export const useRootStore = store<Store>()(
   devtools((...a) => ({
     ...createCommunityModalSlice(...a),
+    ...contextMenuModalSlice(...a),
   })),
 );
