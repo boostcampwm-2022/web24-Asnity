14d0b913a49d443ef2183fda73ce5f8527ed2673
leegwae <leegwae@gmail.com>
2022-11-28 01:16:00 +0900

    feat: `Channel` 페이지에 `ChannelMetadata` 렌더링 #156

--- client/src/pages/Channel/index.tsx
+++ client/src/pages/Channel/index.tsx
@@ -1,7 +1,35 @@
+import Avatar from '@components/Avatar';
+import ChannelMetadata from '@components/ChannelMetadata';
+import { useChannelQuery } from '@hooks/channel';
+import { useCommunityQuery } from '@hooks/community';
+import { useUserQuery } from '@hooks/user';
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const Channel = () => {
-  return <div>Channel</div>;
+  const { communityId, roomId } = useParams();
+  const { communityQuery } = useCommunityQuery(communityId as string);
+  const { channelQuery } = useChannelQuery(roomId as string);
+  const { userQuery } = useUserQuery(channelQuery.data?.managerId as string, {
+    enabled: !!channelQuery.data?.managerId,
+  });
+
+  if (channelQuery.isLoading || communityQuery.isLoading)
+    return <div>loading...</div>;
+
+  return (
+    <div>
+      {channelQuery.data && userQuery.data && communityQuery.data && (
+        <ChannelMetadata
+          profileUrl={communityQuery.data.profileUrl}
+          channelName={channelQuery.data.name}
+          isPrivate={channelQuery.data.isPrivate}
+          createdAt={channelQuery.data.createdAt}
+          managerName={userQuery.data.nickname}
+        />
+      )}
+    </div>
+  );
 };
 
 export default Channel;
