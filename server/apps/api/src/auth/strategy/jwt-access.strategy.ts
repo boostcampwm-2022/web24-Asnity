0c8891e01ce2498d496413d7421ad459c22c5091
soomanbaek <qortnaks311@gmail.com>
2022-11-27 17:57:35 +0900

    refactor: jwt에서 user_id가 DB의 user 목록에 있는 지 확인하는 로직 제거

--- server/apps/api/src/auth/strategy/jwt-access.strategy.ts
+++ server/apps/api/src/auth/strategy/jwt-access.strategy.ts
@@ -1,12 +1,11 @@
 import { PassportStrategy } from '@nestjs/passport';
-import { UserRepository } from '@repository/user.repository';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
-import { ForbiddenException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class JwtAccessStrategy extends PassportStrategy(Strategy, 'jwt-access-token') {
-  constructor(config: ConfigService, private userRepository: UserRepository) {
+  constructor(config: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: config.get('JWT_SECRET'),
@@ -14,11 +13,6 @@ export class JwtAccessStrategy extends PassportStrategy(Strategy, 'jwt-access-to
   }
 
   async validate(payload: any) {
-    const user = await this.userRepository.findById(payload._id);
-    if (!user) {
-      throw new ForbiddenException('잘못된 요청입니다.');
-    }
-    delete user.password;
-    return { ...user, _id: user._id.toString() };
+    return payload._id;
   }
 }
