0cd01b4b2e12aed82db8e6de6f39042f344b6202
NaayoungKwon <kny8092@gmail.com>
2022-11-28 22:43:45 +0900

    test: socket test 중간 작성

--- server/apps/socket/test/socket.spec.ts
+++ server/apps/socket/test/socket.spec.ts
@@ -1,43 +1,42 @@
-import { createServer } from 'http';
-import { Server } from 'socket.io';
-import Client from 'socket.io-client';
+import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import * as io from 'socket.io-client';
+import { SocketModule } from '../src/socket.module';
 
-describe('[Socket]', () => {
-  let io, serverSocket, clientSocket;
+describe('Socket e2e', () => {
+  let app: INestApplication;
 
-  beforeAll((done) => {
-    const httpServer = createServer();
-    io = new Server(httpServer);
-    httpServer.listen(() => {
-      const port = httpServer.address().port;
-      clientSocket = new Client(`http://localhost:${port}`);
-      io.on('connection', (socket) => {
-        serverSocket = socket;
-      });
-      clientSocket.on('connect', done);
-    });
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [SocketModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.listen(8080, '127.0.0.1');
   });
 
-  afterAll(() => {
-    io.close();
-    clientSocket.close();
+  afterAll(async () => {
+    await app.close();
   });
 
-  test('should work', (done) => {
-    clientSocket.on('hello', (arg) => {
-      expect(arg).toBe('world');
-      done();
-    });
-    serverSocket.emit('hello', 'world');
-  });
-
-  test('should work (with ack)', (done) => {
-    serverSocket.on('hi', (cb) => {
-      cb('hola');
-    });
-    clientSocket.emit('hi', (arg) => {
-      expect(arg).toBe('hola');
-      done();
-    });
+  it('should call message', async () => {
+    const url = await app.getUrl();
+    const socket = io.connect(url + '/socket/commu-hello');
+    const inputData = { channelId: 'a', user_id: 'ny', message: 'hello', time: Date.now() };
+    expect(inputData).toEqual(inputData);
+    // socket.on('new-message', ({ channelId, user_id, message, time }) => {
+    //   console.log(channelId, user_id);
+    //   expect({ channelId, user_id, message }).toEqual(inputData);
+    // });
+    // socket.emit('new-message', inputData, ({ channelId, user_id, message, time }) => {
+    //   expect({ channelId, user_id, message, time }).toEqual(inputData);
+    // });
+    // await new Promise<void>((resolve) =>
+    //   socket.on('new-message', ({ channelId, user_id, message, time }) => {
+    //     console.log(channelId, user_id);
+    //     expect({ channelId, user_id, message, time }).toEqual(inputData);
+    //     resolve();
+    //   }),
+    // );
   });
 });
