fd06a2d96494e3863f90afd1a7d3dca61c5c8b12
Cola <bcad1591@gmail.com>
2022-11-29 03:15:37 +0900

    test: get community users mock api 추가

--- client/src/mocks/handlers/User.js
+++ client/src/mocks/handlers/User.js
@@ -7,7 +7,9 @@ import {
   createSuccessContext,
 } from '../utils/createContext';
 
-const GetFilteredUsers = rest.get(`${API_URL}/api/users`, (req, res, ctx) => {
+const BASE_URL = `${API_URL}/api`;
+
+const GetFilteredUsers = rest.get(`${BASE_URL}/users`, (req, res, ctx) => {
   const search = req.url.searchParams.get('search').toUpperCase();
 
   return res(
@@ -26,7 +28,7 @@ const GetFilteredUsers = rest.get(`${API_URL}/api/users`, (req, res, ctx) => {
   );
 });
 
-const GetUser = rest.get(`${API_URL}/api/users/:userId`, (req, res, ctx) => {
+const GetUser = rest.get(`${BASE_URL}/users/:userId`, (req, res, ctx) => {
   const ERROR = false;
 
   const errorResponse = res(...createErrorContext(ctx));
@@ -35,4 +37,24 @@ const GetUser = rest.get(`${API_URL}/api/users/:userId`, (req, res, ctx) => {
   return ERROR ? errorResponse : successResponse;
 });
 
-export default [GetFilteredUsers, GetUser];
+const GetCommunityUsers = rest.get(
+  `${BASE_URL}/community/:communityId/participants`,
+  (req, res, ctx) => {
+    const ERROR = false;
+
+    const communityUsers = [...users];
+
+    // users는 사용자 배열이고, communityUsers는 그중 일부만 가져와서
+    // 커뮤니티 초대 모달에서 유저를 검색하면, 일부는 이미 커뮤니티에 속해있는 것을 재현할 수 있음.
+    communityUsers.splice(0, 10);
+
+    const errorResponse = res(...createErrorContext(ctx));
+    const successResponse = res(
+      ...createSuccessContext(ctx, 200, 500, { users: communityUsers }),
+    );
+
+    return ERROR ? errorResponse : successResponse;
+  },
+);
+
+export default [GetFilteredUsers, GetUser, GetCommunityUsers];
