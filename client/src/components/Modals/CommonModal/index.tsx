cc8765c9ac3b945eb9cacf320dc7d340b70c9357
Cola <bcad1591@gmail.com>
2022-11-28 17:57:19 +0900

    feat: CommonModal 추가

---
+++ client/src/components/Modals/CommonModal/index.tsx
@@ -0,0 +1,59 @@
+import type { CSSProperties } from 'react';
+
+import { useRootStore } from '@stores/rootStore';
+import React, { useMemo } from 'react';
+import ReactModal from 'react-modal';
+
+const OverlayBackground = {
+  black: 'rgba(0, 0, 0, 0.5)',
+  white: 'rgba(255, 255, 255, 0.5)',
+  transparent: 'transparent',
+} as const;
+
+const CommonModal: React.FC = () => {
+  const {
+    isOpen,
+    content: Content,
+    overlayBackground,
+    onCancel,
+    transform,
+    x = 1,
+    y = 1,
+  } = useRootStore((state) => state.commonModal);
+  const closeCommonModal = useRootStore((state) => state.closeCommonModal);
+
+  const overlayStyle: CSSProperties = useMemo(
+    () => ({
+      background: OverlayBackground[overlayBackground],
+    }),
+    [overlayBackground],
+  );
+
+  const contentStyle: CSSProperties = useMemo(
+    () => ({
+      width: 'max-content',
+      height: 'max-content',
+      padding: 0,
+      border: 0,
+      top: y,
+      left: x,
+      transform,
+    }),
+    [x, y, transform],
+  );
+
+  return (
+    <ReactModal
+      isOpen={isOpen}
+      style={{
+        overlay: overlayStyle,
+        content: contentStyle,
+      }}
+      onRequestClose={onCancel ?? closeCommonModal}
+    >
+      {Content}
+    </ReactModal>
+  );
+};
+
+export default CommonModal;
