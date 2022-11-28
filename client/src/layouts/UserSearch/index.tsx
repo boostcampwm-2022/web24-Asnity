c90fb24489e3209b1c86aa7b99614eca985d00c7
Cola <bcad1591@gmail.com>
2022-11-28 20:23:18 +0900

    refactor: UserSearch 리팩토링
    useForm활용, Variant 컴포넌트 받아서 다양한 포맷으로 렌더링 가능

--- client/src/layouts/UserSearch/index.tsx
+++ client/src/layouts/UserSearch/index.tsx
@@ -1,37 +1,42 @@
-import type { FormEvent } from 'react';
+import type { User } from '@apis/user';
+import type { FC } from 'react';
 
-import FollowerUserItem from '@components/FollowerUserItem';
 import SearchInput from '@components/SearchInput';
-import UserList from '@components/UserList';
 import useUsersQuery from '@hooks/useUsersQuery';
 import React, { useState } from 'react';
-import Scrollbars from 'react-custom-scrollbars-2';
+import { useForm } from 'react-hook-form';
 
 import Button from '@/components/Button';
 
-// TODO: `handleKeyDown` 이벤트 핸들러 네이밍 명확하게 지어야함
-const UserSearch = () => {
+interface UserSearchInput {
+  filter: string;
+}
+
+interface Props {
+  Variant: FC<{ users?: User[] }>;
+}
+
+const UserSearch: React.FC<Props> = ({ Variant }) => {
+  const { register, handleSubmit } = useForm<UserSearchInput>();
+
   const [submittedFilter, setSubmittedFilter] = useState('');
   const usersQuery = useUsersQuery(submittedFilter, {
     enabled: !!submittedFilter,
   });
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const filter =
-      (new FormData(e.currentTarget).get('user-search') as string) ?? '';
-
-    if (filter.length === 0) return;
-
-    setSubmittedFilter(filter);
+  const handleSubmitUserSearchForm = (data: UserSearchInput) => {
+    setSubmittedFilter(data.filter);
   };
 
   return (
     <div className="flex flex-col h-full">
       <div className="w-full p-8">
-        <form onSubmit={handleSubmit} className="flex gap-2 items-center">
+        <form
+          onSubmit={handleSubmit(handleSubmitUserSearchForm)}
+          className="flex gap-2 items-center"
+        >
           <SearchInput
-            name="user-search"
+            {...register('filter')}
             placeholder="검색하기"
             className="flex-1"
           />
@@ -40,19 +45,7 @@ const UserSearch = () => {
           </Button>
         </form>
       </div>
-      <Scrollbars>
-        {usersQuery.data?.users.length ? (
-          <UserList>
-            {usersQuery.data.users.map((user) => (
-              <FollowerUserItem key={user._id} user={user} />
-            ))}
-          </UserList>
-        ) : (
-          <div className="flex justify-center items-center">
-            검색된 사용자가 없습니다
-          </div>
-        )}
-      </Scrollbars>
+      <Variant users={usersQuery?.data} />
     </div>
   );
 };
