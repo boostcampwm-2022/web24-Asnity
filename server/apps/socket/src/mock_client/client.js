bf7ac4ece19977e4803a2de7444ad44ef7e99c6d
NaayoungKwon <kny8092@gmail.com>
2022-11-28 17:53:41 +0900

    feat: client side 예시 작성 #175

--- server/apps/socket/src/mock_client/client.js
+++ server/apps/socket/src/mock_client/client.js
@@ -1,12 +1,20 @@
 import { io } from 'socket.io-client';
 
 const port = 8081;
+// 형식은 'commu-{community id}'
 const helloSocket = io(`http://localhost:${port}/socket/commu-hello`);
-// const worldSocket = io(`http://localhost:${port}/socket/commu-world`);
-console.log(helloSocket.connected);
+// const worldSocket = io(`http://localhost:${port}/socket/commu-world`); // 다른 namesapce
+
+// console.log(helloSocket.connected); // 연결되었는지 true, false로 나옴
+
+// 처음 연결 후 channel 배열을 전달해야함
 helloSocket.emit('join', { channels: ['a', 'b', 'c'] });
 // worldSocket.emit('join', { channels: ['x', 'y', 'z'] });
+
+// message listen
 helloSocket.on('new-message', ({ channelId, message }) => {
   console.log(`new message from server : ${channelId} ${message}`);
 });
+
+// message 전송
 helloSocket.emit('new-message', { channelId: 'a', message: 'hi i am the first client~' });
