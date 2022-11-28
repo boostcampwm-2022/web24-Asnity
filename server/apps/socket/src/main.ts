6111ab515242aa36d81e5aabbd781030b070d3fc
NaayoungKwon <kny8092@gmail.com>
2022-11-28 16:35:32 +0900

    feat: socket namespace 별 연결 #175

--- server/apps/socket/src/main.ts
+++ server/apps/socket/src/main.ts
@@ -3,6 +3,6 @@ import { SocketModule } from './socket.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(SocketModule);
-  await app.listen(3000);
+  await app.listen(8080);
 }
 bootstrap();
--- server/apps/socket/src/socket.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class SocketService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
