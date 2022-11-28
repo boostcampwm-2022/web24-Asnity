8cf8bff6a263713b29854837b668302058d985a6
Cola <bcad1591@gmail.com>
2022-11-28 15:48:24 +0900

    design: 헤더에 letter-spacing 추가

--- client/src/layouts/DmNav/index.tsx
+++ client/src/layouts/DmNav/index.tsx
@@ -8,8 +8,10 @@ const DmNav = () => {
 
   return (
     <nav className="flex flex-col flex-1">
-      <header className="flex items-center px-[22px] w-full h-header border-b border-line font-ipSans text-title select-none">
-        <Link to="/dms">Direct Message</Link>
+      <header className="flex items-center px-[22px] w-full h-header border-b border-line font-ipSans text-title select-none tracking-tighter">
+        <h2>
+          <Link to="/dms">Direct Message</Link>
+        </h2>
       </header>
       {directMessagesQuery.isLoading ? (
         <div>loading...</div>
