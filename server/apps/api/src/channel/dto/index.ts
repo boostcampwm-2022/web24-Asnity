41fc8caf4be0f0d83a1fca4c7538c6efc3b7827b
soomanbaek <qortnaks311@gmail.com>
2022-11-28 16:08:10 +0900

    feat: 채널 초대 API 구현
    (cherry picked from commit 4bd4a821db29f60999d3a9edce62fe8f156b30d9)

--- server/apps/api/src/channel/dto/index.ts
+++ server/apps/api/src/channel/dto/index.ts
@@ -2,3 +2,4 @@ export * from './create-channel.dto';
 export * from './modify-channel.dto';
 export * from './exit-channel.dto';
 export * from './delete-channel.dto';
+export * from './invite-channel.dto';
