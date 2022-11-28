b0c9035564f8d6a6cfee8d9c261a75a174fa7477
NaayoungKwon <kny8092@gmail.com>
2022-11-28 17:48:14 +0900

    feat: socket channel 별 join #175

--- server/apps/socket/src/socket.gateway.ts
+++ server/apps/socket/src/socket.gateway.ts
@@ -11,8 +11,10 @@ import {
 import { Server, Socket } from 'socket.io';
 import { Logger } from '@nestjs/common';
 
+//TODO : revers proxy : https://socket.io/docs/v4/reverse-proxy/
+
 @WebSocketGateway(8081, {
-  namespace: /\/commu-.+/,
+  namespace: /\/socket\/commu-.+/,
   cors: {
     origin: '*', //['http://localhost:80'],
   },
@@ -21,29 +23,33 @@ export class SocketGateway implements OnGatewayInit, OnGatewayConnection, OnGate
   @WebSocketServer() server: Server;
   private logger: Logger = new Logger('Socket');
 
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
-  }
-
   @SubscribeMessage('join') // socket.on('join', ({}) => {})
   joinEvent(@MessageBody() data: any, @ConnectedSocket() socket: Socket) {
     const namespace = socket.nsp;
     const namespaceName = socket.nsp.name;
+    const channels = data.channels;
+    // channel 별로 join 하는 로직
+    channels.forEach((channelId) => {
+      socket.join(channelId);
+    });
     console.log(
-      `new client join from ${namespaceName} namespace and channel info : ${data.channel}`,
+      `This socket which ns is '${namespaceName}' join rooms : ` + Array.from(socket.rooms),
     );
-    // channel 별로 join 하는 로직 필요
-    // return data;
   }
 
   @SubscribeMessage('new-message') // socket.on('new-message', ({}) => {})
   newMessageEvent(@MessageBody() data: any, @ConnectedSocket() socket: Socket) {
     const namespace = socket.nsp;
     const namespaceName = socket.nsp.name;
-    console.log(`new message from ${namespaceName} namespace and message : ${data.message}`);
-    namespace.emit('new-message', { message: data.message });
-    // return data;
+    const { channelId, message } = data;
+    console.log(
+      `new message : \n\tns : ${namespaceName}\n\tchannel : ${channelId}\n\tmessage : ${message}`,
+    );
+    // namespace.emit('new-message', { message: message }); // namespace에 전체 broad casting
+    namespace.to(channelId).emit('new-message', {
+      channelId,
+      message,
+    });
   }
 
   afterInit(server: Server) {
@@ -53,11 +59,11 @@ export class SocketGateway implements OnGatewayInit, OnGatewayConnection, OnGate
 
   handleDisconnect(socket: Socket) {
     // client의 disconnect event
-    // this.logger.log(`Client Disconnected : ${client.id}`);
+    this.logger.log(`Client Disconnected : [NS] '${socket.nsp.name}', [ID] ${socket.id}`);
   }
 
   handleConnection(socket: Socket, ...args: any[]) {
     // client의 connect event
-    this.logger.log(`Client Connected : ${socket.id} ns : ${socket.nsp.name}`);
+    this.logger.log(`Client Connected : [NS] '${socket.nsp.name}', [ID] ${socket.id}`);
   }
 }
