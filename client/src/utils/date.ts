60743eee65d1072706ccee68d697703976592741
leegwae <leegwae@gmail.com>
2022-11-28 20:17:03 +0900

    feat: `formatDate`에 `options` 파라미터 추가

--- client/src/utils/date.ts
+++ client/src/utils/date.ts
@@ -1,6 +1,10 @@
-export const formatDate = (str: string) =>
+export const formatDate = (
+  str: string,
+  options: Intl.DateTimeFormatOptions = {},
+) =>
   new Date(str).toLocaleDateString('ko', {
     year: 'numeric',
     month: 'short',
     day: 'numeric',
+    ...options,
   });
