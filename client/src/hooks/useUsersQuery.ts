1684acf60bd974c9d1df3fa1c6c7065dbee80fd5
Cola <bcad1591@gmail.com>
2022-11-28 20:22:35 +0900

    refactor: useUserSearchQuery 리팩토링
    select의 반환 타입 변경, UsersQueryData 인터페이스 제거

--- client/src/hooks/useUsersQuery.ts
+++ client/src/hooks/useUsersQuery.ts
@@ -1,29 +1,22 @@
-import type { GetUsersResponse, GetUsersResult } from '@apis/user';
+import type { GetUsersResponse, User } from '@apis/user';
 
 import { GetUsers } from '@apis/user';
 import { useQuery } from '@tanstack/react-query';
 
 import queryKeyCreator from '@/queryKeyCreator';
 
-type UsersQueryData = {
-  statusCode: number;
-} & GetUsersResult;
-
 const useUserSearchQuery = (
   filter: string,
   options?: { suspense?: boolean; enabled?: boolean },
 ) => {
   const key = queryKeyCreator.userSearch(filter);
-  const query = useQuery<GetUsersResponse, unknown, UsersQueryData>(
+  const query = useQuery<GetUsersResponse, unknown, User[]>(
     key,
     () => GetUsers({ search: filter }),
     {
       ...options,
       select: (data) => {
-        const { statusCode, result } = data;
-        const { users } = result;
-
-        return { statusCode, ...result, users };
+        return data.result.users;
       },
     },
   );
