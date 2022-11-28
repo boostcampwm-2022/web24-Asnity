494ff201a42167aa64ccb4fe3ee984620ecb4752
Cola <bcad1591@gmail.com>
2022-11-29 03:11:30 +0900

    feat: get community users api 추가
    특정 커뮤니티에 속한 유저 정보 얻어오기

--- client/src/hooks/user.ts
+++ client/src/hooks/user.ts
@@ -1,4 +1,7 @@
-import { getUser } from '@apis/user';
+import type { User } from '@apis/user';
+import type { AxiosError } from 'axios';
+
+import { getCommunityUsers, getUser } from '@apis/user';
 import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { useCallback } from 'react';
 
@@ -20,4 +23,24 @@ export const useUserQuery = (
   );
 
   return { userQuery: query, invalidateUserQuery: invalidate };
+};
+
+export const useCommunityUsersQuery = (communityId: string) => {
+  const key = queryKeyCreator.user.communityUsers(communityId);
+
+  const query = useQuery<User[], AxiosError>(key, () =>
+    getCommunityUsers(communityId),
+  );
+
+  return { communityUsersQuery: query };
+};
+
+export const useInvalidateCommunityUsersQuery = (communityId: string) => {
+  const queryClient = useQueryClient();
+  const key = queryKeyCreator.user.communityUsers(communityId);
+
+  const invalidateCommunityUsersQuery = () =>
+    queryClient.invalidateQueries(key);
+
+  return { invalidateCommunityUsersQuery };
 };
