2329cc21dcebf95e85873bd0447f365d7ba96d4e
Cola <bcad1591@gmail.com>
2022-11-28 20:18:08 +0900

    refactor: SearchInput 컴포넌트 forwardRef래핑

--- client/src/components/SearchInput/index.tsx
+++ client/src/components/SearchInput/index.tsx
@@ -1,34 +1,33 @@
-import type { InputHTMLAttributes } from 'react';
+import type { ComponentPropsWithRef } from 'react';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/20/solid';
-import React from 'react';
+import React, { forwardRef } from 'react';
 
-type SearchInputProps = InputHTMLAttributes<HTMLInputElement>;
+export interface Props extends ComponentPropsWithRef<'input'> {}
 
-const SearchInput: React.FC<SearchInputProps> = ({
-  className,
-  value,
-  onChange,
-  placeholder,
-  ...props
-}) => {
-  return (
-    <div className={`relative h-[40px] ${className}`}>
-      <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-        <span className="sr-only">검색</span>
-        <MagnifyingGlassIcon className="w-5 h-5 fill-label" />
+const SearchInput: React.FC<Props> = forwardRef(
+  ({ className, value, onChange, placeholder, ...restProps }, ref) => {
+    return (
+      <div className={`relative h-[40px] ${className}`}>
+        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+          <span className="sr-only">검색</span>
+          <MagnifyingGlassIcon className="w-5 h-5 fill-label" />
+        </div>
+        <input
+          {...restProps}
+          ref={ref}
+          type="text"
+          id="input-group-1"
+          className="h-full bg-inputBackground border border-line text-s14 text-label rounded-2xl min-w-full pl-10 focus:outline-line focus:bg-offWhite focus:font-bold"
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+        />
       </div>
-      <input
-        {...props}
-        type="text"
-        id="input-group-1"
-        className="h-full bg-inputBackground border border-line text-s14 text-label rounded-2xl min-w-full pl-10 focus:outline-line focus:bg-offWhite focus:font-bold"
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-      />
-    </div>
-  );
-};
+    );
+  },
+);
+
+SearchInput.displayName = 'SearchInput';
 
 export default SearchInput;
