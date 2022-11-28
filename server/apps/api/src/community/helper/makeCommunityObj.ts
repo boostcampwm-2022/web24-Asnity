7a0c38e4990a3784177ccd41d8ebd83b5fccc92b
NaayoungKwon <kny8092@gmail.com>
2022-11-29 02:04:43 +0900

    feat: 커뮤니티 사용자 추가시 public 채널 입장 #95

--- server/apps/api/src/community/helper/makeCommunityObj.ts
+++ server/apps/api/src/community/helper/makeCommunityObj.ts
@@ -1,8 +1,8 @@
-export const makeCommunityObj = (community_id: string) => {
+export const makeCommunityObj = (community_id: string, channels: any) => {
   const newCommunity = {};
   newCommunity[`communities.${community_id}`] = {
     _id: community_id,
-    channels: {},
+    channels,
   };
   return newCommunity;
 };
