5d1cdfadd34a656b048f2e3289c0f67347c4a1af
Cola <bcad1591@gmail.com>
2022-11-28 17:16:10 +0900

    feat: commonModalSlice 추가

--- client/src/stores/rootStore.ts
+++ client/src/stores/rootStore.ts
@@ -1,8 +1,10 @@
 import type { AlertModalSlice } from '@stores/alertModalSlice';
+import type { CommonModalSlice } from '@stores/commonModalSlice';
 import type { ContextMenuModalSlice } from '@stores/contextMenuModalSlice';
 import type { CreateCommunityModalSlice } from '@stores/createCommunityModalSlice';
 
 import { alertModalSlice } from '@stores/alertModalSlice';
+import { commonModalSlice } from '@stores/commonModalSlice';
 import { contextMenuModalSlice } from '@stores/contextMenuModalSlice';
 import { createCommunityModalSlice } from '@stores/createCommunityModalSlice';
 import store from 'zustand';
@@ -10,12 +12,14 @@ import { devtools } from 'zustand/middleware';
 
 export type Store = CreateCommunityModalSlice &
   ContextMenuModalSlice &
-  AlertModalSlice;
+  AlertModalSlice &
+  CommonModalSlice;
 
 export const useRootStore = store<Store>()(
   devtools((...a) => ({
     ...createCommunityModalSlice(...a),
     ...contextMenuModalSlice(...a),
     ...alertModalSlice(...a),
+    ...commonModalSlice(...a),
   })),
 );
