36a317e6e88b47c9cc0f3df7163a4c85e743fe8a
Cola <bcad1591@gmail.com>
2022-11-28 14:55:40 +0900

    test: GetCommunities Mock Data 업데이트

--- client/src/mocks/data/communities.js
+++ client/src/mocks/data/communities.js
@@ -2,12 +2,26 @@ import { faker } from '@faker-js/faker';
 
 import { chancify } from '../utils/rand';
 
+export const createMockChannel = () => ({
+  _id: faker.datatype.uuid(),
+  managerId: faker.datatype.uuid(),
+  name: faker.name.jobType(),
+  isPrivate: chancify(() => true, 50, false),
+  profileUrl: chancify(() => faker.image.avatar(), 50),
+  description: faker.lorem.sentence(1),
+  lastRead: chancify(() => true, 50, false),
+  type: 'Channel',
+});
+
+const channels = [...Array(10)].map(createMockChannel);
+
 export const createMockCommunities = () => ({
   _id: faker.datatype.uuid(),
   name: faker.name.jobType(),
   managerId: faker.datatype.uuid(),
   profileUrl: chancify(() => faker.image.avatar(), 50),
   description: faker.lorem.sentence(1),
+  channels,
 });
 
 export const communities = [...Array(15)].map(createMockCommunities);
