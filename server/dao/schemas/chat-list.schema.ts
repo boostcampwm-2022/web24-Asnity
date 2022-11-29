5e6b55bf0d5f63b1e92e6545da4405a75e320b4c
soomanbaek <qortnaks311@gmail.com>
2022-11-29 21:43:00 +0900

    feat: chat-list 스키마 수정
    - chat를 메타데이터로 활용

--- server/dao/schemas/chat-list.schema.ts
+++ server/dao/schemas/chat-list.schema.ts
@@ -1,18 +1,29 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, { Document } from 'mongoose';
-import { Chat } from './chat.schema';
 import { ArrayMaxSize } from 'class-validator';
 
 export type ChatListDocument = ChatList & Document;
 
 @Schema()
 export class ChatList {
-  @Prop()
-  name: string;
-
-  @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Chat' }] })
+  @Prop({
+    type: [
+      {
+        type: Map,
+        of: new mongoose.Schema({
+          id: { type: String },
+          type: { type: String },
+          content: { type: String },
+          senderId: { type: String },
+          createdAt: { type: mongoose.Schema.Types.Date, default: new Date() },
+          updatedAt: { type: mongoose.Schema.Types.Date, default: new Date() },
+          deletedAt: { type: mongoose.Schema.Types.Date },
+        }),
+      },
+    ],
+  })
   @ArrayMaxSize(100)
-  chat: Chat[];
+  chat;
 
   @Prop({ default: new Date(), type: mongoose.Schema.Types.Date })
   firstChatTime: Date;
