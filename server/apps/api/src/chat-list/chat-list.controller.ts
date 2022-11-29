93b127fb1d4fc69b9ea41a87fb0f045c0a74e485
soomanbaek <qortnaks311@gmail.com>
2022-11-29 21:43:44 +0900

    feat: 채팅 저장 로직 구현

--- server/apps/api/src/chat-list/chat-list.controller.ts
+++ server/apps/api/src/chat-list/chat-list.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Inject, LoggerService, Post, UseGuards } from '@nestjs/common';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { ChatListService } from '@chat-list/chat-list.service';
+import { JwtAccessGuard } from '@auth/guard';
+import { RestoreMessageDto } from '@chat-list/dto';
+import { responseForm } from '@utils/responseForm';
 
-@Controller('chat-list')
-export class ChatListController {}
+@Controller('api/chat')
+export class ChatListController {
+  constructor(
+    @Inject(WINSTON_MODULE_NEST_PROVIDER) private readonly logger: LoggerService,
+    private chatListService: ChatListService,
+  ) {}
+
+  @Post('/')
+  @UseGuards(JwtAccessGuard)
+  async restoreMessage(@Body() restoreMessageDto: RestoreMessageDto) {
+    try {
+      await this.chatListService.restoreMessage(restoreMessageDto);
+      return responseForm(200, { message: '채팅 저장 성공!' });
+    } catch (error) {
+      this.logger.error(JSON.stringify(error.response));
+      throw error;
+    }
+  }
+}
