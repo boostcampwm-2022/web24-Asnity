0daff2aa155bd88ef272beb5a197c173474a2ca8
soomanbaek <qortnaks311@gmail.com>
2022-11-29 21:44:01 +0900

    feat: 채팅 저장 로직 구현

--- server/dao/repository/chat-list.respository.ts
+++ server/dao/repository/chat-list.respository.ts
@@ -6,4 +6,16 @@ import { InjectModel } from '@nestjs/mongoose';
 @Injectable()
 export class ChatListRespository {
   constructor(@InjectModel(ChatList.name) private chatListModel: Model<ChatListDocument>) {}
+
+  async create(chat) {
+    return await this.chatListModel.create(chat);
+  }
+  async findById(_id: string) {
+    return await this.chatListModel.findById(_id);
+  }
+  async addArrAtArr(filter, attribute, appendArr) {
+    const addArr = {};
+    addArr[attribute] = { $each: appendArr };
+    return await this.chatListModel.findByIdAndUpdate(filter, { $addToSet: addArr }, { new: true });
+  }
 }
