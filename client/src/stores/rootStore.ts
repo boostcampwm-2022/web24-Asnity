4d58378f7f1f4a0d6fcc6c958b42888892c8803e
Cola <bcad1591@gmail.com>
2022-11-29 15:59:50 +0900

    refactor: createCommunityModalSlice 삭제

--- client/src/stores/rootStore.ts
+++ client/src/stores/rootStore.ts
@@ -1,23 +1,17 @@
 import type { AlertModalSlice } from '@stores/alertModalSlice';
 import type { CommonModalSlice } from '@stores/commonModalSlice';
 import type { ContextMenuModalSlice } from '@stores/contextMenuModalSlice';
-import type { CreateCommunityModalSlice } from '@stores/createCommunityModalSlice';
 
 import { alertModalSlice } from '@stores/alertModalSlice';
 import { commonModalSlice } from '@stores/commonModalSlice';
 import { contextMenuModalSlice } from '@stores/contextMenuModalSlice';
-import { createCommunityModalSlice } from '@stores/createCommunityModalSlice';
 import store from 'zustand';
 import { devtools } from 'zustand/middleware';
 
-export type Store = CreateCommunityModalSlice &
-  ContextMenuModalSlice &
-  AlertModalSlice &
-  CommonModalSlice;
+export type Store = ContextMenuModalSlice & AlertModalSlice & CommonModalSlice;
 
 export const useRootStore = store<Store>()(
   devtools((...a) => ({
-    ...createCommunityModalSlice(...a),
     ...contextMenuModalSlice(...a),
     ...alertModalSlice(...a),
     ...commonModalSlice(...a),
