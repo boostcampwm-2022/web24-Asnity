5c2c190638a64715f6480662ce0bbfc46a38f40d
soomanbaek <qortnaks311@gmail.com>
2022-11-30 15:18:48 +0900

    refactor: STATUS 상수 변경 (CUSTOM_OFFLINE -> AFK)
    (cherry picked from commit 55143ccd4febfe32f54ffb2ca3cb19a8b901ec4e)

--- server/utils/def.ts
+++ server/utils/def.ts
@@ -1,4 +1,4 @@
-export const STATUS = ['ONLINE', 'OFFLINE', 'CUSTOM_OFFLINE'];
+export const STATUS = ['ONLINE', 'OFFLINE', 'AFK'];
 export const PROVIDER = ['ASNITY', 'GITHUB'];
 export const CHANNEL_TYPE = ['CHANNEL', 'DM'];
 export const CHAT_TYPE = ['TEXT', 'IMAGE'];
