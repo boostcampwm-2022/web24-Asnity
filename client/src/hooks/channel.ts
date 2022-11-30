fb3f243e5b56424aa9ffbb782df78f120aafb91d
Cola <bcad1591@gmail.com>
2022-11-30 17:00:57 +0900

    feat: useCreateChannelMutation, useSetChannelsQuery 추가

--- client/src/hooks/channel.ts
+++ client/src/hooks/channel.ts
@@ -1,8 +1,15 @@
-import type { GetChannelResult } from '@apis/channel';
+import type {
+  CreateChannelRequest,
+  CreateChannelResult,
+  GetChannelResult,
+  JoinedChannel,
+} from '@apis/channel';
+import type { GetCommunitiesResult } from '@apis/community';
+import type { UseMutationOptions } from '@tanstack/react-query';
 import type { AxiosError } from 'axios';
 
-import { getChannel } from '@apis/channel';
-import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { createChannel, getChannel } from '@apis/channel';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { useCallback } from 'react';
 
 import queryKeyCreator from '@/queryKeyCreator';
@@ -20,4 +27,47 @@ export const useChannelQuery = (channelId: string) => {
   );
 
   return { channelQuery: query, invalidateChannelQuery: invalidate };
+};
+
+export const useCreateChannelMutation = (
+  options?: UseMutationOptions<
+    CreateChannelResult,
+    unknown,
+    CreateChannelRequest
+  >,
+) => {
+  const key = queryKeyCreator.channel.createChannel();
+
+  const mutation = useMutation(key, createChannel, { ...options });
+
+  return mutation;
+};
+
+/**
+ * ### invalidate queries 하지 않고, 수동으로 queryClient update 할 때 사용합니다.
+ * - addChannelToCommunity: 커뮤니티 아이디와 추가할 채널을 인자로 전달.
+ */
+export const useSetChannelsQuery = () => {
+  const queryClient = useQueryClient();
+  const key = queryKeyCreator.community.all();
+
+  const addChannelToCommunity = (
+    communityId: string,
+    channel: JoinedChannel,
+  ) => {
+    queryClient.setQueryData<GetCommunitiesResult>(key, (communities) => {
+      const newCommunities = communities?.map((community) => {
+        if (community._id !== communityId) return community;
+
+        return {
+          ...community,
+          channels: [...community.channels, channel],
+        };
+      });
+
+      return newCommunities;
+    });
+  };
+
+  return { addChannelToCommunity };
 };
