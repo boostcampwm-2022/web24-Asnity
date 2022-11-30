48d261f6dc2d7e6361c2c5b8053c20470fedff92
soomanbaek <qortnaks311@gmail.com>
2022-11-30 14:06:03 +0900

    feat: join-channel dto 작성
    - requestUserId : String, IsNotEmpty
    - channel_id: String, IsNotEmpty
    - community_id: String, IsNotEmpty

--- server/apps/api/src/channel/dto/index.ts
+++ server/apps/api/src/channel/dto/index.ts
@@ -4,3 +4,4 @@ export * from './exit-channel.dto';
 export * from './delete-channel.dto';
 export * from './invite-channel.dto';
 export * from './update-last-read.dto';
+export * from './join-channel.dto';
