f1f0bb4b476f1fdd2fc02cab632901815edd68d4
leegwae <leegwae@gmail.com>
2022-12-01 23:09:57 +0900

    feat: 팔로우 기능 추가 #227

--- client/src/components/FollowerUserItem/index.tsx
+++ client/src/components/FollowerUserItem/index.tsx
@@ -3,19 +3,29 @@ import type { FC } from 'react';
 
 import UserItem from '@components/UserItem';
 import { EllipsisHorizontalIcon } from '@heroicons/react/20/solid';
+import useFollowingMutation from '@hooks/useFollowingMutation';
 import React, { memo } from 'react';
 
 interface Props {
   user: User;
 }
 
+// TODO: 팔로잉한 사용자는 팔로우할 수 없도록
+// TODO: 팔로워, 팔로잉 정보 띄워주도록
+// TODO: 사용자 아이디 띄워주도록
 const FollowerUserItem: FC<Props> = ({ user }) => {
+  const followingMutation = useFollowingMutation(user._id);
+
   return (
     <UserItem
       user={user}
       right={
         <div className="flex gap-4">
-          <button className="p-2 rounded-full border border-line">
+          <button
+            className="p-2 rounded-full border border-line"
+            onClick={() => followingMutation.mutate(user)}
+            disabled={followingMutation.isLoading}
+          >
             <span className="sr-only">더보기</span>
             <EllipsisHorizontalIcon className="w-6 h-6 fill-indigo" />
           </button>
