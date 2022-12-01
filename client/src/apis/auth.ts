479ae50252aeed0c103c28d36440784ccbda806f
Cola <bcad1591@gmail.com>
2022-12-01 17:08:43 +0900

    refactor: auth api 사용하는 컴포넌트 리팩토링

--- client/src/apis/auth.ts
+++ client/src/apis/auth.ts
@@ -12,20 +12,18 @@ export interface SignUpResult {
   message: string;
 }
 
-export type SignUp = (
-  fields: SignUpRequest,
-) => Promise<SuccessResponse<SignUpResult>>;
+export type SignUp = (fields: SignUpRequest) => Promise<SignUpResult>;
 
 export const signUp: SignUp = ({ id, nickname, password }) => {
   const endPoint = `/api/user/auth/signup`;
 
   return publicAxios
-    .post(endPoint, { id, nickname, password })
-    .then((response) => response.data);
+    .post<SuccessResponse<SignUpResult>>(endPoint, { id, nickname, password })
+    .then((response) => response.data.result);
 };
 
 export interface SignInRequest {
-  id: string;
+  id: string; // username
   password: string;
 }
 
@@ -34,32 +32,35 @@ export interface SignInResult {
   accessToken: string;
 }
 
-export type SignIn = (
-  fields: SignInRequest,
-) => Promise<SuccessResponse<SignInResult>>;
+export type SignIn = (fields: SignInRequest) => Promise<SignInResult>;
 
 export const signIn: SignIn = ({ id, password }) => {
   const endPoint = `/api/user/auth/signin`;
 
   return publicAxios
-    .post(endPoint, { id, password }, { withCredentials: true })
-    .then((response) => response.data);
+    .post<SuccessResponse<SignInResult>>(
+      endPoint,
+      { id, password },
+      { withCredentials: true },
+    )
+    .then((response) => response.data.result);
 };
-// 액세스 토큰으로 다시 유저 정보 요청해야함
-// _id, id(이메일), nickname, status, profileUrl, description
+// 이후 DM 페이지에서 액세스 토큰으로 다시 유저 정보 요청해야함
 
 export interface ReissueTokenResult {
   accessToken: string;
 }
 
-export type ReissueToken = () => Promise<SuccessResponse<ReissueTokenResult>>;
+export type ReissueToken = () => Promise<ReissueTokenResult>;
 
 export const reissueToken: ReissueToken = () => {
   const endPoint = `/api/user/auth/refresh`;
 
   return publicAxios
-    .post(endPoint, {}, { withCredentials: true })
-    .then((response) => {
-      return response.data;
-    });
+    .post<SuccessResponse<ReissueTokenResult>>(
+      endPoint,
+      {},
+      { withCredentials: true },
+    )
+    .then((response) => response.data.result);
 };
