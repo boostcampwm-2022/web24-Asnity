e2ae442eb0beb02ac475459748fd238478d02a4f
leegwae <leegwae@gmail.com>
2022-12-02 18:46:37 +0900

    refactor: callback ref를 사용하고 화면에 보이면 실행할 콜백을 넘기도록 수정
    - `useIsIntersecting` 훅은 요소가 화면에 보이는지 불 변수를 반환한다.
    - `useSetIntersectingRef`는 callback ref를 반환한다. 파라미터로 요소가 화면에 보일 때 실행할 콜백을 넘긴다.

---
+++ client/src/hooks/useSetIntersectingRef.ts
@@ -0,0 +1,31 @@
+import React, { useState, useRef, useCallback, useEffect } from 'react';
+
+/**
+ *
+ * @param onIntersection 관찰되는 요소가 화면에 보이면 실행되는 콜백 함수
+ * @returns 관찰하려는 요소에 붙이는 callback ref
+ * 반환값을 요소의 `ref` 속성에 붙이면 파라미터에 전달한 콜백이 실행됩니다.
+ */
+const useSetIntersectingRef = (onIntersection: () => void) => {
+  const [isIntersecting, setIsIntersecting] = useState(false);
+  const observerRef = useRef<IntersectionObserver | null>(null);
+
+  useEffect(() => {
+    if (isIntersecting) onIntersection();
+  }, [isIntersecting]);
+
+  const setRef = useCallback((node: HTMLElement | null) => {
+    if (node) {
+      if (!observerRef.current) {
+        observerRef.current = new IntersectionObserver((entries) =>
+          setIsIntersecting(entries.some((entry) => entry.isIntersecting)),
+        );
+      }
+      observerRef.current.observe(node);
+    }
+  }, []);
+
+  return setRef;
+};
+
+export default useSetIntersectingRef;
