f2e5bc7e143119b1cf029f3651c5574bcb5323d2
leegwae <leegwae@gmail.com>
2022-12-03 22:44:16 +0900

    feat: 팔로잉 컨텍스트 메뉴 추가 **안 닫히는 문제 있음**

--- client/src/components/FollowingUserSearchResult/index.tsx
+++ client/src/components/FollowingUserSearchResult/index.tsx
@@ -1,13 +1,12 @@
 import type { User } from '@apis/user';
 import type { FC } from 'react';
 
+import FollowingUserContextMenu from '@components/FollowingUserContextMenu';
 import UserItem from '@components/UserItem';
 import UserList from '@components/UserList';
-import defaultErrorHandler from '@errors/defaultErrorHandler';
 import { EllipsisHorizontalIcon } from '@heroicons/react/20/solid';
 import useFollowersQuery from '@hooks/useFollowersQuery';
-import useFollowingMutation from '@hooks/useFollowingMutation';
-import { useInvalidateFollowingsQuery } from '@hooks/useFollowingsQuery';
+import { useRootStore } from '@stores/rootStore';
 import React from 'react';
 import Scrollbars from 'react-custom-scrollbars-2';
 
@@ -20,14 +19,20 @@ interface Props {
  * @returns 팔로잉 목록을 렌더링하는 컴포넌트
  */
 const FollowingUserSearchResult: FC<Props> = ({ users }) => {
+  const openCommonModal = useRootStore((state) => state.openCommonModal);
+
   const followersQuery = useFollowersQuery();
-  const invalidateFollowingsQuery = useInvalidateFollowingsQuery();
-  const followingMutation = useFollowingMutation({
-    onSuccess: () => {
-      invalidateFollowingsQuery();
-    },
-    onError: (error) => defaultErrorHandler(error),
-  });
+
+  const handleMoreButtonClick: (
+    user: User,
+  ) => React.MouseEventHandler<HTMLButtonElement> = (user) => (e) => {
+    openCommonModal({
+      content: <FollowingUserContextMenu user={user} />,
+      overlayBackground: 'transparent',
+      x: e.clientX,
+      y: e.clientY,
+    });
+  };
 
   if (!users.length) {
     return (
@@ -40,29 +45,32 @@ const FollowingUserSearchResult: FC<Props> = ({ users }) => {
   return (
     <Scrollbars>
       <UserList>
-        {users.map((user) => (
-          <UserItem
-            key={user._id}
-            user={user}
-            left={
-              !!followersQuery.data?.find((f) => f._id === user._id) && (
-                <div className="text-placeholder">팔로워</div>
-              )
-            }
-            right={
-              <div className="flex gap-4">
-                <button
-                  className="p-2 rounded-full border border-line disabled:cursor-not-allowed"
-                  onClick={() => followingMutation.mutate(user._id)}
-                  disabled={followingMutation.isLoading}
-                >
-                  <span className="sr-only">더보기</span>
-                  <EllipsisHorizontalIcon className="w-6 h-6 fill-indigo" />
-                </button>
-              </div>
-            }
-          />
-        ))}
+        {users.map((user) => {
+          const isFollowers = !!followersQuery.data?.find(
+            (f) => f._id === user._id,
+          );
+
+          return (
+            <UserItem
+              key={user._id}
+              user={user}
+              left={
+                isFollowers && <div className="text-placeholder">팔로워</div>
+              }
+              right={
+                <div className="flex gap-4">
+                  <button
+                    className="p-2 rounded-full border border-line disabled:cursor-not-allowed"
+                    onClick={handleMoreButtonClick(user)}
+                  >
+                    <span className="sr-only">더보기</span>
+                    <EllipsisHorizontalIcon className="w-6 h-6 fill-indigo" />
+                  </button>
+                </div>
+              }
+            />
+          );
+        })}
       </UserList>
     </Scrollbars>
   );
