dc96a9c8e5c887d83ce6398b121ca374b7262939
NaayoungKwon <kny8092@gmail.com>
2022-12-05 14:21:24 +0900

    refactor: community controller error handling interceptor로 이동

--- server/apps/api/src/community/communities.controller.ts
+++ server/apps/api/src/community/communities.controller.ts
@@ -20,22 +20,14 @@ import { RequestUserAboutCommunityDto } from '@community/dto/request-user-about-
 
 @Controller('api/communities')
 export class CommunitiesController {
-  constructor(
-    @Inject(WINSTON_MODULE_NEST_PROVIDER) private readonly logger: LoggerService,
-    private communityService: CommunityService,
-  ) {}
+  constructor(private communityService: CommunityService) {}
 
   @Get()
   @UseGuards(JwtAccessGuard)
   async getCommunities(@Req() req: any) {
-    try {
-      const _id = req.user._id;
-      const result = await this.communityService.getCommunities(_id);
-      return responseForm(200, result);
-    } catch (error) {
-      this.logger.error(JSON.stringify(error.response));
-      throw error;
-    }
+    const _id = req.user._id;
+    const result = await this.communityService.getCommunities(_id);
+    return responseForm(200, result);
   }
 
   @Post(':community_id/users')
@@ -45,66 +37,46 @@ export class CommunitiesController {
     @Body() appendUsersToCommunityDto: AppendUsersToCommunityDto,
     @Req() req: any,
   ) {
-    try {
-      const requestUserId = req.user._id;
-      await this.communityService.appendUsersToCommunity({
-        ...appendUsersToCommunityDto,
-        community_id,
-        requestUserId,
-      });
-      return responseForm(200, { message: '커뮤니티 사용자 추가 완료' });
-    } catch (error) {
-      this.logger.error(JSON.stringify(error.response));
-      throw error;
-    }
+    const requestUserId = req.user._id;
+    await this.communityService.appendUsersToCommunity({
+      ...appendUsersToCommunityDto,
+      community_id,
+      requestUserId,
+    });
+    return responseForm(200, { message: '커뮤니티 사용자 추가 완료' });
   }
 
   @Delete(':community_id')
   @UseGuards(JwtAccessGuard)
   async deleteCommunity(@Param('community_id') community_id: string, @Req() req: any) {
-    try {
-      const requestUserId = req.user._id;
-      const deleteCommunityDto: DeleteCommunityDto = { requestUserId, community_id };
-      await this.communityService.deleteCommunity(deleteCommunityDto);
-      return responseForm(200, { message: '커뮤니티 삭제 성공' });
-    } catch (error) {
-      this.logger.error(JSON.stringify(error.response));
-      throw error;
-    }
+    const requestUserId = req.user._id;
+    const deleteCommunityDto: DeleteCommunityDto = { requestUserId, community_id };
+    await this.communityService.deleteCommunity(deleteCommunityDto);
+    return responseForm(200, { message: '커뮤니티 삭제 성공' });
   }
 
   @Get(':community_id/users')
   @UseGuards(JwtAccessGuard)
   async getUsersInCommunity(@Param('community_id') community_id: string, @Req() req: any) {
-    try {
-      const requestUserId = req.user._id;
-      const requestUserAboutCommunityDto: RequestUserAboutCommunityDto = {
-        community_id,
-        requestUserId,
-      };
-      const result = await this.communityService.getUsersInCommunity(requestUserAboutCommunityDto);
-      return responseForm(200, result);
-    } catch (error) {
-      this.logger.error(JSON.stringify(error.response));
-      throw error;
-    }
+    const requestUserId = req.user._id;
+    const requestUserAboutCommunityDto: RequestUserAboutCommunityDto = {
+      community_id,
+      requestUserId,
+    };
+    const result = await this.communityService.getUsersInCommunity(requestUserAboutCommunityDto);
+    return responseForm(200, result);
   }
 
   @Delete(':community_id/me')
   @UseGuards(JwtAccessGuard)
   async exitUserInCommunity(@Param(':community_id') community_id: string, @Req() req: any) {
-    try {
-      const requestUserId = req.user._id;
-      const requestUserAboutCommunityDto: RequestUserAboutCommunityDto = {
-        community_id,
-        requestUserId,
-      };
-      await this.communityService.exitUserInCommunity(requestUserAboutCommunityDto);
-      return responseForm(200, { message: '사용자 커뮤니티 탈퇴 성공' });
-    } catch (error) {
-      this.logger.error(JSON.stringify(error.response));
-      throw error;
-    }
+    const requestUserId = req.user._id;
+    const requestUserAboutCommunityDto: RequestUserAboutCommunityDto = {
+      community_id,
+      requestUserId,
+    };
+    await this.communityService.exitUserInCommunity(requestUserAboutCommunityDto);
+    return responseForm(200, { message: '사용자 커뮤니티 탈퇴 성공' });
   }
 
   @Patch(':community_id/settings')
@@ -114,17 +86,12 @@ export class CommunitiesController {
     @Body() modifyCommunityDto: ModifyCommunityDto,
     @Req() req: any,
   ) {
-    try {
-      const requestUserId = req.user._id;
-      await this.communityService.modifyCommunity({
-        ...modifyCommunityDto,
-        community_id,
-        requestUserId,
-      });
-      return responseForm(200, { message: '커뮤니티 정보 수정 완료' });
-    } catch (error) {
-      this.logger.error(JSON.stringify(error.response));
-      throw error;
-    }
+    const requestUserId = req.user._id;
+    await this.communityService.modifyCommunity({
+      ...modifyCommunityDto,
+      community_id,
+      requestUserId,
+    });
+    return responseForm(200, { message: '커뮤니티 정보 수정 완료' });
   }
 }
--- server/apps/api/src/community/community.controller.ts
+++ server/apps/api/src/community/community.controller.ts
@@ -16,24 +16,16 @@ import { CreateCommunityDto, ModifyCommunityDto } from './dto';
 
 @Controller('api/community')
 export class CommunityController {
-  constructor(
-    @Inject(WINSTON_MODULE_NEST_PROVIDER) private readonly logger: LoggerService,
-    private communityService: CommunityService,
-  ) {}
+  constructor(private communityService: CommunityService) {}
 
   @Post()
   @UseGuards(JwtAccessGuard)
   async crateCommunity(@Body() createCommunityDto: CreateCommunityDto, @Req() req: any) {
-    try {
-      const managerId = req.user._id;
-      const result = await this.communityService.createCommunity({
-        managerId,
-        ...createCommunityDto,
-      });
-      return responseForm(200, result);
-    } catch (error) {
-      this.logger.error(JSON.stringify(error.response));
-      throw error;
-    }
+    const managerId = req.user._id;
+    const result = await this.communityService.createCommunity({
+      managerId,
+      ...createCommunityDto,
+    });
+    return responseForm(200, result);
   }
 }
