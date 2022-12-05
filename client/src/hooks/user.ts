d26525f3e3808f6becc5d63111a4c3f470cde44c
Cola <bcad1591@gmail.com>
2022-12-05 14:34:17 +0900

    feat: useChannelUsersMapQuery 커스텀 훅 추가
    채널의 유저 정보를 { userId: user }로 매핑한 객체를 가져옴.
    useQuery의 select 옵션 활용

--- client/src/hooks/user.ts
+++ client/src/hooks/user.ts
@@ -1,6 +1,8 @@
-import type { User } from '@apis/user';
+import type { Channel } from '@apis/channel';
+import type { User, UserUID } from '@apis/user';
 import type { AxiosError } from 'axios';
 
+import { getChannel } from '@apis/channel';
 import { getCommunityUsers } from '@apis/user';
 import { useQuery, useQueryClient } from '@tanstack/react-query';
 
@@ -24,4 +26,28 @@ export const useInvalidateCommunityUsersQuery = (communityId: string) => {
     queryClient.invalidateQueries(key);
 
   return { invalidateCommunityUsersQuery };
+};
+
+export type UsersMap = Record<UserUID, User>;
+
+export const useChannelUsersMapQuery = (channelId: string) => {
+  const key = queryKeyCreator.user.channelUsers(channelId);
+  const query = useQuery<Channel, AxiosError, UsersMap>(
+    key,
+    () => getChannel(channelId),
+    {
+      select: (data) => {
+        const users = data.users;
+
+        const usersMap = users.reduce((acc, user) => {
+          acc[user._id] = user;
+          return acc;
+        }, {} as UsersMap);
+
+        return usersMap;
+      },
+    },
+  );
+
+  return { channelUsersMapQuery: query };
 };
