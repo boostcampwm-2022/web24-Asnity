e84a9d896d523f8227c9ba47d3e704a48b968797
NaayoungKwon <kny8092@gmail.com>
2022-12-05 14:19:24 +0900

    refactor: channel controller error handling interceptor로 이동
    (cherry picked from commit d768b2f03b396859de17f5265dc239e504bef778)

--- server/apps/api/src/channel/channels.controller.ts
+++ server/apps/api/src/channel/channels.controller.ts
@@ -19,10 +19,7 @@ import { JwtAccessGuard } from '@auth/guard';
 
 @Controller('api/channels')
 export class ChannelsController {
-  constructor(
-    @Inject(WINSTON_MODULE_NEST_PROVIDER) private readonly logger: LoggerService,
-    private channelService: ChannelService,
-  ) {}
+  constructor(private channelService: ChannelService) {}
 
   @Patch(':channel_id/settings')
   @UseGuards(JwtAccessGuard)
@@ -32,63 +29,38 @@ export class ChannelsController {
     @Req() req: any,
   ) {
     const requestUserId = req.user._id;
-    try {
-      await this.channelService.modifyChannel({ ...modifyChannelDto, channel_id, requestUserId });
-      return responseForm(200, { message: '채널 수정 성공!' });
-    } catch (error) {
-      this.logger.error(JSON.stringify(error.response));
-      throw error;
-    }
+    await this.channelService.modifyChannel({ ...modifyChannelDto, channel_id, requestUserId });
+    return responseForm(200, { message: '채널 수정 성공!' });
   }
 
   @Delete(':channel_id/me')
   @UseGuards(JwtAccessGuard)
   async exitChannel(@Param('channel_id') channel_id, @Req() req: any) {
     const requestUserId = req.user._id;
-    try {
-      await this.channelService.exitChannel({ channel_id, requestUserId });
-      return responseForm(200, { message: '채널 퇴장 성공!' });
-    } catch (error) {
-      this.logger.error(JSON.stringify(error.response));
-      throw error;
-    }
+    await this.channelService.exitChannel({ channel_id, requestUserId });
+    return responseForm(200, { message: '채널 퇴장 성공!' });
   }
 
   @Get(':channel_id')
   @UseGuards(JwtAccessGuard)
   async getChannelInfo(@Param('channel_id') channel_id: string) {
-    try {
-      const channelInfo = await this.channelService.getChannelInfo(channel_id);
-      return responseForm(200, channelInfo);
-    } catch (error) {
-      this.logger.error(JSON.stringify(error.response));
-      throw error;
-    }
+    const channelInfo = await this.channelService.getChannelInfo(channel_id);
+    return responseForm(200, channelInfo);
   }
 
   @Delete(':channel_id')
   @UseGuards(JwtAccessGuard)
   async deleteChannel(@Param('channel_id') channel_id, @Req() req: any) {
     const requestUserId = req.user._id;
-    try {
-      await this.channelService.deleteChannel({ channel_id, requestUserId });
-      return responseForm(200, { message: '채널 삭제 성공' });
-    } catch (error) {
-      this.logger.error(JSON.stringify(error.response));
-      throw error;
-    }
+    await this.channelService.deleteChannel({ channel_id, requestUserId });
+    return responseForm(200, { message: '채널 삭제 성공' });
   }
 
   @Post(':channel_id/users')
   @UseGuards(JwtAccessGuard)
   async inviteChannel(@Param('channel_id') channel_id, @Body() inviteChannelDto: InviteChannelDto) {
-    try {
-      await this.channelService.inviteChannel({ ...inviteChannelDto, channel_id });
-      return responseForm(200, { message: '채널 초대 성공' });
-    } catch (error) {
-      this.logger.error(JSON.stringify(error.response));
-      throw error;
-    }
+    await this.channelService.inviteChannel({ ...inviteChannelDto, channel_id });
+    return responseForm(200, { message: '채널 초대 성공' });
   }
 
   @Patch(':channel_id/lastRead')
@@ -99,25 +71,15 @@ export class ChannelsController {
     @Req() req: any,
   ) {
     const requestUserId = req.user._id;
-    try {
-      await this.channelService.updateLastRead({ ...updateLastReadDto, requestUserId, channel_id });
-      return responseForm(200, { message: 'Last Read 업데이트 성공' });
-    } catch (error) {
-      this.logger.error(JSON.stringify(error.response));
-      throw error;
-    }
+    await this.channelService.updateLastRead({ ...updateLastReadDto, requestUserId, channel_id });
+    return responseForm(200, { message: 'Last Read 업데이트 성공' });
   }
 
   @Post(':channel_id')
   @UseGuards(JwtAccessGuard)
   async joinChannel(@Param('channel_id') channel_id, @Body() { community_id }, @Req() req: any) {
     const requestUserId = req.user._id;
-    try {
-      await this.channelService.joinChannel({ requestUserId, channel_id, community_id });
-      return responseForm(200, { message: '채널 접속 성공!' });
-    } catch (error) {
-      this.logger.error(JSON.stringify(error.response));
-      throw error;
-    }
+    await this.channelService.joinChannel({ requestUserId, channel_id, community_id });
+    return responseForm(200, { message: '채널 접속 성공!' });
   }
 }
