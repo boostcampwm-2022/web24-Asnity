5577dca9b4a984bcb2f750caa8c589764e3af1be
Cola <bcad1591@gmail.com>
2022-12-06 02:33:35 +0900

    feat: Socket 연결 추가

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -2,12 +2,14 @@ import CommonModal from '@components/Modals/CommonModal';
 import ContextMenuModal from '@components/Modals/ContextMenuModal';
 import Gnb from '@layouts/Gnb';
 import Sidebar from '@layouts/Sidebar';
+import Sockets from '@layouts/SocketLayer';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div className="wrapper flex flex-1">
+      <Sockets />
       <Gnb />
       <Sidebar />
       <Outlet />
