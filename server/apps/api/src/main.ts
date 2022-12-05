a62cd0b3ad03ca12332e2db6c6f9a69165223d8f
NaayoungKwon <kny8092@gmail.com>
2022-12-05 14:12:46 +0900

    refactor: intercepto에서 logging 하도록 변경

--- server/apps/api/src/main.ts
+++ server/apps/api/src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { WINSTON_MODULE_NEST_PROVIDER, WinstonLogger } from 'nest-winston';
 import { ApiModule } from './api.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as Sentry from '@sentry/node';
 import { SentryInterceptor } from './webhook.interceptor';
 import * as cookieParser from 'cookie-parser';
+import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiModule);
@@ -13,16 +14,15 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
+  app.use(morgan(':method :url :status - :response-time ms :date[iso]'));
+  app.use(cookieParser());
+  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new SentryInterceptor(app.get(WINSTON_MODULE_NEST_PROVIDER)));
   if (process.env.NODE_ENV == 'prod') {
     Sentry.init({
       dsn: process.env.SENTRY_DSN,
     });
-    app.useGlobalInterceptors(new SentryInterceptor());
-  }
-  app.use(cookieParser());
-  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
-  app.useGlobalPipes(new ValidationPipe());
-  if (process.env.NODE_ENV == 'prod') {
     await app.listen(3001);
   } else {
     await app.listen(3000);
--- server/apps/api/src/webhook.interceptor.ts
+++ server/apps/api/src/webhook.interceptor.ts
@@ -1,34 +1,46 @@
-import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import {
+  CallHandler,
+  ExecutionContext,
+  Inject,
+  Injectable,
+  LoggerService,
+  NestInterceptor,
+} from '@nestjs/common';
 import { catchError } from 'rxjs/operators';
 import { IncomingWebhook } from '@slack/webhook';
 import { of } from 'rxjs';
 import * as Sentry from '@sentry/minimal';
 import * as ip from 'ip';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 @Injectable()
 export class SentryInterceptor implements NestInterceptor {
-  // TODO: logger로 error logging interceptor에서 하도록 refactoring
+  constructor(@Inject(WINSTON_MODULE_NEST_PROVIDER) private readonly logger: LoggerService) {}
+
   intercept(_: ExecutionContext, next: CallHandler) {
     return next.handle().pipe(
       catchError((error) => {
+        this.logger.error(error.response ?? error);
         Sentry.captureException(error);
-        const webhook = new IncomingWebhook(process.env.ERROR_SLACK_WEBHOOK);
-        webhook.send({
-          attachments: [
-            {
-              color: 'danger',
-              text: '🚨asnity-server 에러 발생🚨',
-              fields: [
-                {
-                  title: error.message,
-                  value: `Server IP : ${ip.address()}\n` + error.stack,
-                  short: false,
-                },
-              ],
-              ts: Math.floor(new Date().getTime() / 1000).toString(),
-            },
-          ],
-        });
+        if (process.env.NODE_ENV == 'prod') {
+          const webhook = new IncomingWebhook(process.env.ERROR_SLACK_WEBHOOK);
+          webhook.send({
+            attachments: [
+              {
+                color: 'danger',
+                text: '🚨asnity-server 에러 발생🚨',
+                fields: [
+                  {
+                    title: error.message,
+                    value: `Server IP : ${ip.address()}\n` + error.stack,
+                    short: false,
+                  },
+                ],
+                ts: Math.floor(new Date().getTime() / 1000).toString(),
+              },
+            ],
+          });
+        }
         return of(error.response ?? error);
       }),
     );
