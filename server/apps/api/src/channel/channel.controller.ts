b6c23d0bcf9bc1e0f5cf9e90445a04193267eee6
NaayoungKwon <kny8092@gmail.com>
2022-12-05 14:19:24 +0900

    refactor: channel controller error handling interceptor로 이동
    (cherry picked from commit d768b2f03b396859de17f5265dc239e504bef778)

--- server/apps/api/src/channel/channel.controller.ts
+++ server/apps/api/src/channel/channel.controller.ts
@@ -7,26 +7,17 @@ import { responseForm } from '@utils/responseForm';
 
 @Controller('api/channel')
 export class ChannelController {
-  constructor(
-    @Inject(WINSTON_MODULE_NEST_PROVIDER) private readonly logger: LoggerService,
-    private channelService: ChannelService,
-  ) {}
+  constructor(private channelService: ChannelService) {}
 
   @Post()
   @UseGuards(JwtAccessGuard)
   async createChannel(@Body() createChannelDto: CreateChannelDto, @Req() req: any) {
     const requestUserId = req.user._id;
-    try {
-      const newChannel = await this.channelService.createChannel({
-        ...createChannelDto,
-        managerId: requestUserId,
-      });
-      delete newChannel.users;
-
-      return responseForm(200, newChannel);
-    } catch (error) {
-      this.logger.error(JSON.stringify(error.response));
-      throw error;
-    }
+    const newChannel = await this.channelService.createChannel({
+      ...createChannelDto,
+      managerId: requestUserId,
+    });
+    delete newChannel.users;
+    return responseForm(200, newChannel);
   }
 }
