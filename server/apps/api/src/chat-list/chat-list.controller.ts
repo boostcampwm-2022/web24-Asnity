c4335e4fa49ba5c4b527a8294f3bce4bf8af06b3
NaayoungKwon <kny8092@gmail.com>
2022-12-05 14:20:00 +0900

    refactor: chat list controller error handling interceptor로 이동

--- server/apps/api/src/chat-list/chat-list.controller.ts
+++ server/apps/api/src/chat-list/chat-list.controller.ts
@@ -18,10 +18,7 @@ import { responseForm } from '@utils/responseForm';
 
 @Controller('api/chat')
 export class ChatListController {
-  constructor(
-    @Inject(WINSTON_MODULE_NEST_PROVIDER) private readonly logger: LoggerService,
-    private chatListService: ChatListService,
-  ) {}
+  constructor(private chatListService: ChatListService) {}
 
   @Post(':channel_id')
   @UseGuards(JwtAccessGuard)
@@ -29,44 +26,29 @@ export class ChatListController {
     @Param('channel_id') channel_id,
     @Body() restoreMessageDto: RestoreMessageDto,
   ) {
-    try {
-      await this.chatListService.restoreMessage({ ...restoreMessageDto, channel_id });
-      return responseForm(200, { message: '채팅 저장 성공!' });
-    } catch (error) {
-      this.logger.error(JSON.stringify(error.response));
-      throw error;
-    }
+    await this.chatListService.restoreMessage({ ...restoreMessageDto, channel_id });
+    return responseForm(200, { message: '채팅 저장 성공!' });
   }
 
   @Get(':channel_id')
   @UseGuards(JwtAccessGuard)
   async getMessage(@Param('channel_id') channel_id, @Query() query: any, @Req() req: any) {
     const requestUserId = req.user._id;
-    try {
-      const chatList = await this.chatListService.getMessage({
-        ...query,
-        requestUserId,
-        channel_id,
-      });
-      return responseForm(200, chatList);
-    } catch (error) {
-      this.logger.error(JSON.stringify(error.response));
-      throw error;
-    }
+    const chatList = await this.chatListService.getMessage({
+      ...query,
+      requestUserId,
+      channel_id,
+    });
+    return responseForm(200, chatList);
   }
   @Get(':channel_id/unread-message')
   @UseGuards(JwtAccessGuard)
   async getUnreadMessagePoint(@Param('channel_id') channel_id, @Req() req: any) {
     const requestUserId = req.user._id;
-    try {
-      const unreadChatId = await this.chatListService.getUnreadMessagePoint({
-        requestUserId,
-        channel_id,
-      });
-      return responseForm(200, { unreadChatId });
-    } catch (error) {
-      this.logger.error(JSON.stringify(error.response));
-      throw error;
-    }
+    const unreadChatId = await this.chatListService.getUnreadMessagePoint({
+      requestUserId,
+      channel_id,
+    });
+    return responseForm(200, { unreadChatId });
   }
 }
