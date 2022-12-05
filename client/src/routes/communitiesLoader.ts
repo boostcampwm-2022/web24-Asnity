5a7e61b29f15954cb603f5c75e27c8e25db8dc44
Cola <bcad1591@gmail.com>
2022-12-05 21:01:13 +0900

    feat: communitiesLoader 추가

---
+++ client/src/routes/communitiesLoader.ts
@@ -0,0 +1,21 @@
+import type { CommunitySummaries } from '@apis/community';
+import type { QueryClient } from '@tanstack/react-query';
+
+import { getCommunities } from '@apis/community';
+
+import queryKeyCreator from '@/queryKeyCreator';
+
+const communitiesLoader = (_queryClient: QueryClient) => async () => {
+  const key = queryKeyCreator.community.all();
+
+  return (
+    _queryClient.getQueryData<CommunitySummaries>(key) ??
+    // eslint-disable-next-line no-return-await
+    (await _queryClient.fetchQuery<CommunitySummaries>({
+      queryKey: key,
+      queryFn: getCommunities,
+    }))
+  );
+};
+
+export default communitiesLoader;
