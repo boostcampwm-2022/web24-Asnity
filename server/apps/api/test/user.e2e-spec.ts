35d21663fe2f3b13a6f04422f144d5064b7bd6b4
NaayoungKwon <kny8092@gmail.com>
2022-12-05 21:59:41 +0900

    test: user module  e2e test 초안
    (cherry picked from commit cad137446d3c8571ff7e9f1361445f24931dfd6a)

--- server/apps/api/test/user.e2e-spec.ts
+++ server/apps/api/test/user.e2e-spec.ts
@@ -1,20 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getConnectionToken, getModelToken, MongooseModule } from '@nestjs/mongoose';
-import request from 'supertest';
+import * as request from 'supertest';
 import {
   mongoDbServerCleanup,
   mongoDbServerModule,
   mongoDbServerStop,
-} from '@api/test/mongo-server.module';
+} from '@api/modules/mongo-server.module';
 import { UserModule } from '@user/user.module';
 import { UserSchema } from '@schemas/user.schema';
 import { User } from '@schemas/user.schema';
 import { JwtAccessGuard } from '@auth/guard';
-import { MongoMemoryServer } from 'mongodb-memory-server';
-import { ApiModule } from '@api/src/api.module';
 import { ValidationPipe } from '@nestjs/common';
 import { UserService } from '@user/user.service';
 import { followerDtoMock, user1, user2 } from '@mock/user.mock';
+import { importConfigModule } from '@api/modules/Config.module';
+import { importWinstonModule } from '@api/modules/Winstone.module';
 
 describe('User E2E Test', () => {
   let app;
@@ -24,7 +24,7 @@ describe('User E2E Test', () => {
 
   beforeAll(async () => {
     const moduleRef: TestingModule = await Test.createTestingModule({
-      imports: [ApiModule],
+      imports: [importConfigModule(), mongoDbServerModule(), importWinstonModule(), UserModule],
     })
       .overrideGuard(JwtAccessGuard)
       .useValue({})
@@ -50,16 +50,29 @@ describe('User E2E Test', () => {
       message: '팔로우 신청 완료',
     });
   });
-  // it('GET /users', () => {
-  //   return request(app.getHttpServer())
-  //     .get('/api/users?search=hi')
-  //     .expect(200)
-  //     .expect((res) => {
-  //       console.log(res);
-  //       expect(res.body).toBeDefined();
-  //       // expect(res.body.result).toHaveProperty('users');
-  //     });
-  // });
+  it('GET /users', async () => {
+    const user1M = await userModel.create(user1);
+    const user2M = await userModel.create(user2);
+    return request(app.getHttpServer())
+      .get('/api/users?search=test')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.result).toBeDefined();
+        expect(res.body.result).toHaveProperty('users');
+      });
+  });
+
+  it('guart test ', async () => {
+    const user2M = await userModel.create(user2);
+    return request(app.getHttpServer())
+      .get('/api/user/followers')
+      .expect(200)
+      .expect((res) => {
+        console.log(res.body);
+        expect(res.body.result).toBeDefined();
+        // expect(res.body.result).toHaveProperty('users');
+      });
+  });
 
   afterEach(async () => {
     // await mongoDbServerCleanup();
