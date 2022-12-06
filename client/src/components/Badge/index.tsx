c6bba810982ab180972dbdb5af57d1afa38d5f0c
leegwae <leegwae@gmail.com>
2022-12-07 08:56:13 +0900

    feat: `Badge` 컴포넌트에 색상 값과 위치 프로퍼티 추가 #261

--- client/src/components/Badge/index.tsx
+++ client/src/components/Badge/index.tsx
@@ -1,11 +1,13 @@
 import type { FC } from 'react';
 
-import React from 'react';
+import React, { memo } from 'react';
 
 interface BadgeProps {
   children?: React.ReactNode;
   size?: 'small' | 'medium';
-  color?: 'success' | 'error' | 'default';
+  color?: 'success' | 'error' | 'default' | 'primary';
+  vertical?: 'top' | 'bottom';
+  horizontal?: 'right' | 'left';
 }
 
 const scale = {
@@ -17,21 +19,34 @@ const background = {
   success: 'bg-success',
   error: 'bg-error',
   default: 'bg-label',
+  primary: 'bg-indigo',
+};
+
+const verticalPosition = {
+  top: 'top-0',
+  bottom: 'bottom-0',
+};
+
+const horizontalPosition = {
+  right: 'right-0',
+  left: 'left-0',
 };
 
 const Badge: FC<BadgeProps> = ({
   children,
   size = 'small',
   color = 'default',
+  vertical = 'bottom',
+  horizontal = 'right',
 }) => {
   return (
     <div className={`relative`}>
       <div
-        className={`absolute bottom-0 right-0 rounded-full ${scale[size]} border-2 border-offWhite ${background[color]}`}
+        className={`absolute ${verticalPosition[vertical]} ${horizontalPosition[horizontal]} rounded-full ${scale[size]} border-2 border-offWhite ${background[color]}`}
       />
       {children}
     </div>
   );
 };
 
-export default Badge;
+export default memo(Badge);
