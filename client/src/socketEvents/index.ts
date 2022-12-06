b193aae848f8420b1208a0d3bf5ac7c3e3b13d1b
Cola <bcad1591@gmail.com>
2022-12-06 19:07:30 +0900

    feat: 소켓 이벤트 및 payload 인터페이스 추가

--- client/src/socketEvents/index.ts
+++ client/src/socketEvents/index.ts
@@ -1,7 +1,30 @@
 export const SOCKET_EVENTS = {
   JOIN_CHANNEL: 'join',
+  SEND_CHAT: 'new-message',
 } as const;
 
 export const joinChannelsPayload = (channelIds: string[]) => ({
   channels: channelIds,
+});
+
+export interface SendChannelPayloadParameter {
+  id: string;
+  channelId: string;
+  senderId: string;
+  content: string;
+  createdAt: Date;
+}
+
+export const sendChatPayload = ({
+  id,
+  channelId,
+  senderId,
+  content,
+  createdAt,
+}: SendChannelPayloadParameter) => ({
+  id,
+  channelId,
+  user_id: senderId,
+  message: content,
+  time: createdAt,
 });
