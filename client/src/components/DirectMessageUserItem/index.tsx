5d36d016389f9dac97d10db7b01f21abc24b9329
Cola <bcad1591@gmail.com>
2022-12-07 16:24:37 +0900

    refactor: 사용하지 않는 변수 제거

--- client/src/components/DirectMessageUserItem/index.tsx
+++ client/src/components/DirectMessageUserItem/index.tsx
@@ -1,22 +1,13 @@
 import type { FC } from 'react';
 
-import UserProfile from '@components/UserProfile';
-import { useUserQuery } from '@hooks/user';
 import React from 'react';
 
 interface Props {
   userId: string;
 }
 
-const DirectMessageUserItem: FC<Props> = ({ userId }) => {
-  const { userQuery } = useUserQuery(userId);
-
-  if (userQuery.isLoading) return <div></div>;
-  return userQuery.data ? (
-    <UserProfile user={userQuery.data} />
-  ) : (
-    <div>사용자가 존재하지 않습니다</div>
-  );
+const DirectMessageUserItem: FC<Props> = () => {
+  return <div>사용자가 존재하지 않습니다</div>;
 };
 
 export default DirectMessageUserItem;
