88b21359c2ea73d4c5f8edf967b54c187656f0e7
soomanbaek <qortnaks311@gmail.com>
2022-12-08 01:49:27 +0900

    feat: getChannelUserForm parameter 타입 선언

--- server/apps/api/src/channel/helper/addObjectForm.ts
+++ server/apps/api/src/channel/helper/addObjectForm.ts
@@ -1,4 +1,4 @@
-export function getChannelToUserForm(community_id, channel_id) {
+export function getChannelToUserForm(community_id: string, channel_id: string) {
   return {
     [`communities.${community_id.toString()}.channels.${channel_id.toString()}`]: new Date(),
   };
