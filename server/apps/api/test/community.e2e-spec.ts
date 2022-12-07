d8a78057d4e3230492117fd0e5b8a25ff8dcf804
NaayoungKwon <kny8092@gmail.com>
2022-12-08 05:12:21 +0900

    test: community module integration test 정상동작 TC 작성
    (cherry picked from commit 1f3989fa154373ba6536b6432c5182fb4ecadfa4)

---
+++ server/apps/api/test/community.e2e-spec.ts
@@ -0,0 +1,249 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { getConnectionToken } from '@nestjs/mongoose';
+import * as request from 'supertest';
+import { mongoDbServerModule, mongoDbServerStop } from '@api/modules/mongo-server.module';
+import { UserModule } from '@user/user.module';
+import { UserSchema } from '@schemas/user.schema';
+import { User } from '@schemas/user.schema';
+import { ValidationPipe } from '@nestjs/common';
+import { initTestUser1, initTestUser2 } from '@mock/user.mock';
+import { importConfigModule } from '@api/modules/Config.module';
+import { importWinstonModule } from '@api/modules/Winstone.module';
+import { followingURL, signupURL, singinURL } from '@api/test/urls/urls';
+import { CommunityModule } from '@community/community.module';
+import { Community, CommunitySchema } from '@schemas/community.schema';
+import { communityDto1, modifyCommunityDto1 } from '@mock/community.mock';
+
+describe('Community E2E Test', () => {
+  let app, userModel, communityModel, mongod, user1;
+  let accessToken;
+
+  beforeAll(async () => {
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      imports: [
+        importConfigModule(),
+        mongoDbServerModule(),
+        importWinstonModule(),
+        CommunityModule,
+      ],
+    }).compile();
+
+    mongod = await moduleRef.get(getConnectionToken());
+    userModel = mongod.model(User.name, UserSchema);
+    communityModel = mongod.model(Community.name, CommunitySchema);
+    app = moduleRef.createNestApplication();
+    await app.init();
+  });
+
+  beforeEach(async () => {
+    await request(app.getHttpServer())
+      .post(signupURL)
+      .send({
+        id: initTestUser1.id,
+        password: initTestUser1.password,
+        nickname: initTestUser1.nickname,
+      })
+      .then(async () => {
+        user1 = await userModel.findOne({ id: initTestUser1.id });
+      });
+    accessToken = (
+      await request(app.getHttpServer())
+        .post(singinURL)
+        .send({ id: user1.id, password: initTestUser1.password })
+    ).body.result.accessToken;
+  });
+
+  it('should be defined', () => {
+    expect(communityModel).toBeDefined();
+  });
+
+  describe('Post /api/community ', () => {
+    it('커뮤니티 추가', async () => {
+      await request(app.getHttpServer())
+        .post(`/api/community`)
+        .set('Authorization', `Bearer ${accessToken}`)
+        .send(communityDto1)
+        .expect((res) => {
+          expect(res.body.result).toBeDefined();
+          expect(res.body.result.name).toEqual(communityDto1.name);
+        });
+    });
+  });
+
+  describe('Get /api/communities', () => {
+    it('자신이 속한 커뮤니티들의 정보', async () => {
+      await request(app.getHttpServer())
+        .post(`/api/community`)
+        .set('Authorization', `Bearer ${accessToken}`)
+        .send(communityDto1);
+
+      await request(app.getHttpServer())
+        .get(`/api/communities`)
+        .set('Authorization', `Bearer ${accessToken}`)
+        .expect((res) => {
+          expect(res.body.result.communities).toBeDefined();
+          expect(res.body.result.communities[0].managerId).toEqual(user1._id.toString());
+          expect(res.body.result.communities[0].name).toEqual(communityDto1.name);
+        });
+    });
+  });
+
+  describe('Post /api/communities/:id/users ', () => {
+    it('커뮤니티 사용자 추가', async () => {
+      const user2 = await userModel.create(initTestUser2);
+      await request(app.getHttpServer())
+        .post(`/api/community`)
+        .set('Authorization', `Bearer ${accessToken}`)
+        .send(communityDto1);
+
+      const community = await communityModel.findOne({ name: communityDto1.name });
+      await request(app.getHttpServer())
+        .post(`/api/communities/${community._id}/users`)
+        .set('Authorization', `Bearer ${accessToken}`)
+        .send({ community_id: community._id, requestUserId: user1._id, users: [user2._id] })
+        .expect((res) => {
+          expect(res.body.result.message).toEqual('커뮤니티 사용자 추가 완료');
+        });
+      const updatedCommunity = await communityModel.findById(community._id);
+      expect(updatedCommunity.users).toContain(user2._id.toString());
+    });
+  });
+
+  describe('Delete /api/communities/:id', () => {
+    it('커뮤니티들 삭제', async () => {
+      await request(app.getHttpServer())
+        .post(`/api/community`)
+        .set('Authorization', `Bearer ${accessToken}`)
+        .send(communityDto1);
+
+      const community = await communityModel.findOne({ name: communityDto1.name });
+      await request(app.getHttpServer())
+        .delete(`/api/communities/${community._id.toString()}`)
+        .set('Authorization', `Bearer ${accessToken}`)
+        .send(communityDto1)
+        .expect((res) => {
+          expect(res.body.result.message).toEqual('커뮤니티 삭제 성공');
+        });
+
+      const updatedCommunity = await communityModel.findOne({ _id: community._id });
+      expect(community.deletedAt).toBeUndefined();
+      expect(updatedCommunity.deletedAt).toBeDefined();
+    });
+  });
+
+  describe('Get /api/communities/:id/users ', () => {
+    it('커뮤니티 사용자 정보 전달', async () => {
+      const user2 = await userModel.create(initTestUser2);
+      await request(app.getHttpServer())
+        .post(`/api/community`)
+        .set('Authorization', `Bearer ${accessToken}`)
+        .send(communityDto1);
+
+      const community = await communityModel.findOne({ name: communityDto1.name });
+      await request(app.getHttpServer())
+        .post(`/api/communities/${community._id.toString()}/users`)
+        .set('Authorization', `Bearer ${accessToken}`)
+        .send({ community_id: community._id, requestUserId: user1._id, users: [user2._id] });
+
+      await request(app.getHttpServer())
+        .get(`/api/communities/${community._id.toString()}/users`)
+        .set('Authorization', `Bearer ${accessToken}`)
+        .expect((res) => {
+          expect(res.body.result).toBeDefined();
+          expect(res.body.result.users[1]).toHaveProperty('_id', user2._id.toString());
+        });
+    });
+  });
+
+  describe('Delete /api/communities/:id/me ', () => {
+    it('커뮤니티 퇴장', async () => {
+      let user2;
+      await request(app.getHttpServer())
+        .post(signupURL)
+        .send({
+          id: initTestUser2.id,
+          password: initTestUser2.password,
+          nickname: initTestUser2.nickname,
+        })
+        .then(async () => {
+          user2 = await userModel.findOne({ id: initTestUser2.id });
+        });
+      const user2AccessToken = (
+        await request(app.getHttpServer())
+          .post(singinURL)
+          .send({ id: user2.id, password: initTestUser2.password })
+      ).body.result.accessToken;
+
+      await request(app.getHttpServer())
+        .post(`/api/community`)
+        .set('Authorization', `Bearer ${accessToken}`)
+        .send(communityDto1);
+
+      const community = await communityModel.findOne({ name: communityDto1.name });
+      await request(app.getHttpServer())
+        .post(`/api/communities/${community._id.toString()}/users`)
+        .set('Authorization', `Bearer ${accessToken}`)
+        .send({
+          community_id: community._id.toString(),
+          requestUserId: user1._id.toString(),
+          users: [user2._id],
+        });
+      await request(app.getHttpServer())
+        .delete(`/api/communities/${community._id.toString()}/me`)
+        .set('Authorization', `Bearer ${accessToken}`)
+        .expect((res) => {
+          expect(res.body.message).toEqual(
+            '매니저는 커뮤니티에서 탈퇴할 수 없습니다. 매니저 위임하세요.',
+          );
+          expect(res.body.error).toBeDefined();
+        });
+
+      await request(app.getHttpServer())
+        .delete(`/api/communities/${community._id.toString()}/me`)
+        .set('Authorization', `Bearer ${user2AccessToken}`)
+        .expect((res) => {
+          expect(res.body.result.message).toEqual('사용자 커뮤니티 탈퇴 성공');
+        });
+      const updatedCommunity = await communityModel.findOne({ name: communityDto1.name });
+      expect(updatedCommunity.users.length).toEqual(1);
+    });
+  });
+
+  describe('Patch /api/communities/:id/settings ', () => {
+    it('커뮤니티 정보 수', async () => {
+      const user2 = await userModel.create(initTestUser2);
+      await request(app.getHttpServer())
+        .post(`/api/community`)
+        .set('Authorization', `Bearer ${accessToken}`)
+        .send(communityDto1);
+
+      const community = await communityModel.findOne({ name: communityDto1.name });
+
+      await request(app.getHttpServer())
+        .patch(`/api/communities/${community._id.toString()}/settings`)
+        .set('Authorization', `Bearer ${accessToken}`)
+        .send(modifyCommunityDto1)
+        .expect((res) => {
+          expect(res.body.result.message).toEqual('커뮤니티 정보 수정 완료');
+        });
+
+      const updatedCommunity = await communityModel.findById(community._id);
+      expect(updatedCommunity.description).toEqual(modifyCommunityDto1.description);
+      expect(updatedCommunity.name).toEqual(modifyCommunityDto1.name);
+    });
+  });
+
+  afterEach(async () => {
+    // await mongoDbServerCleanup();
+    const collections = mongod.collections;
+    for (const key in collections) {
+      const collection = collections[key];
+      await collection.deleteMany({});
+    }
+  });
+
+  afterAll(async () => {
+    await mongoDbServerStop();
+    app.close();
+  });
+});
