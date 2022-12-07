cee2af6fe4b537fba9d8f9f0ff0cd721036869e1
NaayoungKwon <kny8092@gmail.com>
2022-12-07 21:42:03 +0900

    feat: new message api server 요청

--- server/apps/socket/src/exceptions/socket-catch-error.ts
+++ server/apps/socket/src/exceptions/socket-catch-error.ts
@@ -5,14 +5,9 @@ import { BaseWsExceptionFilter } from '@nestjs/websockets';
 @Catch()
 export class WsCatchAllFilter extends BaseWsExceptionFilter {
   catch(exception: Error, host: ArgumentsHost) {
+    // super.catch(exception, host);
     const socket: SocketWithAuth = host.switchToWs().getClient();
-    console.log('catch err');
-    // if (exception instanceof UnauthorizedException) {
-    //
-    //   const message = exceptionData['message'] ?? exceptionData ?? exception.name;
-    // }
-    // const exceptionData = exception.getResponse();
-    const message = exception['message'] ?? exception ?? exception.name;
-    socket.emit('failed-to-send-message', message);
+    const message = exception['message'] ?? exception ?? exception.name ?? '에러 발생';
+    socket.emit('failed-to-send-message', { message });
   }
 }
