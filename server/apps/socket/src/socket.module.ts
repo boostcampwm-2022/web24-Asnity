f58cc7b0b6759f9fe306fffd095d3aaf9ed1a602
NaayoungKwon <kny8092@gmail.com>
2022-12-07 20:07:34 +0900

    feat: socket auth 확인
    (cherry picked from commit 192f99f4c7753c394f3f8dd1c4cad778db3cf394)

--- server/apps/socket/src/socket.module.ts
+++ server/apps/socket/src/socket.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { SocketGateway } from './socket.gateway';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ChatListRespository } from '@repository/chat-list.respository';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `config/${process.env.NODE_ENV}.env`,
+    }),
+    JwtModule.register({ secret: process.env.JWT_SECRET }),
+  ],
   controllers: [],
   providers: [SocketGateway],
 })
