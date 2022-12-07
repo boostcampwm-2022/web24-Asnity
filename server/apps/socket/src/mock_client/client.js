5809b96c7818465fda2840110f23afeccff35f3b
NaayoungKwon <kny8092@gmail.com>
2022-12-07 21:42:03 +0900

    feat: new message api server 요청

--- server/apps/socket/src/mock_client/client.js
+++ server/apps/socket/src/mock_client/client.js
@@ -3,12 +3,12 @@ import { io } from 'socket.io-client';
 const port = 8080;
 // 형식은 'commu-{community id}'
 const accessToken =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzkwNDU4ODNmNDI2OGUxMjY3OGVmN2YiLCJuaWNrbmFtZSI6Im55IiwiaWF0IjoxNjcwNDA5MDQ0LCJleHAiOjE2NzA0MDk5NDR9.939Xi2pgHPi8Sm75PKw887g6EXmgzPgWTpFrTgZIgBU';
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzkwNDU4ODNmNDI2OGUxMjY3OGVmN2YiLCJuaWNrbmFtZSI6Im55IiwiaWF0IjoxNjcwNDE1OTYwLCJleHAiOjE2NzA0MTY4NjB9.iAiuTofbrlZZD6FFIddH45NF91vapWsV6V-yeTHqY4k';
 const accessToken2 =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2Mzg0NWNiMTU0NDRmMGEyMGRlNTYxMDUiLCJuaWNrbmFtZSI6InNvb21hbiIsImlhdCI6MTY2OTY1MTM1NywiZXhwIjoxNjY5NjUyMjU3fQ.T3OGoF2hz4ew1iw2c4TA1tldHgTwDxkEyUFBkfUqeHo';
 const opt = {
   auth: {
-    token: `Bearer ${accessToken2}`,
+    token: `Bearer ${accessToken}`,
   },
 };
 try {
@@ -18,7 +18,7 @@ try {
   // console.log(helloSocket.connected); // 연결되었는지 true, false로 나옴
 
   // 처음 연결 후 channel 배열을 전달해야함
-  const result = helloSocket.emit('join', { channels: ['a', 'b', 'c'] });
+  const result = helloSocket.emit('join', { channels: ['639086392258e789af7d736e', 'b', 'c'] });
   // worldSocket.emit('join', { channels: ['x', 'y', 'z'] });
   // console.log(result);
   // console.log(helloSocket);
@@ -38,18 +38,18 @@ try {
     console.log(err.message); // not authorized
   });
   helloSocket.on('failed-to-send-message', ({ message }) => {
-    console.log(message);
+    console.log('fail error ', message);
   });
   // message 전송
   helloSocket.emit('new-message', {
-    channelId: 'a',
-    user_id: '311',
-    message: 'hi i am the first client~',
+    channelId: '639086392258e789af7d736e',
+    user_id: '639045883f4268e12678ef7f',
+    message: 'hi its third message',
     time: new Date(),
   });
 
   helloSocket.emit('modify-message', {
-    channelId: 'a',
+    channelId: '639086392258e789af7d736e',
     user_id: '311',
     messageId: '0a2',
     message: 'hi this is modify message',
