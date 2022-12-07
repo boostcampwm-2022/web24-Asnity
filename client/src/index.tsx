c27b334203dc423b8abd6ad8e0e2de0983f2bd3f
Cola <bcad1591@gmail.com>
2022-12-08 05:21:25 +0900

    chore: 불필요한 import 제거

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,4 +1,4 @@
-import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
