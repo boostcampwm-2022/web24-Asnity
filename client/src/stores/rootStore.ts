610bd1f54906b01dc4689257198ac9cf2204b56b
Cola <bcad1591@gmail.com>
2022-12-07 21:32:41 +0900

    feat: 스크롤바 Ref Slice 추가

--- client/src/stores/rootStore.ts
+++ client/src/stores/rootStore.ts
@@ -1,16 +1,21 @@
+import type { ChatScrollbarSlice } from '@stores/chatScrollBarSlice';
 import type { CommonModalSlice } from '@stores/commonModalSlice';
 import type { ContextMenuModalSlice } from '@stores/contextMenuModalSlice';
 
+import { chatScrollbarSlice } from '@stores/chatScrollBarSlice';
 import { commonModalSlice } from '@stores/commonModalSlice';
 import { contextMenuModalSlice } from '@stores/contextMenuModalSlice';
 import store from 'zustand';
 import { devtools } from 'zustand/middleware';
 
-export type Store = ContextMenuModalSlice & CommonModalSlice;
+export type Store = ContextMenuModalSlice &
+  CommonModalSlice &
+  ChatScrollbarSlice;
 
 export const useRootStore = store<Store>()(
   devtools((...a) => ({
     ...contextMenuModalSlice(...a),
     ...commonModalSlice(...a),
+    ...chatScrollbarSlice(...a),
   })),
 );
