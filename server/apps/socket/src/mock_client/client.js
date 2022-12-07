eeca328760ca1f2c24c54ff460191195e1c90b15
NaayoungKwon <kny8092@gmail.com>
2022-12-07 20:07:34 +0900

    feat: socket auth 확인
    (cherry picked from commit 192f99f4c7753c394f3f8dd1c4cad778db3cf394)

--- server/apps/socket/src/mock_client/client.js
+++ server/apps/socket/src/mock_client/client.js
@@ -2,36 +2,58 @@ import { io } from 'socket.io-client';
 
 const port = 8080;
 // 형식은 'commu-{community id}'
-const helloSocket = io(`http://localhost:${port}/socket/commu-hello`);
-// const worldSocket = io(`http://localhost:${port}/socket/commu-world`); // 다른 namesapce
+const accessToken =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzkwNDU4ODNmNDI2OGUxMjY3OGVmN2YiLCJuaWNrbmFtZSI6Im55IiwiaWF0IjoxNjcwNDA5MDQ0LCJleHAiOjE2NzA0MDk5NDR9.939Xi2pgHPi8Sm75PKw887g6EXmgzPgWTpFrTgZIgBU';
+const accessToken2 =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2Mzg0NWNiMTU0NDRmMGEyMGRlNTYxMDUiLCJuaWNrbmFtZSI6InNvb21hbiIsImlhdCI6MTY2OTY1MTM1NywiZXhwIjoxNjY5NjUyMjU3fQ.T3OGoF2hz4ew1iw2c4TA1tldHgTwDxkEyUFBkfUqeHo';
+const opt = {
+  auth: {
+    token: `Bearer ${accessToken2}`,
+  },
+};
+try {
+  const helloSocket = io(`http://localhost:${port}/socket/commu-hello`, opt);
+  // const worldSocket = io(`http://localhost:${port}/socket/commu-world`); // 다른 namesapce
 
-// console.log(helloSocket.connected); // 연결되었는지 true, false로 나옴
+  // console.log(helloSocket.connected); // 연결되었는지 true, false로 나옴
 
-// 처음 연결 후 channel 배열을 전달해야함
-helloSocket.emit('join', { channels: ['a', 'b', 'c'] });
-// worldSocket.emit('join', { channels: ['x', 'y', 'z'] });
+  // 처음 연결 후 channel 배열을 전달해야함
+  const result = helloSocket.emit('join', { channels: ['a', 'b', 'c'] });
+  // worldSocket.emit('join', { channels: ['x', 'y', 'z'] });
+  // console.log(result);
+  // console.log(helloSocket);
+  // message listen
+  helloSocket.on('new-message', ({ channelId, user_id, message, time }) => {
+    console.log(
+      `new message channel : ${channelId}, sender : ${user_id}, msg : [${time}]${message}`,
+    );
+  });
+  helloSocket.on('modify-message', ({ channelId, user_id, messageId, message }) => {
+    console.log(
+      `modify message channel : ${channelId}, sender : ${user_id}, msg(${messageId}) : ${message}`,
+    );
+  });
+  helloSocket.on('connect_error', (err) => {
+    console.log(err instanceof Error); // true
+    console.log(err.message); // not authorized
+  });
+  helloSocket.on('failed-to-send-message', ({ message }) => {
+    console.log(message);
+  });
+  // message 전송
+  helloSocket.emit('new-message', {
+    channelId: 'a',
+    user_id: '311',
+    message: 'hi i am the first client~',
+    time: new Date(),
+  });
 
-// message listen
-helloSocket.on('new-message', ({ channelId, user_id, message, time }) => {
-  console.log(`new message channel : ${channelId}, sender : ${user_id}, msg : [${time}]${message}`);
-});
-helloSocket.on('modify-message', ({ channelId, user_id, messageId, message }) => {
-  console.log(
-    `modify message channel : ${channelId}, sender : ${user_id}, msg(${messageId}) : ${message}`,
-  );
-});
-
-// message 전송
-helloSocket.emit('new-message', {
-  channelId: 'a',
-  user_id: '311',
-  message: 'hi i am the first client~',
-  time: new Date(),
-});
-
-helloSocket.emit('modify-message', {
-  channelId: 'a',
-  user_id: '311',
-  messageId: '0a2',
-  message: 'hi this is modify message',
-});
+  helloSocket.emit('modify-message', {
+    channelId: 'a',
+    user_id: '311',
+    messageId: '0a2',
+    message: 'hi this is modify message',
+  });
+} catch (error) {
+  console.log(error);
+}
