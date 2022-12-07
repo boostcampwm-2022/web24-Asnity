436f4c7a5ccc204d1cc55b78e060c655cb37b8eb
NaayoungKwon <kny8092@gmail.com>
2022-12-07 11:18:37 +0900

    test: user module integration test

--- server/__mock__/user.mock.ts
+++ server/__mock__/user.mock.ts
@@ -1,6 +1,19 @@
 import { ObjectId } from 'bson';
 import { FollowerDto } from '@user/dto/follower.dto';
 // _id: new ObjectId('63734af9e62b37012c73e399'),
+export const initTestUser1 = {
+  name: '',
+  id: 'test@gmail.com',
+  password: 'hi12341234',
+  nickname: 'testUser1',
+};
+
+export const initTestUser2 = {
+  name: '',
+  id: 'test_asmi@naver.com',
+  password: '12341234',
+  nickname: 'asmi',
+};
 export const user1 = {
   name: '',
 
