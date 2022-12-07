d36213ae8d609c69fca7c195114072d045f273b9
leegwae <leegwae@gmail.com>
2022-12-07 17:03:33 +0900

    feat: 채팅 메시지의 목록 렌더링하는 `ChatList` 컴포넌트 분리 #272

---
+++ client/src/components/ChatList/index.tsx
@@ -0,0 +1,35 @@
+import type { GetChatsResult } from '@apis/chat';
+import type { UsersMap } from '@hooks/user';
+import type { FC } from 'react';
+
+import ChatItem from '@components/ChatItem';
+import React, { Fragment } from 'react';
+
+interface Props {
+  pages: GetChatsResult[];
+  users: UsersMap;
+}
+
+const ChatList: FC<Props> = ({ pages, users }) => {
+  return (
+    <>
+      {pages.map(
+        (page) =>
+          page.chat?.length && (
+            <Fragment key={page.chat[0].id}>
+              {page.chat.map((chat) => (
+                <ChatItem
+                  key={chat.id}
+                  chat={chat}
+                  className="px-5 py-3 tracking-tighter"
+                  user={users[chat.senderId]}
+                />
+              ))}
+            </Fragment>
+          ),
+      )}
+    </>
+  );
+};
+
+export default ChatList;
