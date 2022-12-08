a5d53e5dc87d8dc96cde836fe1aa912113ed16fe
soomanbaek <qortnaks311@gmail.com>
2022-12-09 02:16:00 +0900

    refactor: 클라이언트로부터 값을 한번에 받아오게 community 리펙토링 #291
    (cherry picked from commit 19059eba19032e367052404769e8a9ce70fc36f6)

--- server/apps/api/src/community/community.controller.ts
+++ server/apps/api/src/community/community.controller.ts
@@ -1,8 +1,8 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Controller, Post, UseGuards } from '@nestjs/common';
 import { CommunityService } from '@api/src/community/community.service';
 import { responseForm } from '@utils/responseForm';
 import { JwtAccessGuard } from '@api/src/auth/guard';
-import { CreateCommunityDto, ModifyCommunityDto } from './dto';
+import { ReceivedData } from '@custom/decorator/ReceivedData.decorator';
 
 @Controller('api/community')
 export class CommunityController {
@@ -10,12 +10,11 @@ export class CommunityController {
 
   @Post()
   @UseGuards(JwtAccessGuard)
-  async crateCommunity(@Body() createCommunityDto: CreateCommunityDto, @Req() req: any) {
-    const managerId = req.user._id;
-    const result = await this.communityService.createCommunity({
-      managerId,
-      ...createCommunityDto,
-    });
+  async crateCommunity(@ReceivedData() createCommunityDto) {
+    createCommunityDto['managerId'] = createCommunityDto.requestUserId;
+    delete createCommunityDto.requestUserId;
+
+    const result = await this.communityService.createCommunity(createCommunityDto);
     return responseForm(200, result);
   }
 }
