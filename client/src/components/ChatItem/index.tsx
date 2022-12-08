da0b96ec3a1e3806ac7772e5580c426947f1d520
Cola <bcad1591@gmail.com>
2022-12-09 04:19:03 +0900

    feat: 전송 실패시 재전송 및 지우기 버튼

--- client/src/components/ChatItem/index.tsx
+++ client/src/components/ChatItem/index.tsx
@@ -5,7 +5,8 @@ import type { ComponentPropsWithoutRef, FC } from 'react';
 import Avatar from '@components/Avatar';
 import ChatContent from '@components/ChatContent';
 import { dateStringToKRLocaleDateString } from '@utils/date';
-import React, { memo } from 'react';
+import cn from 'classnames';
+import React, { memo, useState } from 'react';
 
 interface Props extends ComponentPropsWithoutRef<'li'> {
   className?: string;
@@ -23,14 +24,27 @@ const ChatItem: FC<Props> = ({
   },
   isSystem = false,
 }) => {
-  const { content, createdAt, updatedAt, deletedAt } = chat;
+  const { content, createdAt, updatedAt, deletedAt, written } = chat;
+  const [isHover, setIsHover] = useState(false);
 
   const isUpdated = updatedAt && updatedAt !== createdAt;
   const isDeleted = !!deletedAt;
+  const isFailedToSendChat = written === false;
+  const contentClassnames = cn({
+    'opacity-40': written === -1 || isFailedToSendChat,
+  });
+
+  const failedChatControlButtonsClassnames = `flex items-center px-3 rounded`;
+  const handleMouseEnter = () => setIsHover(true);
+  const handleMouseLeave = () => setIsHover(false);
 
   return (
     chat && (
-      <li className={className}>
+      <li
+        className={className}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
         <div className="flex gap-3">
           <div className="pt-1">
             <Avatar
@@ -40,32 +54,56 @@ const ChatItem: FC<Props> = ({
               name={user.nickname}
             />
           </div>
-          <div>
-            <div className="flex gap-2 items-center text-s16">
+          <div className="grow">
+            <div className="flex gap-2 items-center text-s16 mb-2">
               <span
                 className={`font-bold ${
                   isSystem ? 'text-primary' : 'text-indigo'
-                }`}
+                } ${contentClassnames}`}
               >
                 {user.nickname}
               </span>
-              <span className="text-placeholder">
-                {dateStringToKRLocaleDateString(createdAt, {
-                  hour: 'numeric',
-                  minute: 'numeric',
-                })}
-              </span>
+              {isFailedToSendChat ? (
+                <span className="text-error font-bold">전송 실패</span>
+              ) : (
+                <span className="text-placeholder">
+                  {dateStringToKRLocaleDateString(createdAt, {
+                    hour: 'numeric',
+                    minute: 'numeric',
+                  })}
+                </span>
+              )}
               {isDeleted ? (
                 <span className="text-label">(삭제됨)</span>
               ) : (
                 isUpdated && <span className="text-label">(수정됨)</span>
               )}
+              <div className="flex justify-end grow gap-2 text-s14">
+                {isFailedToSendChat && isHover && (
+                  <>
+                    <button
+                      type="button"
+                      className={`${failedChatControlButtonsClassnames} bg-secondary hover:bg-secondary-dark text-offWhite`}
+                    >
+                      재전송
+                    </button>
+                    <button
+                      type="button"
+                      className={`${failedChatControlButtonsClassnames} bg-error hover:bg-error-dark text-offWhite`}
+                    >
+                      지우기
+                    </button>
+                  </>
+                )}
+              </div>
             </div>
-            {isDeleted ? (
-              <div>삭제된 메시지입니다</div>
-            ) : (
-              <ChatContent content={content} />
-            )}
+            <div className={`${contentClassnames}`}>
+              {isDeleted ? (
+                '삭제된 메시지입니다'
+              ) : (
+                <ChatContent content={content} />
+              )}
+            </div>
           </div>
         </div>
       </li>
