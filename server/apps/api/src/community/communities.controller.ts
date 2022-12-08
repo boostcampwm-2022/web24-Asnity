19059eba19032e367052404769e8a9ce70fc36f6
soomanbaek <qortnaks311@gmail.com>
2022-12-09 02:16:00 +0900

    refactor: 클라이언트로부터 값을 한번에 받아오게 community 리펙토링 #291

--- server/apps/api/src/community/communities.controller.ts
+++ server/apps/api/src/community/communities.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Req, UseGuards } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post, Req, UseGuards } from '@nestjs/common';
 import { CommunityService } from '@api/src/community/community.service';
 import { responseForm } from '@utils/responseForm';
 import { JwtAccessGuard } from '@api/src/auth/guard';
 import { AppendUsersToCommunityDto, DeleteCommunityDto, ModifyCommunityDto } from '@community/dto';
 import { RequestUserAboutCommunityDto } from '@community/dto/request-user-about-community.dto';
+import { ReceivedData } from '@custom/decorator/ReceivedData.decorator';
 
 @Controller('api/communities')
 export class CommunitiesController {
@@ -12,73 +13,50 @@ export class CommunitiesController {
   @Get()
   @UseGuards(JwtAccessGuard)
   async getCommunities(@Req() req: any) {
-    const _id = req.user._id;
-    const result = await this.communityService.getCommunities(_id);
+    const requestUserid = req.user._id;
+    const result = await this.communityService.getCommunities(requestUserid);
     return responseForm(200, result);
   }
 
   @Post(':community_id/users')
   @UseGuards(JwtAccessGuard)
   async appendUsersToCommunity(
-    @Param('community_id') community_id: string,
-    @Body() appendUsersToCommunityDto: AppendUsersToCommunityDto,
-    @Req() req: any,
+    @ReceivedData() appendUsersToCommunityDto: AppendUsersToCommunityDto,
   ) {
-    const requestUserId = req.user._id;
-    await this.communityService.appendUsersToCommunity({
-      ...appendUsersToCommunityDto,
-      community_id,
-      requestUserId,
-    });
+    console.log(appendUsersToCommunityDto);
+    await this.communityService.appendUsersToCommunity(appendUsersToCommunityDto);
     return responseForm(200, { message: '커뮤니티 사용자 추가 완료' });
   }
 
   @Delete(':community_id')
   @UseGuards(JwtAccessGuard)
-  async deleteCommunity(@Param('community_id') community_id: string, @Req() req: any) {
-    const requestUserId = req.user._id;
-    const deleteCommunityDto: DeleteCommunityDto = { requestUserId, community_id };
+  async deleteCommunity(@ReceivedData() deleteCommunityDto: DeleteCommunityDto) {
     await this.communityService.deleteCommunity(deleteCommunityDto);
     return responseForm(200, { message: '커뮤니티 삭제 성공' });
   }
 
   @Get(':community_id/users')
   @UseGuards(JwtAccessGuard)
-  async getUsersInCommunity(@Param('community_id') community_id: string, @Req() req: any) {
-    const requestUserId = req.user._id;
-    const requestUserAboutCommunityDto: RequestUserAboutCommunityDto = {
-      community_id,
-      requestUserId,
-    };
+  async getUsersInCommunity(
+    @ReceivedData() requestUserAboutCommunityDto: RequestUserAboutCommunityDto,
+  ) {
     const result = await this.communityService.getUsersInCommunity(requestUserAboutCommunityDto);
     return responseForm(200, result);
   }
 
   @Delete(':community_id/me')
   @UseGuards(JwtAccessGuard)
-  async exitUserInCommunity(@Param('community_id') community_id: string, @Req() req: any) {
-    const requestUserId = req.user._id;
-    const requestUserAboutCommunityDto: RequestUserAboutCommunityDto = {
-      community_id,
-      requestUserId,
-    };
+  async exitUserInCommunity(
+    @ReceivedData() requestUserAboutCommunityDto: RequestUserAboutCommunityDto,
+  ) {
     await this.communityService.exitUserInCommunity(requestUserAboutCommunityDto);
     return responseForm(200, { message: '사용자 커뮤니티 탈퇴 성공' });
   }
 
   @Patch(':community_id/settings')
   @UseGuards(JwtAccessGuard)
-  async modifyCommunitySetting(
-    @Param('community_id') community_id: string,
-    @Body() modifyCommunityDto: ModifyCommunityDto,
-    @Req() req: any,
-  ) {
-    const requestUserId = req.user._id;
-    await this.communityService.modifyCommunity({
-      ...modifyCommunityDto,
-      community_id,
-      requestUserId,
-    });
+  async modifyCommunitySetting(@ReceivedData() modifyCommunityDto: ModifyCommunityDto) {
+    await this.communityService.modifyCommunity(modifyCommunityDto);
     return responseForm(200, { message: '커뮤니티 정보 수정 완료' });
   }
 }
