8633b2318a9f5b5ff018296ffd3c7e4069539999
soomanbaek <qortnaks311@gmail.com>
2022-12-09 01:28:55 +0900

    refactor: 클라이언트로부터 값을 한번에 받아오게 channel 리펙토링 #291

--- server/apps/api/src/channel/channels.controller.ts
+++ server/apps/api/src/channel/channels.controller.ts
@@ -1,8 +1,17 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Req, UseGuards } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
 import { responseForm } from '@utils/responseForm';
 import { ChannelService } from '@channel/channel.service';
-import { InviteChannelDto, ModifyChannelDto, UpdateLastReadDto } from '@channel/dto';
+import {
+  DeleteChannelDto,
+  ExitChannelDto,
+  InviteChannelDto,
+  JoinChannelDto,
+  ModifyChannelDto,
+  UpdateLastReadDto,
+} from '@channel/dto';
 import { JwtAccessGuard } from '@auth/guard';
+import { ReceivedData } from '@custom/decorator/ReceivedData.decorator';
+import { ObjectIdValidationPipe } from '@custom/pipe/mongodbObjectIdValidation.pipe';
 
 @Controller('api/channels')
 export class ChannelsController {
@@ -10,63 +19,50 @@ export class ChannelsController {
 
   @Patch(':channel_id/settings')
   @UseGuards(JwtAccessGuard)
-  async modifyChannel(
-    @Param('channel_id') channel_id,
-    @Body() modifyChannelDto: ModifyChannelDto,
-    @Req() req: any,
-  ) {
-    const requestUserId = req.user._id;
-    await this.channelService.modifyChannel({ ...modifyChannelDto, channel_id, requestUserId });
+  async modifyChannel(@ReceivedData() modifyChannelDto: ModifyChannelDto) {
+    await this.channelService.modifyChannel(modifyChannelDto);
     return responseForm(200, { message: '채널 수정 성공!' });
   }
 
   @Delete(':channel_id/me')
   @UseGuards(JwtAccessGuard)
-  async exitChannel(@Param('channel_id') channel_id, @Req() req: any) {
-    const requestUserId = req.user._id;
-    await this.channelService.exitChannel({ channel_id, requestUserId });
+  async exitChannel(@ReceivedData() exitChannelDto: ExitChannelDto) {
+    await this.channelService.exitChannel(exitChannelDto);
     return responseForm(200, { message: '채널 퇴장 성공!' });
   }
 
   @Get(':channel_id')
   @UseGuards(JwtAccessGuard)
-  async getChannelInfo(@Param('channel_id') channel_id: string) {
+  async getChannelInfo(@Param('channel_id', ObjectIdValidationPipe) channel_id) {
     const channelInfo = await this.channelService.getChannelInfo(channel_id);
     return responseForm(200, channelInfo);
   }
 
   @Delete(':channel_id')
   @UseGuards(JwtAccessGuard)
-  async deleteChannel(@Param('channel_id') channel_id, @Req() req: any) {
-    const requestUserId = req.user._id;
-    await this.channelService.deleteChannel({ channel_id, requestUserId });
+  async deleteChannel(@ReceivedData() deleteChannelDto: DeleteChannelDto) {
+    await this.channelService.deleteChannel(deleteChannelDto);
     return responseForm(200, { message: '채널 삭제 성공' });
   }
 
   @Post(':channel_id/users')
   @UseGuards(JwtAccessGuard)
-  async inviteChannel(@Param('channel_id') channel_id, @Body() inviteChannelDto: InviteChannelDto) {
-    await this.channelService.inviteChannel({ ...inviteChannelDto, channel_id });
+  async inviteChannel(@ReceivedData() inviteChannelDto: InviteChannelDto) {
+    await this.channelService.inviteChannel(inviteChannelDto);
     return responseForm(200, { message: '채널 초대 성공' });
   }
 
   @Patch(':channel_id/lastRead')
   @UseGuards(JwtAccessGuard)
-  async updateLastRead(
-    @Param('channel_id') channel_id,
-    @Body() updateLastReadDto: UpdateLastReadDto,
-    @Req() req: any,
-  ) {
-    const requestUserId = req.user._id;
-    await this.channelService.updateLastRead({ ...updateLastReadDto, requestUserId, channel_id });
+  async updateLastRead(@ReceivedData() updateLastReadDto: UpdateLastReadDto) {
+    await this.channelService.updateLastRead(updateLastReadDto);
     return responseForm(200, { message: 'Last Read 업데이트 성공' });
   }
 
   @Post(':channel_id')
   @UseGuards(JwtAccessGuard)
-  async joinChannel(@Param('channel_id') channel_id, @Body() { community_id }, @Req() req: any) {
-    const requestUserId = req.user._id;
-    await this.channelService.joinChannel({ requestUserId, channel_id, community_id });
+  async joinChannel(@ReceivedData() joinChannelDto: JoinChannelDto) {
+    await this.channelService.joinChannel(joinChannelDto);
     return responseForm(200, { message: '채널 접속 성공!' });
   }
 }
