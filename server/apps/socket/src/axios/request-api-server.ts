4e82665d459be12f343b0ddc5dbab59f815168b4
NaayoungKwon <kny8092@gmail.com>
2022-12-09 17:13:34 +0900

    fix: api request error 발생 시 error throw 대신 callback 인자로 전달.

--- server/apps/socket/src/axios/request-api-server.ts
+++ server/apps/socket/src/axios/request-api-server.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { WsException } from '@nestjs/websockets';
 
 export const requestApiServer = async ({ path, accessToken, data }) => {
-  const apiUrl = 'http://localhost:3000' + path;
+  const apiUrl = 'http://localhost:' + (process.env.NODE_ENV == 'dev' ? 3000 : 3001) + path;
   try {
     const response = await axios({
       method: 'post',
@@ -19,9 +19,12 @@ export const requestApiServer = async ({ path, accessToken, data }) => {
       response.data?.name?.match(/error/i) ||
       response.data?.message?.match(/error/i)
     ) {
-      throw new WsException('API Server 요청 중 에러가 발생했습니다.');
+      // throw new WsException('API Server 요청 중 에러가 발생했습니다.');
+      return false;
     }
+    return true;
   } catch (error) {
-    throw new WsException('API Server 요청 중 에러가 발생했습니다.');
+    // throw new WsException('API Server 요청 중 에러가 발생했습니다.');
+    return false;
   }
 };
