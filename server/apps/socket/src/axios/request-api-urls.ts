c8504905ab4a31651e6fc3e8395d562720b5f885
NaayoungKwon <kny8092@gmail.com>
2022-12-11 00:36:51 +0900

    feature: message 관련 event 정의(modify, delete) #312, #314
     - 새로운 메세지, 메세지 수정, 메세지 삭제
     - 메세지 이벤트 일원화 및 input에 타입 추가

--- server/apps/socket/src/axios/request-api-urls.ts
+++ server/apps/socket/src/axios/request-api-urls.ts
@@ -1,4 +1,11 @@
 export const storeMessageURL = (channelId) => `/api/channels/${channelId}/message`;
 export const joinChannelInUsersURL = (channelId) => `/api/channels/${channelId}/users`;
-export const modifyMessageURL = (channelId, messageId) =>
+export const modifyOrDeleteMessageURL = (channelId, messageId) =>
   `api/channels/${channelId}/message/${messageId}`;
+
+export const getMessageRequestURL = (data) => {
+  if (data.type == 'new') return storeMessageURL(data.channelId);
+  else if (data.type == 'modify' || data.type == 'delete')
+    return modifyOrDeleteMessageURL(data.channelId, data.messageId);
+  else throw Error('Unknown Message Request Type');
+};
