13d1deef52359f5f9bdc163e2193acb07728e7ae
soomanbaek <qortnaks311@gmail.com>
2022-12-10 17:32:59 +0900

    feat: 봇 메세지를 담당하는 BotService 생성

---
+++ server/apps/api/src/channel/bot.service.ts
@@ -0,0 +1,36 @@
+import { BOT_ID } from '@utils/def';
+import { makeChat } from '@chat-list/helper/makeChat';
+import { ChatListRespository } from '@repository/chat-list.respository';
+import { ChannelRepository } from '@repository/channel.repository';
+import { Injectable } from '@nestjs/common';
+
+const makeBotMessageDate = () =>
+  new Date().toLocaleDateString('ko', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+  });
+
+@Injectable()
+export class BotService {
+  constructor(
+    private readonly chatListRepository: ChatListRespository,
+    private readonly channelRepository: ChannelRepository,
+  ) {}
+  async infoMakeChannel(channel_id, nickname) {
+    const botMessage = {
+      channel_id: channel_id,
+      type: 'TEXT',
+      content: `${nickname}님이 이 채널을 ${makeBotMessageDate()}에 생성했습니다.`,
+      senderId: BOT_ID,
+    } as const;
+    const newChatList = await this.chatListRepository.create({
+      chat: [makeChat(0, botMessage)],
+    });
+    await this.channelRepository.addArrAtArr({ _id: channel_id }, 'chatLists', [
+      newChatList._id.toString(),
+    ]);
+  }
+}
