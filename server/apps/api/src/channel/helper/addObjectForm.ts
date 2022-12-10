60cb517e9ca995bd589552e0c58175951b2b90e2
soomanbaek <qortnaks311@gmail.com>
2022-12-11 01:19:24 +0900

    fix: 채널 이탈 시 communities:_id가 사라지는 이슈 해결

--- server/apps/api/src/channel/helper/addObjectForm.ts
+++ server/apps/api/src/channel/helper/addObjectForm.ts
@@ -1,6 +1,5 @@
 export function getChannelToUserForm(community_id: string, channel_id: string) {
   return {
-    [`communities.${community_id.toString()}._id`]: community_id,
     [`communities.${community_id.toString()}.channels.${channel_id.toString()}`]: new Date(),
   };
 }
