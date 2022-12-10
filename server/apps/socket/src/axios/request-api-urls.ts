992b682db8bfbbcb086696f58dda0aa73f22f8cc
NaayoungKwon <kny8092@gmail.com>
2022-12-10 22:05:17 +0900

    feat: message 수정 API url 생성 #312

--- server/apps/socket/src/axios/request-api-urls.ts
+++ server/apps/socket/src/axios/request-api-urls.ts
@@ -1,2 +1,4 @@
 export const storeMessageURL = (channelId) => `/api/channels/${channelId}/message`;
 export const joinChannelInUsersURL = (channelId) => `/api/channels/${channelId}/users`;
+export const modifyMessageURL = (channelId, messageId) =>
+  `api/channels/${channelId}/message/${messageId}`;
