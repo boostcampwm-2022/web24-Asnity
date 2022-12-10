d39d77d5be9cb841daa8c60f0917c3906f8918d0
NaayoungKwon <kny8092@gmail.com>
2022-12-10 19:48:13 +0900

    fix: jest 설치 의존성 문제 해결

--- server/custom/pipe/mongodbObjectIdValidation.pipe.ts
+++ server/custom/pipe/mongodbObjectIdValidation.pipe.ts
@@ -9,11 +9,8 @@ import mongoose from 'mongoose';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 export class ObjectIdValidationPipe implements PipeTransform {
-  constructor(@Inject(WINSTON_MODULE_NEST_PROVIDER) private readonly logger: LoggerService) {}
-
   transform(value: any, metadata: ArgumentMetadata) {
     if (!mongoose.Types.ObjectId.isValid(value)) {
-      this.logger.error(JSON.stringify('MongoDB 올바른 사용자 _id 형식이 아닙니다.'));
       throw new BadRequestException('올바른 사용자 _id 형식이 아닙니다.');
     }
     return value;
