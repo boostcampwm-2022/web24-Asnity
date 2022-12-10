5931c6b6f4868fbc6f14cb78d672518ea1e9e613
Cola <bcad1591@gmail.com>
2022-12-11 07:03:13 +0900

    test: 채팅 mock data 변경

--- client/src/mocks/data/chats.js
+++ client/src/mocks/data/chats.js
@@ -2,14 +2,40 @@ import { faker } from '@faker-js/faker';
 
 import { users } from './users';
 
+let id = 0;
+
 export const createMockChat = () => ({
-  id: faker.datatype.uuid(),
+  id: id++,
   type: 'TEXT',
   content: faker.lorem.sentences(),
-  senderId: users[0]._id,
+  senderId: users[Math.floor(Math.random() * users.length)]._id,
   updatedAt: '',
   createdAt: new Date().toISOString(),
   deletedAt: '',
 });
 
-export const chats = [...Array(20)].map(createMockChat);
+const chatsLength = 60;
+const chatCountPerPage = 20;
+const chats = [...Array(chatsLength)].map(createMockChat);
+const totalPageCount = Math.ceil(chatsLength / chatCountPerPage) - 1;
+
+/**
+ * @param cursor {number}
+ */
+const getChats = (cursor) => {
+  if (cursor === -1) {
+    return [];
+  }
+
+  const start = cursor * chatCountPerPage;
+  const end = start + chatCountPerPage;
+
+  return chats.slice(start, end);
+};
+
+export default {
+  chats,
+  chatCountPerPage,
+  totalPageCount,
+  getChats,
+};
