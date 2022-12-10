1b12e72b3f5acf5aff879c8d66e8c6e1d8d18b05
NaayoungKwon <kny8092@gmail.com>
2022-12-11 00:34:29 +0900

    fix: message 관련 이벤트 수정에 대한 mock client 수정

--- server/apps/socket/src/mock_client/client.js
+++ server/apps/socket/src/mock_client/client.js
@@ -1,10 +1,10 @@
 import { io } from 'socket.io-client';
 
-const port = 80;
-const url = 'http://49.50.167.202';
+const port = 8080;
+const url = 'http://localhost'; //49.50.167.202';
 // 형식은 'commu-{community id}'
 const accessToken =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzkwNDU4ODNmNDI2OGUxMjY3OGVmN2YiLCJuaWNrbmFtZSI6Im55IiwiaWF0IjoxNjcwNjY5OTgxLCJleHAiOjE2NzA2NzA4ODF9.X8-umkeMY0V5UoCyQJvEljbBSp5n-79BwD9-Sk-JYUg';
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzkwNDU4ODNmNDI2OGUxMjY3OGVmN2YiLCJuaWNrbmFtZSI6Im55IiwiaWF0IjoxNjcwNjg2NDE0LCJleHAiOjE2NzA2ODczMTR9.Bf8s_YY--guj7OtYbwD84r2qO9ShvXN4BC33AN-qPTY';
 const accessToken2 =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2Mzg0NWNiMTU0NDRmMGEyMGRlNTYxMDUiLCJuaWNrbmFtZSI6InNvb21hbiIsImlhdCI6MTY2OTY1MTM1NywiZXhwIjoxNjY5NjUyMjU3fQ.T3OGoF2hz4ew1iw2c4TA1tldHgTwDxkEyUFBkfUqeHo';
 const opt = {
@@ -19,7 +19,7 @@ try {
   // console.log(helloSocket.connected); // 연결되었는지 true, false로 나옴
 
   // 처음 연결 후 channel 배열을 전달해야함
-  const result = helloSocket.emit('join', { channels: ['a', 'b', 'c'] });
+  const result = helloSocket.emit('join', { channels: ['639086392258e789af7d736e', 'b', 'c'] });
   worldSocket.emit('join', { channels: ['x', 'y', 'z'] });
 
   helloSocket.on('new-message', ({ channelId, user_id, message, time }) => {
@@ -30,11 +30,13 @@ try {
   worldSocket.on('new-message', ({ channelId, user_id, message, time }) => {
     console.log(`new world channel : ${channelId}, sender : ${user_id}, msg : [${time}]${message}`);
   });
-  helloSocket.on('modify-message', ({ channelId, user_id, messageId, message }) => {
+
+  helloSocket.on('modify-message', ({ channelId, user_id, message, time }) => {
     console.log(
-      `modify message channel : ${channelId}, sender : ${user_id}, msg(${messageId}) : ${message}`,
+      `modify message channel : ${channelId}, sender : ${user_id}, msg : [${time}]${message}`,
     );
   });
+
   helloSocket.on('connect_error', (err) => {
     console.log(err instanceof Error); // true
     console.log(err.message); // not authorized
@@ -44,8 +46,9 @@ try {
   });
   // message 전송
   helloSocket.emit(
-    'new-message',
+    'message',
     {
+      type: 'new',
       channelId: '639086392258e789af7d736e',
       user_id: '639045883f4268e12678ef7f',
       message: 'hi its third message',
@@ -57,8 +60,9 @@ try {
   );
 
   worldSocket.emit(
-    'new-message',
+    'message',
     {
+      type: 'new',
       channelId: 'x',
       user_id: '639045883f4268e12678ef7f',
       message: 'hi its third message',
@@ -69,12 +73,19 @@ try {
     },
   );
 
-  helloSocket.emit('modify-message', {
-    channelId: '639086392258e789af7d736e',
-    user_id: '311',
-    messageId: '0a2',
-    message: 'hi this is modify message',
-  });
+  helloSocket.emit(
+    'message',
+    {
+      type: 'modify',
+      channelId: '639086392258e789af7d736e',
+      user_id: '311',
+      messageId: '0a2',
+      message: 'hi this is modify message',
+    },
+    (c) => {
+      console.log(c.written, ' modify done');
+    },
+  );
 } catch (error) {
   console.log(error);
 }
