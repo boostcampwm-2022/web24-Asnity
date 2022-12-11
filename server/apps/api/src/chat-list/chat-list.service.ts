cacfe5031cf95daf096a93c3154530f7a244c19d
soomanbaek <qortnaks311@gmail.com>
2022-12-12 03:23:42 +0900

    refactor: 채널 수정 API

--- server/apps/api/src/chat-list/chat-list.service.ts
+++ server/apps/api/src/chat-list/chat-list.service.ts
@@ -136,11 +136,12 @@ export class ChatListService {
 
     await this.chatListRespository.updateOne({ _id: chatList._id }, chatList);
 
-    chatList.chat[chatNum]['channelId'] = channel_id;
-    chatList.chat[chatNum]['communityId'] = channel.communityId;
-    chatList.chat[chatNum]['chatId'] = chat_id;
     delete chatList.chat[chatNum].id;
-
-    return chatList.chat[chatNum];
+    return {
+      ...chatList.chat[chatNum],
+      channelId: channel_id,
+      communityId: channel.communityId,
+      chatId: chat_id,
+    };
   }
 }
