c36e6721c7c602892168a320818127e84a642bb2
NaayoungKwon <kny8092@gmail.com>
2022-12-12 01:46:07 +0900

    fix: api url fix

--- server/apps/socket/src/axios/request-api-urls.ts
+++ server/apps/socket/src/axios/request-api-urls.ts
@@ -1,7 +1,7 @@
-export const storeMessageURL = (channelId) => `/api/channels/${channelId}/message`;
+export const storeMessageURL = (channelId) => `/api/channels/${channelId}/chat`;
 export const joinChannelInUsersURL = (channelId) => `/api/channels/${channelId}/users`;
 export const modifyOrDeleteMessageURL = (channelId, messageId) =>
-  `api/channels/${channelId}/messages/${messageId}`;
+  `api/channels/${channelId}/chats/${messageId}`;
 
 export const getMessageRequestURL = (data) => {
   if (data.type == 'new') return storeMessageURL(data.channelId);
