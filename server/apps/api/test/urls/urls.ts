7e8eb3f1b638cf6a097ba6d78e1b5cea64ef7c36
soomanbaek <qortnaks311@gmail.com>
2022-12-11 17:50:46 +0900

    test: url 수정 및 추가
    - 수정 : singinURL -> signinURL
    - 추가 : signoutURL, getMyInfoURL

--- server/apps/api/test/urls/urls.ts
+++ server/apps/api/test/urls/urls.ts
@@ -1,3 +1,5 @@
-export const singinURL = '/api/user/auth/signin';
+export const signinURL = '/api/user/auth/signin';
 export const signupURL = '/api/user/auth/signup';
+export const signoutURL = '/api/user/auth/signout';
+export const getMyInfoURL = '/api/user/auth/me';
 export const followingURL = '/api/user/following';
