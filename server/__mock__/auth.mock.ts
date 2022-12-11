26d987c0a9c23d476110bf321d8101949f9e2a00
soomanbaek <qortnaks311@gmail.com>
2022-12-11 18:59:17 +0900

    test: data 폴더 -> __mock__ 폴더 로 이동

--- server/__mock__/auth.mock.ts
+++ server/__mock__/auth.mock.ts
@@ -25,3 +25,21 @@ export const authData = {
     },
   },
 };
+
+export const userData = {
+  getMyInfo: {
+    responseForm: {
+      statusCode: 200,
+      result: {
+        _id: expect.any(String),
+        id: 'test@gmail.com',
+        nickname: 'test1',
+        status: 'ONLINE',
+        profileUrl: 'url',
+        description: 'default description',
+        createdAt: expect.any(String),
+        updatedAt: expect.any(String),
+      },
+    },
+  },
+};
