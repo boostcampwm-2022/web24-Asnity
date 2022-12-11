f712f897915f49ecb4034f2e1a5ebda92016fc56
soomanbaek <qortnaks311@gmail.com>
2022-12-11 17:48:58 +0900

    test: 사용자 계정정보 e2e 테스트
    - 회원가입 -> 로그인 -> 로그아웃

---
+++ server/apps/api/test/auth.e2e-spec.ts
@@ -0,0 +1,54 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import * as request from 'supertest';
+import { mongoDbServerModule } from '@api/modules/mongo-server.module';
+import { UserModule } from '@user/user.module';
+import { importConfigModule } from '@api/modules/Config.module';
+import { importWinstonModule } from '@api/modules/Winstone.module';
+import { signoutURL, signupURL, singinURL } from '@api/test/urls/urls';
+import { authData } from '@api/test/data/authData';
+
+describe('Auth E2E Test', () => {
+  let app, server;
+
+  beforeAll(async () => {
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      imports: [importConfigModule(), mongoDbServerModule(), importWinstonModule(), UserModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    await app.init();
+  });
+
+  beforeEach(async () => {
+    server = await app.getHttpServer();
+  });
+
+  describe('회원가입, 로그인', () => {
+    it('정상 동작', async () => {
+      await request(server)
+        .post(signupURL)
+        .send(authData.signup.requestForm)
+        .expect((res) => {
+          expect(res.body).toBeDefined();
+          expect(res.body).toStrictEqual(authData.signup.responseForm);
+        });
+
+      const accessToken = (
+        await request(server)
+          .post(singinURL)
+          .send(authData.signin.requestForm)
+          .expect((res) => {
+            expect(res.body).toBeDefined();
+            expect(res.body).toEqual(authData.signin.responseForm);
+          })
+      ).body.result.accessToken;
+
+      await request(server)
+        .post(signoutURL)
+        .set('Authorization', `Bearer ${accessToken}`)
+        .expect((res) => {
+          expect(res.body).toEqual(authData.signout.responseForm);
+        });
+    });
+  });
+});
