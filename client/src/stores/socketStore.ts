240675094477c7e3084ecaf3566883a080a1fdfe
Cola <bcad1591@gmail.com>
2022-12-12 09:06:03 +0900

    feat: socketStore 업데이트

--- client/src/stores/socketStore.ts
+++ client/src/stores/socketStore.ts
@@ -1,18 +1,22 @@
-import type { Socket } from 'socket.io-client';
+import type ClientIO from '@sockets/ClientIO';
 
 import store from 'zustand';
 import { devtools } from 'zustand/middleware';
 
-export type Sockets = Record<string, Socket>;
+export type Sockets = Record<string, ClientIO>;
 
 export type SocketStore = {
   sockets: Sockets;
   setSockets: (sockets: Sockets) => void;
+  clearSockets: () => void;
 };
+
+const initialSockets = {};
 
 export const useSocketStore = store<SocketStore>()(
   devtools((set) => ({
-    sockets: {},
+    sockets: initialSockets,
     setSockets: (sockets) => set({ sockets }),
+    clearSockets: () => set(initialSockets),
   })),
 );
