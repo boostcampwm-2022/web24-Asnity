73bd197ab9a27c5a40c4daafc65929b2b421fc78
Cola <bcad1591@gmail.com>
2022-12-13 01:30:28 +0900

    feat: static profile url 추가

--- client/src/constants/user.ts
+++ client/src/constants/user.ts
@@ -3,3 +3,9 @@ export const USER_STATUS = {
   ONLINE: 'ONLINE',
   AFK: 'AFK',
 } as const;
+
+export const ASNITY_DEVELOPER: Record<string, string> = {
+  default: 'https://cdn-icons-png.flaticon.com/512/1946/1946429.png',
+  'pepe@pepe.com':
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKe5YqD_avOk-6bk5y_Ag9zx4r3qlCj0q1lg&usqp=CAU',
+};
