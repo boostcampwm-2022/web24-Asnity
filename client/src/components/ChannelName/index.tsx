c863794e64e74c32627fe60b1e544704bd318697
Cola <bcad1591@gmail.com>
2022-12-12 22:28:45 +0900

    feat: 비공개인지 공개채널인지에 따라 조건부 아이콘 표시

--- client/src/components/ChannelName/index.tsx
+++ client/src/components/ChannelName/index.tsx
@@ -6,23 +6,41 @@ import React, { memo } from 'react';
 export interface Props extends ComponentPropsWithoutRef<'div'> {
   isPrivate: boolean;
   name: string;
+  size?: 'sm' | 'md' | 'lg';
 }
 
-const ChannelName: FC<Props> = ({ isPrivate, name, ...restProps }) => {
+const nameClassnames = {
+  sm: 'text-s16',
+  md: 'text-s18',
+  lg: 'text-s20',
+};
+
+const iconClassnames = {
+  sm: 'w-5 h-5',
+  md: 'w-6 h-6',
+  lg: 'w-7 h-7',
+};
+
+const ChannelName: FC<Props> = ({
+  isPrivate,
+  name,
+  size = 'sm',
+  ...restProps
+}) => {
   return (
     <div {...restProps}>
       {isPrivate ? (
         <div>
           <span className="sr-only">비공개 채널</span>
-          <LockClosedIcon className="w-5 h-5" />
+          <LockClosedIcon className={iconClassnames[size]} />
         </div>
       ) : (
         <div>
           <span className="sr-only">공개 채널</span>
-          <HashtagIcon className="w-5 h-5" />
+          <HashtagIcon className={iconClassnames[size]} />
         </div>
       )}
-      <div className="text-s16 w-full">{name}</div>
+      <div className={`${nameClassnames[size]} w-full`}>{name}</div>
     </div>
   );
 };
