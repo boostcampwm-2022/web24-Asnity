1024cfe4673906ad089d1b0c9a2f432e8bbca48d
leegwae <leegwae@gmail.com>
2022-12-12 15:06:23 +0900

    refactor: path param에서 커뮤니티/채널 아이디 뽑는 정규식 `@constants/regex`로 분리 (#325)

--- client/src/constants/regex.ts
+++ client/src/constants/regex.ts
@@ -1,5 +1,6 @@
 const REGEX = {
   EMAIL: new RegExp('^\\w+([.-]?\\w+)*@\\w+([.-]?\\w+)*(\\.\\w{2,3})+$'),
+  CHANNEL: /\/communities\/(?<communityId>\w+)\/channels\/(?<roomId>\w+)/u,
 };
 
 export default REGEX;
