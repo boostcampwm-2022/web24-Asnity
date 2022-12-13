2f2c0921657c078fd0839c632dc40565f3eac92b
leegwae <leegwae@gmail.com>
2022-12-13 19:40:52 +0900

    fix: DMNav에서 Direct Message를 Asnity로 변경

--- client/src/layouts/DmNav/index.tsx
+++ client/src/layouts/DmNav/index.tsx
@@ -14,7 +14,7 @@ const DmNav = () => {
     <nav className="flex flex-col flex-1 h-full">
       <header className="flex shrink-0 items-center px-[10px] w-full h-header border-b border-line font-ipSans text-title select-none tracking-tighter">
         <h2>
-          <Link to="/dms">Direct Message</Link>
+          <Link to="/dms">Asnity</Link>
         </h2>
       </header>
       {/* <Scrollbars>
