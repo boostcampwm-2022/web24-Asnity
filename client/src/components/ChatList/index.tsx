d5ffdb9342cb195cd8d5cef04b5ca85aec70a27c
leegwae <leegwae@gmail.com>
2022-12-13 13:14:01 +0900

    fix: 안 읽은 메시지 오류 수정
    - `useUnreadChatIdQuery`의 `cacheTime`을 0으로 주어 inactive 쿼리가 되면 무조건 캐시에서 삭제되도록 함
    - 채널 페이지에 입장할 때마다 새로 fetching해온 unradChatId 값을 ref에서 포착하도록 한다.

--- client/src/components/ChatList/index.tsx
+++ client/src/components/ChatList/index.tsx
@@ -5,6 +5,7 @@ import type { FC } from 'react';
 import ChatItem from '@components/ChatItem';
 import {
   useSetUnreadChatIdQueryData,
+  useUnreadChatIdQuery,
   useUnreadChatIdQueryData,
 } from '@hooks/chat';
 import useIntersectionObservable from '@hooks/useIntersectionObservable';
@@ -16,6 +17,7 @@ interface Props {
   users: UsersMap;
   communityManagerId?: string;
   channelManagerId?: string;
+  unreadChatId: number;
 }
 
 const ChatList: FC<Props> = ({
@@ -23,6 +25,7 @@ const ChatList: FC<Props> = ({
   users,
   communityManagerId,
   channelManagerId,
+  unreadChatId,
 }) => {
   const params = useParams();
   const channelId = params.roomId as string;
@@ -36,10 +39,10 @@ const ChatList: FC<Props> = ({
     },
   );
 
-  // 캐시에 있는 안 읽은 메시지의 id를 가져온다.
+  // 현재 캐시에 저장된 안 읽은 메시지의 id를 가져온다.
   const cachedUnreadChatId = useUnreadChatIdQueryData(channelId) as number;
-  // 채널 입장 시의 캐시에 저장되어있는 안 읽은 메시지의 id를 저장한다.
-  const firstUnreadChatId = useRef(cachedUnreadChatId);
+  // 채널 입장 시의 안 읽은 메시지의 id를 저장한다.
+  const firstUnreadChatId = useRef(unreadChatId);
 
   return (
     <>
@@ -49,17 +52,14 @@ const ChatList: FC<Props> = ({
             <Fragment key={page.chat[0].id}>
               {page.chat.map((chat) => (
                 <Fragment key={chat.id}>
-                  {/* chat의 id가 첫 렌더링 시의 캐시에 저장된 안 읽은 메시지의 id와 같다면 divider를 렌더링한다.
-                  최신 캐시에 있는 안 읽은 메시지의 id와 비교하지 않으므로 해당 값이 -1로 바뀌어도
-                  divider는 사라지지 않는다.
-                  */}
+                  {/* 채널 입장 시의 안 읽은 메시지의 id와 chat.id가 같다면 divider를 띄운다 */}
                   {chat.id === firstUnreadChatId.current && (
                     <div
                       className="flex items-center relative h-0 my-3 border-b-[1px] border-error"
                       ref={
                         /* 캐시에 저장된 안 읽은 메시지의 id가 -1이 되면 divider에 더이상
                         observable ref가 붙지 않도록 한다*/
-                        chat.id === cachedUnreadChatId
+                        cachedUnreadChatId !== -1
                           ? firstUnreadChatObservable
                           : null
                       }
