4d61fac8d59df336329d2ab01a2697b696a86482
Cola <bcad1591@gmail.com>
2022-12-13 22:37:29 +0900

    feat: static 사용자 링크 추가

--- client/src/constants/user.ts
+++ client/src/constants/user.ts
@@ -6,6 +6,14 @@ export const USER_STATUS = {
 
 export const ASNITY_DEVELOPER: Record<string, string> = {
   default: 'https://cdn-icons-png.flaticon.com/512/1946/1946429.png',
+  'a@asnity.com':
+    'https://user-images.githubusercontent.com/79135734/207336121-5c984a8a-40d3-404b-ae8e-32b1e474014e.jpg',
+  'w@asnity.com':
+    'https://user-images.githubusercontent.com/79135734/207336712-c40b1343-8110-4d50-bc53-55782ef51fba.png',
+  'e@asnity.com':
+    'https://user-images.githubusercontent.com/72093196/207337464-cc135ea2-738d-4f20-b64e-c7c9935ed0b5.png',
+  'r@asnity.com':
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKe5YqD_avOk-6bk5y_Ag9zx4r3qlCj0q1lg&usqp=CAU',
   'pepe@pepe.com':
     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKe5YqD_avOk-6bk5y_Ag9zx4r3qlCj0q1lg&usqp=CAU',
 };
