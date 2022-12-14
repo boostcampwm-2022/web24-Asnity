fd37f71ce0eceb5865482b48e1ede83c69a7398b
NaayoungKwon <kny8092@gmail.com>
2022-12-15 01:21:24 +0900

    fix: redis config 변경

--- server/apps/api/modules/Redis.module.ts
+++ server/apps/api/modules/Redis.module.ts
@@ -3,8 +3,7 @@ import { RedisModule } from '@liaoliaots/nestjs-redis';
 export const importRedisModule = () =>
   RedisModule.forRoot({
     config: {
-      host: 'localhost',
-      port: Number(process.env.REDIS_PORT),
-      password: process.env.REDIS_PW,
+      host: process.env.NODE_ENV == 'prod' ? 'cache-redis' : 'localhost',
+      port: 6379,
     },
   });
