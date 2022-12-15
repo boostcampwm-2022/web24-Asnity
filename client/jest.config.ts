55c3f1831ffb379f4bdcf26511286b90d60ba9b7
Cola <bcad1591@gmail.com>
2022-12-16 06:48:16 +0900

    test: jest.config

--- client/jest.config.ts
+++ client/jest.config.ts
@@ -11,6 +11,28 @@ const config: Config = {
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  moduleDirectories: ['node_modules', 'shared'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/src/__mocks__/fileMock.js',
+
+    '@pages/(.*)$': '<rootDir>/src/pages/$1',
+    '@layouts/(.*)$': '<rootDir>/src/layouts/$1',
+    '@components/(.*)$': '<rootDir>/src/components/$1',
+    '@mocks/(.*)$': '<rootDir>/src/mocks/$1',
+    '@stores/(.*)$': '<rootDir>/src/stores/$1',
+    '@icons/(.*)$': '<rootDir>/src/assets/icons/$1',
+    '@constants/(.*)$': '<rootDir>/src/constants/$1',
+    '@apis/(.*)$': '<rootDir>/src/apis/$1',
+    '@hooks/(.*)$': '<rootDir>/src/hooks/$1',
+    '@errors/(.*)$': '<rootDir>/src/errors/$1',
+    '@utils/(.*)$': '<rootDir>/src/utils/$1',
+    '@routes/(.*)$': '<rootDir>/src/routes/$1',
+    '@sockets/(.*)$': '<rootDir>/src/sockets/$1',
+    '@@types/(.*)$': '<rootDir>/src/types/$1',
+    '@/(.*)$': '<rootDir>/src/$1',
+  },
 };
 
 module.exports = config;
