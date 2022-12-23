ce5545f4f6d8ca0f876eb46a100f2aadac2fbecb
NaayoungKwon <kny8092@gmail.com>
2022-12-23 21:56:27 +0900

    pipe 삭제

--- server/utils/pipe.ts
+++
@@ -1,9 +0,0 @@
-export const pipe =
-  (...functions) =>
-  (dataToConvert) =>
-    functions.reduce((acc, fn) => fn(acc), dataToConvert);
-
-export const pipeAsync =
-  (...fns) =>
-  (arg) =>
-    fns.reduce((p, f) => p.then(f), Promise.resolve(arg));
