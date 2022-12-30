caa08779416c503d82b69bfa4d9a759380d9faff
NaayoungKwon <kny8092@gmail.com>
2022-12-30 16:55:07 +0900

    refactor: user controller dto 리팩토링
      - 팔로잉 추가
      - 사용자 정보 수정
      - 사용자 정보 검색: getUser -> getUsers 복수형 변경

--- server/apps/api/src/user/helper/checkRelation.ts
+++ server/apps/api/src/user/helper/checkRelation.ts
@@ -3,7 +3,7 @@ import { RELATION } from './Relation';
 
 export const checkRelation = (myFollowings, yourFollowers, followerDto) => {
   const isAlreadyFollow = myFollowings.includes(followerDto.followId);
-  const isAlreadyFollowAtOther = yourFollowers.includes(followerDto.myId);
+  const isAlreadyFollowAtOther = yourFollowers.includes(followerDto.requestUserId);
   if (!isAlreadyFollow && isAlreadyFollowAtOther) {
     throw new ConflictException('갱신 이상! (팔로우 안되어있으나, 상대방에겐 내가 팔로우됨)');
   } else if (isAlreadyFollow && !isAlreadyFollowAtOther) {
