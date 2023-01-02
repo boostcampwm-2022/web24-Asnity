fab4a17df459fe09745fa47588bafe0bf507ea26
NaayoungKwon <kny8092@gmail.com>
2023-01-02 16:49:20 +0900

    feat: 다음 chatlist 미리 캐싱

--- server/dao/repository/chat-list.respository.ts
+++ server/dao/repository/chat-list.respository.ts
@@ -2,10 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { ChatList, ChatListDocument } from '@schemas/chat-list.schema';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
+import { InjectRedis } from '@liaoliaots/nestjs-redis';
+import Redis from 'ioredis';
 
 @Injectable()
 export class ChatListRespository {
-  constructor(@InjectModel(ChatList.name) private chatListModel: Model<ChatListDocument>) {}
+  constructor(
+    @InjectModel(ChatList.name) private chatListModel: Model<ChatListDocument>,
+    @InjectRedis() private readonly redis: Redis,
+  ) {}
 
   async create(chat) {
     return await this.chatListModel.create(chat);
@@ -13,17 +18,60 @@ export class ChatListRespository {
   async findById(_id: string) {
     return await this.chatListModel.findById(_id);
   }
+
+  async findByIdAfterCache(_id: string) {
+    const cache = await this.getCache(_id);
+    if (cache) {
+      console.log('find by id hit cache ', _id);
+      return JSON.parse(cache);
+    } else {
+      console.log('find by id miss cache ', _id);
+    }
+    return await this.chatListModel.findById(_id);
+  }
+
   async addArrAtArr(filter, attribute, appendArr) {
+    this.deleteCache(filter._id);
     const addArr = {};
     addArr[attribute] = { $each: appendArr };
     return await this.chatListModel.findByIdAndUpdate(filter, { $addToSet: addArr }, { new: true });
   }
 
   async updateOne(filter, updateField) {
+    this.deleteCache(filter._id);
     return await this.chatListModel.updateOne(filter, updateField);
   }
 
   async findOneAndUpdate(filter, updateField) {
+    this.deleteCache(filter._id);
     return await this.chatListModel.findOneAndUpdate(filter, updateField, { new: true });
+  }
+
+  async deleteCache(_id: string) {
+    if (_id) {
+      this.redis.del(`chats/${_id}`);
+    }
+  }
+
+  async storeCache(_id: string, result: ChatListDocument) {
+    await this.redis.set(`chats/${_id}`, JSON.stringify(result));
+    await this.redis.expire(`chats/${_id}`, 5 * 60);
+  }
+
+  async getCache(_id: string) {
+    return await this.redis.get(`chats/${_id}`);
+  }
+
+  async saveNextExpectedChats(_id: string) {
+    if (_id === undefined) return;
+    const cache = await this.getCache(_id);
+    if (!cache) {
+      this.findById(_id).then((data) => {
+        console.log('다음 채팅을 찾아 캐시에 저장합니다 ', _id);
+        this.storeCache(_id, data);
+      });
+    } else {
+      console.log('캐시에 이미 있어 미리저장하지 않습니다. ', _id);
+    }
   }
 }
